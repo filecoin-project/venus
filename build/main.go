1d7d3f60d85666e2e33008c327e4de20f8d3a61a
Jeromy <jeromyj@gmail.com>
2018-02-08 09:34:03 -0800

    fixing deps

--- build/main.go
+++ build/main.go
@@ -43,6 +43,7 @@ func deps() {
 	log.Println("Installing dependencies...")
 
 	list := []string{
+		"go get -d github.com/ipfs/go-ipfs",
 		"go get -u github.com/whyrusleeping/gx",
 		"go get -u github.com/whyrusleeping/gx-go",
 		"gx install",
