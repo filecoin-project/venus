dcbe5722d02d2d37b617b7672f2932534feddf46
Fritz Schneider <fritz@protocol.ai>
2018-02-16 20:28:49 -1000

    uh actually add the worker files

---
+++ mining/worker.go
@@ -0,0 +1,35 @@
+package mining
+
+import (
+	"context"
+	cid "gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
+
+	"github.com/filecoin-project/go-filecoin/core"
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+// AddNewBlockFunc is a signature that enables us to hide implementation
+// details from the Worker and makes it easier to test.
+type AddNewBlockFunc func(context.Context, *types.Block) error
+
+// Worker mines. If successful, it passes the new block to AddNewBlock()
+// and returns its cid.
+type Worker struct {
+	Bg          BlockGeneratorInterface
+	AddNewBlock AddNewBlockFunc
+}
+
+// Mine attempts to mine one block. Returns the cid of the new block, if any.
+func (w *Worker) Mine(ctx context.Context, cur *types.Block, tree types.StateTreeInterface) (*cid.Cid, error) {
+	processBlock := func(ctx context.Context, b *types.Block) error {
+		return core.ProcessBlock(ctx, b, tree)
+	}
+	flushTree := func(ctx context.Context) (*cid.Cid, error) {
+		return tree.Flush(ctx)
+	}
+	next, err := w.Bg.Generate(ctx, cur, processBlock, flushTree)
+	if err != nil {
+		return nil, err
+	}
+	return next.Cid(), w.AddNewBlock(ctx, next)
+}
---
+++ mining/worker_test.go
@@ -0,0 +1,80 @@
+package mining
+
+import (
+	"context"
+	"errors"
+	cid "gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
+	"testing"
+
+	"github.com/filecoin-project/go-filecoin/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockBlockGenerator struct {
+	mock.Mock
+}
+
+func (bg *MockBlockGenerator) Generate(ctx context.Context, h *types.Block, pbf ProcessBlockFunc, ftf FlushTreeFunc) (b *types.Block, err error) {
+	args := bg.Called(ctx, h, pbf, ftf)
+	if args.Get(0) != nil {
+		b = args.Get(0).(*types.Block)
+	}
+	err = args.Error(1)
+	return
+}
+
+type mockAddNewBlockFunc struct {
+	Called bool
+	Arg    *types.Block
+}
+
+func (m *mockAddNewBlockFunc) AddNewBlock(ctx context.Context, b *types.Block) error {
+	m.Called = true
+	m.Arg = b
+	return nil
+}
+
+// Implements types.StateTreeInterface
+type FakeStateTree struct{}
+
+func (f FakeStateTree) Flush(ctx context.Context) (*cid.Cid, error) {
+	return types.SomeCid(), nil
+}
+func (f FakeStateTree) GetActor(context.Context, types.Address) (*types.Actor, error) {
+	return nil, errors.New("boom -- not expected to be called")
+}
+func (f FakeStateTree) SetActor(context.Context, types.Address, *types.Actor) error {
+	return errors.New("boom -- not expected to be called")
+}
+
+func TestWorker_Mine(t *testing.T) {
+	cur := &types.Block{Height: 2}
+	next := &types.Block{Height: 3}
+	ctx := context.Background()
+
+	// Success
+	mockBg, mockAddNewBlock := &MockBlockGenerator{}, &mockAddNewBlockFunc{}
+	w := &Worker{
+		Bg:          mockBg,
+		AddNewBlock: mockAddNewBlock.AddNewBlock,
+	}
+	mockBg.On("Generate", ctx, cur, mock.AnythingOfType("ProcessBlockFunc"), mock.AnythingOfType("FlushTreeFunc")).Return(next, nil)
+	cid, err := w.Mine(ctx, cur, FakeStateTree{})
+	assert.NoError(t, err)
+	assert.True(t, cid.Equals(next.Cid()))
+	mockBg.AssertExpectations(t)
+	assert.True(t, mockAddNewBlock.Called)
+	assert.Equal(t, next, mockAddNewBlock.Arg)
+
+	// Block generation fails.
+	mockBg, mockAddNewBlock = &MockBlockGenerator{}, &mockAddNewBlockFunc{}
+	w = &Worker{
+		Bg:          mockBg,
+		AddNewBlock: mockAddNewBlock.AddNewBlock,
+	}
+	mockBg.On("Generate", ctx, cur, mock.AnythingOfType("ProcessBlockFunc"), mock.AnythingOfType("FlushTreeFunc")).Return(nil, errors.New("boom"))
+	cid, err = w.Mine(ctx, cur, FakeStateTree{})
+	assert.Error(t, err)
+	mockBg.AssertExpectations(t)
+}
