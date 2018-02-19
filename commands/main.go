aac2b52cee78b494b8345c8392f7e64de585c8dc
Fritz Schneider <fritz@protocol.ai>
2018-02-19 09:11:27 -1000

    remove comment

--- commands/main.go
+++ commands/main.go
@@ -48,7 +48,6 @@ var rootCmd = &cmds.Command{
 
 // all top level commands. set during init() to avoid configuration loops.
 var rootSubcmdsDaemon = map[string]*cmds.Command{
-	//TODO temp?
 	"address":      addrsCmd,
 	"chain":        chainCmd,
 	"daemon":       daemonCmd,
