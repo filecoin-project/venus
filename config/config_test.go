fe45f1b314da4247458daec0c71ffa93a445d74d
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-03-22 15:57:06 +0100

    feat(config): set defaults when reading from disk

--- config/config_test.go
+++ config/config_test.go
@@ -61,3 +61,72 @@ func TestConfigRoundtrip(t *testing.T) {
 
 	assert.Equal(cfg, cfgout)
 }
+
+func TestConfigReadFileDefaults(t *testing.T) {
+	t.Run("all sections exist", func(t *testing.T) {
+		assert := assert.New(t)
+
+		cfgpath, cleaner, err := createConfigFile(`
+[api]
+address = ":9999"
+# ignored
+other = false
+
+[swarm]
+# ignored
+other = "hello"
+`)
+		assert.NoError(err)
+		defer cleaner()
+		cfg, err := ReadFile(cfgpath)
+		assert.NoError(err)
+
+		assert.Equal(cfg.API.Address, ":9999")
+		assert.Equal(cfg.Swarm.Address, "/ip4/127.0.0.1/tcp/6000")
+	})
+
+	t.Run("missing one section", func(t *testing.T) {
+		assert := assert.New(t)
+
+		cfgpath, cleaner, err := createConfigFile(`
+[api]
+address = ":9999"
+# ignored
+other = false
+`)
+		assert.NoError(err)
+		defer cleaner()
+		cfg, err := ReadFile(cfgpath)
+		assert.NoError(err)
+
+		assert.Equal(cfg.API.Address, ":9999")
+		assert.Equal(cfg.Swarm.Address, "/ip4/127.0.0.1/tcp/6000")
+	})
+
+	t.Run("empty file", func(t *testing.T) {
+		assert := assert.New(t)
+
+		cfgpath, cleaner, err := createConfigFile("")
+		assert.NoError(err)
+		defer cleaner()
+		cfg, err := ReadFile(cfgpath)
+		assert.NoError(err)
+
+		assert.Equal(cfg.API.Address, ":3453")
+		assert.Equal(cfg.Swarm.Address, "/ip4/127.0.0.1/tcp/6000")
+	})
+}
+
+func createConfigFile(content string) (string, func(), error) {
+	dir, err := ioutil.TempDir("", "config")
+	if err != nil {
+		return "", nil, err
+	}
+	cfgpath := filepath.Join(dir, "config.toml")
+
+	if err := ioutil.WriteFile(cfgpath, []byte(content), 0644); err != nil {
+		return "", nil, err
+	}
+
+	return cfgpath, func() { os.RemoveAll(dir) }, nil
+}
