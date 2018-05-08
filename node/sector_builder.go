53008b9b8d4a8f96b03d787eaba1eba9ac02ce3d
porcuquine <porcuquine@users.noreply.github.com>
2018-05-08 11:12:14 +0100

    Persist SectorBuilder metadata (#413)
    * Persist SectorBuilder metadata.
    
    Rename SectorBuilder receiver to sb.
    
    Implement datastore for metadata (sealed/staging paths initially).
    
    Add iniital support for sealedSector metadata.
    
    Add metadata checkpointing.
    
    Add SectorBuilder metadata.
    
    Don't defer checkpoints.
    
    Pull datastore into SectorBuilder.
    
    Serialize sector builder metadata to CBOR.
    
    * don't recreate the dagservice every time we use it

--- node/sector_builder.go
+++ node/sector_builder.go
@@ -19,7 +19,11 @@ import (
 	//	"golang.org/x/exp/mmap"
 
 	"gx/ipfs/QmVmDhyTTUcQXFD1rRQ64fGLMSAoaQvNH3hwuaCFAPq2hy/errors"
+	ds "gx/ipfs/QmXRKBQA4wXP7xWbFiZsR1GP4HV6wMDQ1aWFxZZ4uBcPX9/go-datastore"
 	"gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
+
+	cbor "gx/ipfs/QmRVSCwQtW1rjHCay9NqKXDwbtKTgDcN4iY7PrpSqfKM5D/go-ipld-cbor"
+	ipld "gx/ipfs/Qme5bWv7wtjUNGsK2BNGVUFPKiuxWrsqrtvYwCLRw8YFES/go-ipld-format"
 
 	"github.com/filecoin-project/go-filecoin/abi"
 	"github.com/filecoin-project/go-filecoin/types"
@@ -27,11 +31,20 @@ import (
 	"github.com/filecoin-project/paper-porep-drg/go-porep/drg/drgraph"
 )
 
+func init() {
+	cbor.RegisterCborType(PieceInfo{})
+	cbor.RegisterCborType(SectorMetadata{})
+	cbor.RegisterCborType(SealedSectorMetadata{})
+	cbor.RegisterCborType(SectorBuilderMetadata{})
+}
+
 // ErrPieceTooLarge is an error indicating that a piece cannot be larger than the sector into which it is written.
 var ErrPieceTooLarge = errors.New("piece too large for sector")
 
 const sectorSize = 1024
 const nodeSize = 16
+
+var noSectorID = big.NewInt(-1)
 
 // SectorDirs describes the methods required to supply sector directories to a SectorBuilder.
 type SectorDirs interface {
@@ -46,6 +59,29 @@ type PieceInfo struct {
 	DealID uint64
 }
 
+// SectorBuilder manages packing deals into sectors
+// maybe this belongs somewhere else as part of a different thing?
+type SectorBuilder struct {
+	MinerAddr types.Address
+
+	// TODO: information about all sectors needs to be persisted to disk
+	CurSector     *Sector
+	SealedSectors []*SealedSector
+	G             *drgraph.Graph
+	Prover        *drg.Prover
+
+	stagingDir string
+	sealedDir  string
+
+	// yada yada don't hold a reference to this here, just take what you need
+
+	nd    *Node
+	store ds.Datastore
+	dserv ipld.DAGService
+
+	sectorSize uint64
+}
+
 // Sector is a filecoin storage sector. A miner fills this up with data, and
 // then seals it. The first part I can do, the second part needs to be figured out more.
 // Somehow, I turn this piecemap and backing data buffer into something that the chain can verify.
@@ -57,40 +93,99 @@ type Sector struct {
 	Pieces []*PieceInfo
 	ID     int64
 
-	filename string
-	file     *os.File
-	data     []byte
-	sealed   *SealedSector
+	Label         string
+	filename      string
+	file          *os.File
+	data          []byte
+	sealed        *SealedSector
+	sectorBuilder *SectorBuilder
 	//ReaderAt *mmap.ReaderAt
 }
 
-// SectorBuilder manages packing deals into sectors
-// maybe this belongs somewhere else as part of a different thing?
-type SectorBuilder struct {
-	MinerAddr types.Address
+// SealedSector is a sector that has been sealed by the PoRep setup process
+type SealedSector struct {
+	replicaData []byte
+	merkleRoot  []byte
+	baseSector  *Sector
 
-	// TODO: information about all sectors needs to be persisted to disk
-	CurSector     *Sector
-	SealedSectors []*SealedSector
+	label string
 
-	G      *drgraph.Graph
-	Prover *drg.Prover
+	filename string
+	file     *os.File
+}
 
-	stagingDir string
-	sealedDir  string
+// SectorMetadata represent the persistent metadata associated with a Sector.
+type SectorMetadata struct {
+	StagingPath string
+	Pieces      []*PieceInfo
+	Size        uint64
+	Free        uint64
+	MerkleRoot  []byte
+}
 
-	// yada yada don't hold a reference to this here, just take what you need
-	nd         *Node
-	sectorSize uint64
+// SealedSectorMetadata represent the persistent metadata associated with a SealedSector.
+type SealedSectorMetadata struct {
+	MerkleRoot      []byte
+	Label           string
+	BaseSectorLabel string
+	SealedPath      string
+}
+
+// SectorBuilderMetadata represent the persistent metadata associated with a SectorBuilder.
+type SectorBuilderMetadata struct {
+	Miner         types.Address
+	Sectors       []string
+	SealedSectors []string
+}
+
+// SectorMetadata returns the metadata associated with a Sector.
+func (s *Sector) SectorMetadata() *SectorMetadata {
+	meta := &SectorMetadata{
+		StagingPath: s.filename,
+		Pieces:      s.Pieces,
+		Size:        s.Size,
+		Free:        s.Free,
+		MerkleRoot:  []byte{},
+	}
+	if s.sealed != nil {
+		meta.MerkleRoot = s.sealed.merkleRoot
+	}
+	return meta
+}
+
+// SealedSectorMetadata returns the metadata associated with a SealedSector.
+func (ss *SealedSector) SealedSectorMetadata() *SealedSectorMetadata {
+	meta := &SealedSectorMetadata{
+		MerkleRoot:      ss.merkleRoot,
+		Label:           ss.label,
+		BaseSectorLabel: ss.baseSector.Label,
+		SealedPath:      ss.filename,
+	}
+	return meta
+}
+
+// SectorBuilderMetadata returns the metadata associated with a SectorBuilderMetadata.
+func (sb *SectorBuilder) SectorBuilderMetadata() *SectorBuilderMetadata {
+	meta := SectorBuilderMetadata{
+		Miner:         sb.MinerAddr,
+		Sectors:       []string{sb.CurSector.Label},
+		SealedSectors: make([]string, len(sb.SealedSectors)),
+	}
+	for i, sealed := range sb.SealedSectors {
+		meta.SealedSectors[i] = sealed.label
+	}
+	return &meta
 }
 
 // NewSector allocates and returns a new Sector with file initialized, along with any error.
-func (smc *SectorBuilder) NewSector() (s *Sector, err error) {
+func (sb *SectorBuilder) NewSector() (s *Sector, err error) {
+	p, label := sb.newSectorPath()
 	s = &Sector{
-		Size: smc.sectorSize,
-		Free: smc.sectorSize,
+		Size:          sb.sectorSize,
+		Free:          sb.sectorSize,
+		Label:         label,
+		sectorBuilder: sb,
 	}
-	p := smc.newSectorPath()
 	err = os.MkdirAll(path.Dir(p), os.ModePerm)
 	if err != nil {
 		return s, err
@@ -104,14 +199,16 @@ func (smc *SectorBuilder) NewSector() (s *Sector, err error) {
 }
 
 // NewSealedSector allocates and returns a new SealedSector from replicaData, merkleRoot, and baseSector, along with any error.
-func (smc *SectorBuilder) NewSealedSector(replicaData []byte, merkleRoot []byte, baseSector *Sector) (ss *SealedSector,
+func (sb *SectorBuilder) NewSealedSector(replicaData []byte, merkleRoot []byte, baseSector *Sector) (ss *SealedSector,
 	err error) {
+	p, label := sb.newSealedSectorPath()
 	ss = &SealedSector{
 		replicaData: replicaData,
 		merkleRoot:  merkleRoot,
 		baseSector:  baseSector,
+		label:       label,
 	}
-	p := smc.newSealedSectorPath()
+	sb.SealedSectors = append(sb.SealedSectors, ss)
 	err = os.MkdirAll(path.Dir(p), os.ModePerm)
 	if err != nil {
 		return ss, err
@@ -127,52 +224,74 @@ func (smc *SectorBuilder) NewSealedSector(replicaData []byte, merkleRoot []byte,
 // NewSectorBuilder creates a new sector builder from the given node
 func NewSectorBuilder(nd *Node, sectorSize int, fs SectorDirs) (*SectorBuilder, error) {
 	g := drgraph.New(sectorSize / nodeSize)
-	smc := &SectorBuilder{
+	sb := &SectorBuilder{
 		G:          g,
 		Prover:     drg.NewProver("cats", g, nodeSize),
 		nd:         nd,
 		sectorSize: uint64(sectorSize),
+		store:      nd.Repo.Datastore(),
+		dserv:      dag.NewDAGService(nd.Blockservice),
 	}
-	smc.stagingDir = fs.StagingDir()
-	smc.sealedDir = fs.SealedDir()
 
-	s, err := smc.NewSector()
+	sb.stagingDir = fs.StagingDir()
+	sb.sealedDir = fs.SealedDir()
+
+	s, err := sb.NewSector()
 	if err != nil {
 		return nil, err
 	}
-	smc.CurSector = s
-	return smc, nil
+	sb.CurSector = s
+	return sb, sb.checkpoint()
+}
+
+func (sb *SectorBuilder) checkpoint() error {
+	sector := sb.CurSector
+	if err := sb.checkpointBuilderMeta(); err != nil {
+		return err
+	}
+	if err := sb.checkpointSectorMeta(sector); err != nil {
+		return err
+	}
+	if sector.sealed != nil {
+		return sb.checkpointSealedMeta(sector.sealed)
+	}
+	return nil
+
 }
 
 // AddPiece writes the given piece into a sector
-func (smc *SectorBuilder) AddPiece(ctx context.Context, pi *PieceInfo) error {
-	if pi.Size > smc.CurSector.Size {
+func (sb *SectorBuilder) AddPiece(ctx context.Context, pi *PieceInfo) error {
+	if pi.Size > sb.CurSector.Size {
 		return ErrPieceTooLarge
 	}
-	if smc.CurSector.Free < pi.Size {
+	if sb.CurSector.Free < pi.Size {
 		// No room for piece in current sector.
-		curSector := smc.CurSector
-		newSector, err := smc.NewSector()
+		oldSector := sb.CurSector
+		newSector, err := sb.NewSector()
 		if err != nil {
 			return err
 		}
-		smc.CurSector = newSector
+		sb.CurSector = newSector
 
-		go smc.SealAndPostSector(context.Background(), curSector)
+		go func() {
+			err := sb.SealAndPostSector(context.Background(), oldSector)
+			if err != nil {
+				panic(err)
+			}
+		}()
 	}
 
-	dserv := dag.NewDAGService(smc.nd.Blockservice)
-	root, err := dserv.Get(ctx, pi.Ref)
+	root, err := sb.dserv.Get(ctx, pi.Ref)
 	if err != nil {
 		return err
 	}
 
-	r, err := uio.NewDagReader(ctx, root, dserv)
+	r, err := uio.NewDagReader(ctx, root, sb.dserv)
 	if err != nil {
 		return err
 	}
 
-	if err := smc.CurSector.WritePiece(pi, r); err != nil {
+	if err := sb.CurSector.WritePiece(pi, r); err != nil {
 		return err
 	}
 
@@ -206,35 +325,40 @@ func (s *Sector) OpenAppend() error {
 //}
 
 // SealAndPostSector seals the given sector and posts its commitment on-chain
-func (smc *SectorBuilder) SealAndPostSector(ctx context.Context, s *Sector) {
-	ss, err := smc.Seal(s, smc.MinerAddr, filecoinParameters)
+func (sb *SectorBuilder) SealAndPostSector(ctx context.Context, s *Sector) (err error) {
+	ss, err := sb.Seal(s, sb.MinerAddr, filecoinParameters)
 	if err != nil {
 		// Hard to say what to do in this case.
 		// Depending on the error, it could be "try again"
 		// or 'verify data integrity and try again'
-		panic(err)
+		return
 	}
-
+	if err := sb.checkpointSealedMeta(ss); err != nil {
+		return err
+	}
 	s.sealed = ss
-
-	if err := smc.PostSealedSector(ctx, ss); err != nil {
+	if err := sb.checkpointSectorMeta(s); err != nil {
+		return err
+	}
+	if err := sb.PostSealedSector(ctx, ss); err != nil {
 		// 'try again'
 		// This can fail if the miners owner doesnt have enough funds to pay gas.
 		// It can also happen if the miner included a deal in this sector that
 		// is already sealed in a different sector.
-		panic(err)
+		return err
 	}
+	return nil
 }
 
 // PostSealedSector posts the given sealed sector's commitment to the chain
-func (smc *SectorBuilder) PostSealedSector(ctx context.Context, ss *SealedSector) error {
+func (sb *SectorBuilder) PostSealedSector(ctx context.Context, ss *SealedSector) error {
 	var deals []uint64
 	for _, p := range ss.baseSector.Pieces {
 		deals = append(deals, p.DealID)
 	}
 
 	params, err := abi.ToEncodedValues(
-		big.NewInt(-1), // NB: we might already know the sector ID from having created it already
+		noSectorID, // NB: we might already know the sector ID from having created it already
 		ss.merkleRoot,
 		deals,
 	)
@@ -243,9 +367,9 @@ func (smc *SectorBuilder) PostSealedSector(ctx context.Context, ss *SealedSector
 	}
 
 	minerOwner := types.Address{} // TODO: get the miner owner to send this from
-	msg := types.NewMessage(minerOwner, smc.MinerAddr, 0, nil, "commitSector", params)
+	msg := types.NewMessage(minerOwner, sb.MinerAddr, 0, nil, "commitSector", params)
 
-	if err := smc.nd.AddNewMessage(ctx, msg); err != nil {
+	if err := sb.nd.AddNewMessage(ctx, msg); err != nil {
 		return errors.Wrap(err, "pushing out commitSector message failed")
 	}
 
@@ -254,10 +378,12 @@ func (smc *SectorBuilder) PostSealedSector(ctx context.Context, ss *SealedSector
 }
 
 // WritePiece writes data from the given reader to the sectors underlying storage
-func (s *Sector) WritePiece(pi *PieceInfo, r io.Reader) error {
+func (s *Sector) WritePiece(pi *PieceInfo, r io.Reader) (finalErr error) {
 	if err := s.OpenAppend(); err != nil {
 		return err
 	}
+	// Capture any error from the deferred close as the value to return.
+	defer func() { finalErr = s.file.Close() }()
 
 	n, err := io.Copy(s.file, r)
 	if err != nil {
@@ -268,7 +394,7 @@ func (s *Sector) WritePiece(pi *PieceInfo, r io.Reader) error {
 		return err
 	}
 
-	// TODO: this is temporary. Use the above code later
+	// TODO:
 	// We should be writing this out to a file in the 'staging' area. Once the
 	// sector is ready to be sealed, we should mmap it and pass it to the
 	// sealing code. The sealing code prefers fairly random access to the data,
@@ -284,7 +410,11 @@ func (s *Sector) WritePiece(pi *PieceInfo, r io.Reader) error {
 	s.Free -= pi.Size
 	s.Pieces = append(s.Pieces, pi)
 
-	return s.file.Close()
+	err = s.sectorBuilder.checkpointSectorMeta(s) // TODO: make this a method of Sector.
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // PublicParameters is
@@ -293,19 +423,9 @@ type PublicParameters struct {
 	blockSize uint64
 }
 
-// SealedSector is a sector that has been sealed by the PoRep setup process
-type SealedSector struct {
-	replicaData []byte
-	merkleRoot  []byte
-	baseSector  *Sector
-
-	filename string
-	file     *os.File
-}
-
 // Seal runs the PoRep setup process using the given parameters
 // address may turn into a secret key
-func (smc *SectorBuilder) Seal(s *Sector, minerID types.Address, publicParams *PublicParameters) (*SealedSector,
+func (sb *SectorBuilder) Seal(s *Sector, minerID types.Address, publicParams *PublicParameters) (*SealedSector,
 	error) {
 	// TODO: Actually use the mmap.ReaderAt for the sealing, instead of passing s.data.
 	/* defer s.OpenMMap().Close() // nolint: errcheck
@@ -328,7 +448,9 @@ func (smc *SectorBuilder) Seal(s *Sector, minerID types.Address, publicParams *P
 	prover := drg.NewProver(string(minerID[:]), publicParams.graph, int(publicParams.blockSize))
 	_, root := prover.Setup(s.data)
 
-	ss, err := smc.NewSealedSector(prover.Replica, root, s)
+	ss, err := sb.NewSealedSector(prover.Replica, root, s)
+	// NOTE: Between here and the call to os.Rename below, the SealedSector has a filename which is never persisted.
+	// FIXME: Does it need to be?
 	if err != nil {
 		return nil, err
 	}
@@ -336,15 +458,17 @@ func (smc *SectorBuilder) Seal(s *Sector, minerID types.Address, publicParams *P
 	if err != nil {
 		return ss, err
 	}
-	merklePath, err := smc.merkleFilepath(ss)
+	merklePath, err := sb.merkleFilepath(ss)
 	if err != nil {
 		return ss, err
 	}
 	err = os.Rename(ss.filename, merklePath)
 	if err == nil {
 		ss.filename = merklePath
+	} else {
+		return ss, err
 	}
-	return ss, err
+	return ss, nil
 }
 
 // ReadFile reads the content of a Sector's file into a byte array and returns it along with any error.
@@ -362,8 +486,8 @@ func (ss *SealedSector) Dump() error {
 	return ioutil.WriteFile(ss.filename, ss.replicaData, 0600)
 }
 
-// newSectorFileName returns a newly-generated, random 32-character base32 string.
-func newSectorFileName() string {
+// newSectorLabel returns a newly-generated, random 32-character base32 string.
+func newSectorLabel() string {
 	c := 20
 	b := make([]byte, c)
 	_, err := rand.Read(b)
@@ -375,24 +499,131 @@ func newSectorFileName() string {
 }
 
 // newSectorPath returns a path to a new (random) filename in the staging directory.
-func (smc *SectorBuilder) newSectorPath() string {
-	// FIXME: Lookup or create by ID in metadata -- Repo.Datastore.
-	return path.Join(smc.stagingDir, newSectorFileName())
+func (sb *SectorBuilder) newSectorPath() (pathname string, label string) {
+	label = newSectorLabel()
+	pathname = path.Join(sb.stagingDir, label)
+	return pathname, label
 }
 
 // newSealedSectorPath returns a path to a new (random) filename in the sealed directory.
-func (smc *SectorBuilder) newSealedSectorPath() string {
-	// FIXME: Lookup or create by ID in metadata -- Repo.Datastore.
-	return path.Join(smc.sealedDir, newSectorFileName())
+func (sb *SectorBuilder) newSealedSectorPath() (pathname string, label string) {
+	label = newSectorLabel()
+	pathname = path.Join(sb.sealedDir, label)
+	return pathname, label
 }
 
 // merkleFilepath returns a path in smc's sealed directory, with name derived from ss's merkleRoot.
-func (smc *SectorBuilder) merkleFilepath(ss *SealedSector) (string, error) {
+func (sb *SectorBuilder) merkleFilepath(ss *SealedSector) (string, error) {
 	if ss.merkleRoot == nil {
 		return "", errors.New("no merkleRoot")
 	}
-	merkleString := base32.StdEncoding.EncodeToString(ss.merkleRoot)
+	merkleString := merkleString(ss.merkleRoot)
 	merkleString = strings.Trim(merkleString, "=")
 
-	return path.Join(smc.sealedDir, merkleString), nil
+	return path.Join(sb.sealedDir, merkleString), nil
+}
+
+func merkleString(merkleRoot []byte) string {
+	return base32.StdEncoding.EncodeToString(merkleRoot)
+}
+
+func (sb *SectorBuilder) metadataKey(label string) ds.Key {
+	path := []string{"sectors", "metadata"}
+	return ds.KeyWithNamespaces(path).Instance(label)
+}
+
+func (sb *SectorBuilder) sealedMetadataKey(merkleRoot []byte) ds.Key {
+	path := []string{"sealedSectors", "metadata"}
+	return ds.KeyWithNamespaces(path).Instance(merkleString(merkleRoot))
+}
+
+func (sb *SectorBuilder) builderMetadataKey(minerAddress types.Address) ds.Key {
+	path := []string{"sectors", "metadata"}
+	return ds.KeyWithNamespaces(path).Instance(minerAddress.String())
+}
+
+// GetMeta returns SectorMetadata for sector labeled, label, and any error.
+func (sb *SectorBuilder) GetMeta(label string) (*SectorMetadata, error) {
+	key := sb.metadataKey(label)
+
+	data, err := sb.store.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	var m SectorMetadata
+	if err := cbor.DecodeInto(data.([]byte), &m); err != nil {
+		return nil, err
+	}
+	return &m, err
+}
+
+// GetSealedMeta returns SealedSectorMetadata for merkleRoot, and any error.
+func (sb *SectorBuilder) GetSealedMeta(merkleRoot []byte) (*SealedSectorMetadata, error) {
+	key := sb.sealedMetadataKey(merkleRoot)
+
+	data, err := sb.store.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	var m SealedSectorMetadata
+	if err := cbor.DecodeInto(data.([]byte), &m); err != nil {
+		return nil, err
+	}
+
+	return &m, err
+}
+
+// GetBuilderMeta returns SectorBuilderMetadata for SectorBuilder, sb, and any error.
+func (sb *SectorBuilder) GetBuilderMeta(minerAddress types.Address) (*SectorBuilderMetadata, error) {
+	key := sb.builderMetadataKey(minerAddress)
+
+	data, err := sb.store.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	var m SectorBuilderMetadata
+	if err := cbor.DecodeInto(data.([]byte), &m); err != nil {
+		return nil, err
+	}
+	return &m, err
+}
+
+func (sb *SectorBuilder) setMeta(label string, meta *SectorMetadata) error {
+	key := sb.metadataKey(label)
+	data, err := cbor.DumpObject(meta)
+	if err != nil {
+		return err
+	}
+	return sb.store.Put(key, data)
+}
+
+func (sb *SectorBuilder) setSealedMeta(merkleRoot []byte, meta *SealedSectorMetadata) error {
+	key := sb.sealedMetadataKey(merkleRoot)
+	data, err := cbor.DumpObject(meta)
+	if err != nil {
+		return err
+	}
+	return sb.store.Put(key, data)
+}
+
+func (sb *SectorBuilder) setBuilderMeta(minerAddress types.Address, meta *SectorBuilderMetadata) error {
+	key := sb.metadataKey(minerAddress.String())
+	data, err := cbor.DumpObject(meta)
+	if err != nil {
+		return err
+	}
+	return sb.store.Put(key, data)
+}
+
+// TODO: only actually set if changed.
+func (sb *SectorBuilder) checkpointSectorMeta(s *Sector) error {
+	return sb.setMeta(s.Label, s.SectorMetadata())
+}
+
+func (sb *SectorBuilder) checkpointSealedMeta(ss *SealedSector) error {
+	return sb.setSealedMeta(ss.merkleRoot, ss.SealedSectorMetadata())
+}
+
+func (sb *SectorBuilder) checkpointBuilderMeta() error {
+	return sb.setBuilderMeta(sb.MinerAddr, sb.SectorBuilderMetadata())
 }
--- node/sector_builder_test.go
+++ node/sector_builder_test.go
@@ -20,8 +20,8 @@ var sectorDirsForTest = &repo.MemRepo{}
 func TestWhatever(t *testing.T) {
 	require := require.New(t)
 	nd := MakeOfflineNode(t)
-	smc := requireSectorBuilder(require, nd, 50)
-	sector, err := smc.NewSector()
+	sb := requireSectorBuilder(require, nd, 50)
+	sector, err := sb.NewSector()
 	require.NoError(err)
 
 	d1Data := []byte("hello world")
@@ -35,7 +35,7 @@ func TestWhatever(t *testing.T) {
 	}
 
 	ag := types.NewAddressForTestGetter()
-	ss, err := smc.Seal(sector, ag(), filecoinParameters)
+	ss, err := sb.Seal(sector, ag(), filecoinParameters)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -67,39 +67,62 @@ func TestSectorBuilder(t *testing.T) {
 	require := require.New(t)
 	ctx := context.Background()
 
-	fname := newSectorFileName()
+	fname := newSectorLabel()
 	assert.Len(fname, 32) // Sanity check, nothing more.
 
 	nd := MakeOfflineNode(t)
 
-	smc := requireSectorBuilder(require, nd, 60)
+	sb := requireSectorBuilder(require, nd, 60)
 
-	// New paths are in the right places.
-	stagingPath := smc.newSectorPath()
-	sealedPath := smc.newSealedSectorPath()
-	assert.Contains(stagingPath, smc.stagingDir)
-	assert.Contains(sealedPath, smc.sealedDir)
+	assertMetadataMatch := func(sector *Sector, pieces int) {
+		meta := sector.SectorMetadata()
+		assert.Len(meta.Pieces, pieces)
 
-	// New paths are generated each time.
-	stpath2 := smc.newSectorPath()
-	sepath2 := smc.newSealedSectorPath()
-	assert.NotEqual(stagingPath, stpath2)
-	assert.NotEqual(sealedPath, sepath2)
+		// persisted and calculated metadata match.
+		metaPersisted, err := sb.GetMeta(sector.Label)
+		assert.NoError(err)
+		assert.Equal(metaPersisted, meta)
 
-	sector := smc.CurSector
-	assert.NotNil(sector.file)
-	assert.IsType(&os.File{}, sector.file)
+		sealed := sector.sealed
+		if sealed != nil {
+			sealedMeta := sealed.SealedSectorMetadata()
+			sealedMetaPersisted, err := sb.GetSealedMeta(sealed.merkleRoot)
+			assert.NoError(err)
+			assert.Equal(sealedMeta, sealedMetaPersisted)
+		}
+	}
 
 	requireAddPiece := func(s string) {
-		err := smc.AddPiece(ctx, requirePieceInfo(require, nd, []byte(s)))
+		err := sb.AddPiece(ctx, requirePieceInfo(require, nd, []byte(s)))
 		assert.NoError(err)
 
 	}
 
+	assertMetadataMatch(sb.CurSector, 0)
+
+	// New paths are in the right places.
+	stagingPath, _ := sb.newSectorPath()
+	sealedPath, _ := sb.newSealedSectorPath()
+	assert.Contains(stagingPath, sb.stagingDir)
+	assert.Contains(sealedPath, sb.sealedDir)
+
+	// New paths are generated each time.
+	stpath2, _ := sb.newSectorPath()
+	sepath2, _ := sb.newSealedSectorPath()
+	assert.NotEqual(stagingPath, stpath2)
+	assert.NotEqual(sealedPath, sepath2)
+
+	sector := sb.CurSector
+	assert.NotNil(sector.file)
+	assert.IsType(&os.File{}, sector.file)
+
+	assertMetadataMatch(sb.CurSector, 0)
 	text := "What's our vector, sector?" // len(text) = 26
 	requireAddPiece(text)
-	assert.Equal(sector, smc.CurSector)
+	assert.Equal(sector, sb.CurSector)
 	all := text
+
+	assertMetadataMatch(sector, 1)
 
 	d := requireReadAll(require, sector)
 	assert.Equal(all, string(d))
@@ -107,7 +130,7 @@ func TestSectorBuilder(t *testing.T) {
 
 	text2 := "We have clearance, Clarence." // len(text2) = 28
 	requireAddPiece(text2)
-	assert.Equal(sector, smc.CurSector)
+	assert.Equal(sector, sb.CurSector)
 	all += text2
 
 	d2 := requireReadAll(require, sector)
@@ -116,18 +139,25 @@ func TestSectorBuilder(t *testing.T) {
 
 	assert.NotContains(string(sector.data), string(d2)) // Document behavior: data only set at sealing time.
 
+	// persisted and calculated metadata match.
+	assertMetadataMatch(sector, 2)
+
 	text3 := "I'm too sexy for this sector." // len(text3) = 29
 	requireAddPiece(text3)
 	time.Sleep(100 * time.Millisecond) // Wait for sealing to finish: FIXME, don't sleep.
-	assert.NotEqual(sector, smc.CurSector)
+	assert.NotEqual(sector, sb.CurSector)
 
 	d3 := requireReadAll(require, sector)
 	assert.Equal(all, string(d3)) // Initial sector still contains initial data.
 
 	assert.Contains(string(sector.data), string(d3)) // Sector data has been set. 'Contains' because it is padded.
 
-	newSector := smc.CurSector
+	// persisted and calculated metadata match after a sector is sealed.
+	assertMetadataMatch(sector, 2)
+
+	newSector := sb.CurSector
 	d4 := requireReadAll(require, newSector)
+	assertMetadataMatch(newSector, 1)
 
 	assert.Equal(text3, d4)
 	sealed := sector.sealed
@@ -139,9 +169,42 @@ func TestSectorBuilder(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(sealed.replicaData, sealedData)
 
+	meta := sb.CurSector.SectorMetadata()
+	assert.Len(meta.Pieces, 1)
+	assert.Equal(uint64(60), meta.Size)
+	assert.Equal(60-len(text3), int(meta.Free))
+
 	text4 := "I am text, and I am long. My reach exceeds my grasp exceeds exceeds my allotted space."
-	err = smc.AddPiece(ctx, requirePieceInfo(require, nd, []byte(text4)))
+	err = sb.AddPiece(ctx, requirePieceInfo(require, nd, []byte(text4)))
 	assert.EqualError(err, ErrPieceTooLarge.Error())
+}
+
+func TestSectorBuilderMetadata(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	//ctx := context.Background()
+
+	fname := newSectorLabel()
+	assert.Len(fname, 32) // Sanity check, nothing more.
+
+	nd := MakeOfflineNode(t)
+
+	sb := requireSectorBuilder(require, nd, 60)
+
+	label := "SECTORFILENAMEWHATEVER"
+
+	k := sb.metadataKey(label).String()
+	// Don't accidentally test Datastore namespacing implementation.
+	assert.Contains(k, "sectors")
+	assert.Contains(k, "metadata")
+	assert.Contains(k, label)
+
+	merkleRoot := ([]byte)("someMerkleRootLOL")
+	k2 := sb.sealedMetadataKey(merkleRoot).String()
+	// Don't accidentally test Datastore namespacing implementation.
+	assert.Contains(k2, "sealedSectors")
+	assert.Contains(k2, "metadata")
+	assert.Contains(k2, merkleString(merkleRoot))
 }
 
 func requireReadAll(require *require.Assertions, sector *Sector) string {
