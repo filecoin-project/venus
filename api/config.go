ffb467649c3fc559332d032bd2aaf14105921089
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-08-08 13:17:32 +0200

    refactor(api): move config

--- api/config.go
+++ api/config.go
@@ -1,3 +1,9 @@
 package api
 
-type Config interface{}
+type Config interface {
+	// Get, returns the configuration value for the passed in key.
+	Get(key string) (interface{}, error)
+	// Set, sets the configuration value for the passed in key, to the given value.
+	// Returns the newly set value on success.
+	Set(key, value string) (interface{}, error)
+}
---
+++ api_impl/config_test.go
@@ -0,0 +1,110 @@
+package api_impl
+
+import (
+	"testing"
+
+	"github.com/filecoin-project/go-filecoin/config"
+	"github.com/filecoin-project/go-filecoin/node"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigGet(t *testing.T) {
+	t.Parallel()
+	t.Run("emits the referenced config value", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+		require := require.New(t)
+
+		n := node.MakeNodesUnstarted(t, 1, true, true)[0]
+
+		api := New(n)
+
+		out, err := api.Config().Get("bootstrap")
+
+		require.NoError(err)
+		expected := config.NewDefaultConfig().Bootstrap
+		assert.Equal(expected, out)
+	})
+
+	t.Run("failure cases fail", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+		n := node.MakeNodesUnstarted(t, 1, true, true)[0]
+		api := New(n)
+
+		_, err := api.Config().Get("nonexistantkey")
+		assert.EqualError(err, "key: nonexistantkey invalid for config")
+
+		_, err = api.Config().Get("bootstrap.nope")
+		assert.EqualError(err, "key: bootstrap.nope invalid for config")
+
+		_, err = api.Config().Get(".inval.id-key")
+		assert.EqualError(err, "key: .inval.id-key invalid for config")
+	})
+}
+
+func TestConfigSet(t *testing.T) {
+	t.Parallel()
+	t.Run("sets the config value", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+		require := require.New(t)
+
+		defaultCfg := config.NewDefaultConfig()
+
+		n := node.MakeNodesUnstarted(t, 1, true, true, func(c *node.Config) error {
+			c.Repo.Config().API.Address = defaultCfg.API.Address
+			return nil
+		})[0]
+		api := New(n)
+		tomlBlob := `{addresses = ["bootup1", "bootup2"]}  `
+
+		out, err := api.Config().Set("bootstrap", tomlBlob)
+		require.NoError(err)
+
+		// validate output
+		expected := config.NewDefaultConfig().Bootstrap
+		expected.Addresses = []string{"bootup1", "bootup2"}
+		assert.Equal(expected, out)
+
+		// validate config write
+		cfg := n.Repo.Config()
+		defaultCfg.Mining.RewardAddress = n.RewardAddress()
+		assert.Equal(expected, cfg.Bootstrap)
+		assert.Equal(defaultCfg.API, cfg.API)
+		assert.Equal(defaultCfg.Datastore, cfg.Datastore)
+		assert.Equal(defaultCfg.Mining, cfg.Mining)
+		assert.Equal(defaultCfg.Swarm, cfg.Swarm)
+	})
+
+	t.Run("failure cases fail", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		n := node.MakeNodesUnstarted(t, 1, true, true)[0]
+		api := New(n)
+
+		// bad key
+		tomlBlob := `{addresses = ["bootup1", "bootup2"]}  `
+
+		_, err := api.Config().Set("botstrap", tomlBlob)
+		assert.EqualError(err, "key: botstrap invalid for config")
+
+		// bad value type (bootstrap is a struct not a list)
+		tomlBlobBadType := `["bootup1", "bootup2"]`
+		_, err = api.Config().Set("bootstrap", tomlBlobBadType)
+		assert.EqualError(err, "input could not be marshaled to sub-config at: bootstrap: Near line 2 (last key parsed 'bootstrap'): expected '.' or ']' to end table name, but got ',' instead")
+
+		// bad TOML
+		tomlBlobInvalid := `{addresses =[""bootup1", "bootup2"]`
+		_, err = api.Config().Set("bootstrap", tomlBlobInvalid)
+		assert.EqualError(err, "input could not be marshaled to sub-config at: bootstrap: Near line 1 (last key parsed 'bootstrap.addresses'): expected a comma or array terminator ']', but got 'b' instead")
+
+		// bad address
+		tomlBlobBadAddr := `"fcqnyc0muxjajygqavu645m8ja04vckk2kcorrupt"`
+		_, err = api.Config().Set("mining.rewardAddress", tomlBlobBadAddr)
+		assert.EqualError(err, "input could not be marshaled to sub-config at: mining.rewardAddress: invalid character")
+	})
+}
