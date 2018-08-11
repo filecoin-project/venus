22708adb39ed3f3ac8cc259595537e2285660166
whyrusleeping <why@ipfs.io>
2018-08-10 17:40:32 -0700

    gengen binary comment

--- gengen/main.go
+++ gengen/main.go
@@ -17,6 +17,11 @@ func writeKey(ki *types.KeyInfo, name string) error {
 	return json.NewEncoder(fi).Encode(ki)
 }
 
+// gengen takes as input a json encoded 'Genesis Config'
+// It outputs a 'car' encoded genesis dag.
+// The outputed file can be used by go-filecoin during init to
+// set the initial genesis block:
+// $ go-filecoin init --genesisfile=genesis.car
 func main() {
 	var cfg gengen.GenesisCfg
 	if err := json.NewDecoder(os.Stdin).Decode(&cfg); err != nil {
