ae4129d4c7e9866c31a9a37ed9b3ba3723249ce7
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-08-24 20:06:16 +0200

    fix(config): use the correct port for default origins
    otherwise the filecoin explorer is broken out of the box

--- repo/fsrepo_test.go
+++ repo/fsrepo_test.go
@@ -18,7 +18,7 @@ import (
 const (
 	expectContent = `[api]
   address = "/ip4/127.0.0.1/tcp/3453"
-  accessControlAllowOrigin = ["http://localhost", "https://localhost", "http://127.0.0.1", "https://127.0.0.1"]
+  accessControlAllowOrigin = ["http://localhost:8080", "https://localhost:8080", "http://127.0.0.1:8080", "https://127.0.0.1:8080"]
   accessControlAllowCredentials = false
   accessControlAllowMethods = ["GET", "POST", "PUT"]
 
