b3cc05ddea4a1051fe9070ab1be2f44d565ce57b
gmasgras <gmasgras@users.noreply.github.com>
2018-09-07 10:51:46 -0700

    add ECR repos for filebeat and logstash images

--- terraform/providers/aws/us-east-1/ecr.tf
+++ terraform/providers/aws/us-east-1/ecr.tf
@@ -2,6 +2,21 @@ resource "aws_ecr_repository" "filecoin" {
   name = "filecoin"
 }
 
+resource "aws_ecr_repository" "filebeat" {
+  name = "filebeat"
+}
+
+resource "aws_ecr_repository" "logstash" {
+  name = "logstash"
+}
+
+
 output "ecr-filecoin-url" {
-  value = "${aws_ecr_repository.filecoin.repository_url}"  
+  value = "${aws_ecr_repository.filecoin.repository_url}"
+}
+output "ecr-filebeat-url" {
+  value = "${aws_ecr_repository.filebeat.repository_url}"
+}
+output "ecr-logstash-url" {
+  value = "${aws_ecr_repository.logstash.repository_url}"
 }
