da8044853f7c4d47446a29b9d67447bcefceb0a4
gmasgras <gmasgras@users.noreply.github.com>
2018-09-27 16:35:58 -0700

    add script for fetching peer IDs via SSH. formatting.

--- terraform/modules/aws/ec2/main.tf
+++ terraform/modules/aws/ec2/main.tf
@@ -2,25 +2,35 @@ variable "instance_name" {}
 variable "public_key_name" {}
 variable "vpc_id" {}
 variable "subnet_id" {}
+
 variable "docker_uri" {
   default = "657871693752.dkr.ecr.us-east-1.amazonaws.com/filecoin"
 }
+
 variable "docker_tag" {
   default = "latest"
 }
+
 variable "filebeat_docker_uri" {
   default = "657871693752.dkr.ecr.us-east-1.amazonaws.com/filebeat"
 }
+
 variable "filebeat_docker_tag" {
   default = "latest"
 }
-variable "vpc_security_group_ids" { type = "list" }
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
+
 variable "iam_instance_profile_name" {}
 variable "route53_zone_id" {}
 variable "route53_zone_name" {}
+
 variable "logstash_hosts" {
   default = "172.17.0.1:5044" # comma separated
 }
+
 variable "instance_type" {
   default = "m5d.large"
 }
@@ -58,8 +68,37 @@ resource "aws_instance" "this" {
   }
 
   tags {
-    Name     = "${var.instance_name}"
-    metrics  = "true"
+    Name    = "${var.instance_name}"
+    metrics = "true"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.peers.rendered}"
+    destination = "/home/ubuntu/peers.sh"
+
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/peers.sh",
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+    }
+  }
+}
+
+data "template_file" "peers" {
+  template = "${file("${path.module}/scripts/peers.sh")}"
+
+  vars {
+    instance_name = "${var.instance_name}"
   }
 }
 
@@ -67,15 +106,15 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/scripts/docker_user_data.sh")}"
 
   vars {
-    node_exporter_install = "${data.template_file.node_exporter_install.rendered}"
-    cadvisor_install = "${data.template_file.cadvisor_install.rendered}"
-    docker_install = "${data.template_file.docker_install.rendered}"
+    node_exporter_install  = "${data.template_file.node_exporter_install.rendered}"
+    cadvisor_install       = "${data.template_file.cadvisor_install.rendered}"
+    docker_install         = "${data.template_file.docker_install.rendered}"
     setup_instance_storage = "${data.template_file.setup_instance_storage.rendered}"
-    docker_uri = "${var.docker_uri}"
-    docker_tag = "${var.docker_tag}"
-    filebeat_docker_uri = "${var.filebeat_docker_uri}"
-    filebeat_docker_tag = "${var.filebeat_docker_tag}"
-    logstash_hosts = "${var.logstash_hosts}"
+    docker_uri             = "${var.docker_uri}"
+    docker_tag             = "${var.docker_tag}"
+    filebeat_docker_uri    = "${var.filebeat_docker_uri}"
+    filebeat_docker_tag    = "${var.filebeat_docker_tag}"
+    logstash_hosts         = "${var.logstash_hosts}"
   }
 }
 
@@ -90,6 +129,7 @@ resource "aws_route53_record" "this" {
 output "instance_public_ip" {
   value = "${aws_instance.this.public_ip}"
 }
+
 output "instance_dns" {
   value = "${aws_route53_record.this.fqdn}"
 }
