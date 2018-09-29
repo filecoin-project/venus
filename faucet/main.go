cae91acc81804b6adc046b19c09db7d0a42c32cc
frrist <forrest@protocol.ai>
2018-09-29 16:00:23 -0700

    add html form, logging for requests, from address

--- faucet/main.go
+++ faucet/main.go
@@ -8,40 +8,63 @@ import (
 	"net/http"
 
 	"github.com/filecoin-project/go-filecoin/address"
+	logging "gx/ipfs/QmRREK2CAZ5Re2Bd9zZFG6FeYDppUWt5cMgsoUEp3ktgSr/go-log"
 	"gx/ipfs/QmZFbDTY9jfSBms2MchvYM9oYRbAF19K7Pby47yDBfpPrb/go-cid"
 )
 
+var log = logging.Logger("faucet")
+
+func init() {
+	// Info level
+	logging.SetAllLoggers(4)
+}
+
 func main() {
 	filapi := flag.String("fil-api", "localhost:3453", "set the api address of the filecoin node to use")
+	filwal := flag.String("fil-wallet", "", "(required) set the wallet address for the controlled filecoin node to send funds from")
 	faucetval := flag.Int64("faucet-val", 500, "set the amount of fil to pay to each requester")
 	flag.Parse()
 
+	if *filwal == "" {
+		fmt.Println("ERROR: must provide wallet address to send funds from")
+		flag.Usage()
+		return
+	}
+
+	http.HandleFunc("/", displayForm)
 	http.HandleFunc("/tap", func(w http.ResponseWriter, r *http.Request) {
 		target := r.FormValue("target")
 		if target == "" {
 			http.Error(w, "must specify a target address to send FIL to", 400)
 			return
 		}
+		log.Infof("Request to send funds to: %s", target)
 
 		addr, err := address.NewFromString(target)
 		if err != nil {
-			http.Error(w, err.Error(), 400)
+			log.Errorf("Failed to parse target address: %s %s", target, err)
+			http.Error(w, fmt.Sprintf("Failed to parse target address %s %s", target, err.Error()), 400)
 			return
 		}
 
-		resp, err := http.Post(fmt.Sprintf("http://%s/api/message/send?arg=%s&value=%d", *filapi, addr.String(), *faucetval), "application/json", nil)
+		reqStr := fmt.Sprintf("http://%s/api/message/send?arg=%s&value=%d&from=%s", *filapi, addr.String(), *faucetval, *filwal)
+		log.Infof("Request URL: %s", reqStr)
+
+		resp, err := http.Post(reqStr, "application/json", nil)
 		if err != nil {
+			log.Errorf("Failed to Post request. Status: %s Error: %s", resp.Status, err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
 
 		out, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			fmt.Println("failed to read body: ", err)
+			log.Errorf("failed to read response body: %s", err)
+			http.Error(w, "failed to read response", 500)
 			return
 		}
 		if resp.StatusCode != 200 {
-			fmt.Println("Failed request: ", string(out))
+			log.Errorf("Status: %s Body: %s", resp.Status, string(out))
 			http.Error(w, "failed to send funds", 500)
 			return
 		}
@@ -49,15 +72,36 @@ func main() {
 		// result should be a message cid
 		var msgcid cid.Cid
 		if err := json.Unmarshal(out, &msgcid); err != nil {
-			fmt.Println("json unmarshal from command failed: ", err)
-			fmt.Println("data was: ", string(out))
+			log.Errorf("json unmarshal from response failed: %s", err)
+			log.Errorf("response data was: %s", string(out))
 			http.Error(w, "faucet unmarshal failed", 500)
 			return
 		}
 
+		log.Info("Request successful. Message CID: %s", msgcid.String())
 		w.WriteHeader(200)
-		fmt.Fprintln(w, msgcid.String())
+		fmt.Fprint(w, "Success! Message CID: ") // nolint: errcheck
+		fmt.Fprintln(w, msgcid.String())        // nolint: errcheck
 	})
 
 	panic(http.ListenAndServe(":9797", nil))
+}
+
+const form = `
+<html>
+	<body>
+		<h1> What is your wallet address </h1>
+		<p> You can find this by running: </p>
+		<tt> go-filecoin wallet addrs ls </tt>
+		<p> Address: </p>
+		<form action="/tap" method="post">
+			<input type="text" name="target" size="30" />
+			<input type="submit" value="Submit" size="30" />
+		</form>
+	</body>
+</html>
+`
+
+func displayForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, form) // nolint: errcheck
 }
