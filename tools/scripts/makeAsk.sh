46408cd7b17c55fea4d8916766824dc773d8e201
frrist <forrest@protocol.ai>
2018-10-02 18:36:27 -0700

    add message wait and timeout

--- tools/scripts/makeAsk.sh
+++ tools/scripts/makeAsk.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# ONLY TESTED ON LINUX, OSX UNTESTED (probably missing timeout command)
 # This script can be used to created asks on the filecoin network,
 # it accepts 2 required arguments, a nodes repo dir and miner address to create asks with,
-# it accepts 2 optional arguments, an ask size in bytes (default 1GB) and ask price,
-# if a price is not provided a random value between 1 and 11 will be picked.
+# it accepts 3 optional arguments, an ask size in bytes (default 1GB) an ask price,
+# if a price is not provided a random value between 1 and 11 will be picked, and a
+# timeout value for ask message propogation, if no timeout is given then will default to 1min.
 
 function Usage() {
   echo "USAGE:"
   echo
-  echo "makeAsk <REPO_DIR> <MINER_ADDRESS> [ASK_SIZE] [ASK_PRICE]"
+  echo "makeAsk <REPO_DIR> <MINER_ADDRESS> [ASK_SIZE] [ASK_PRICE] [MSG_TIMEOUT]"
 }
 
 # Required
@@ -18,6 +20,8 @@ MINER_ADDRESS="$2"
 # Optional
 ASK_SIZE="$3"
 ASK_PRICE="$4"
+# ask message propogation timeout in seconds
+MSG_TIMEOUT="$5"
 
 if test -z "$REPO_DIR"
 then
@@ -56,7 +60,25 @@ then
   echo
 fi
 
-printf "Creating ask on node: %s, miner: %s, owner: %s, size: %s, price: %s\n" "$REPO_DIR" "$MINER_ADDRESS" "$MINER_OWNER" "$ASK_SIZE" "$ASK_PRICE"
+if test -z "$MSG_TIMEOUT"
+then
+  MSG_TIMEOUT=60
+  printf "No timout given, will use default value: %s second\n" "$MSG_TIMEOUT"
   echo
+fi
 
-go-filecoin --repodir="$REPO_DIR" miner add-ask "$MINER_ADDRESS" "$ASK_SIZE" "$ASK_PRICE" --from="$MINER_OWNER"
+printf "Creating ask on node: %s, miner: %s, owner: %s, size: %s, price: %s\n" "$REPO_DIR" "$MINER_ADDRESS" "$MINER_OWNER" "$ASK_SIZE" "$ASK_PRICE"
+echo
+
+MSG_CID=$(go-filecoin --repodir="$REPO_DIR" miner add-ask "$MINER_ADDRESS" "$ASK_SIZE" "$ASK_PRICE" --from="$MINER_OWNER")
+
+MSG_RESULT=$(timeout $MSG_TIMEOUT go-filecoin --repodir="$REPO_DIR" message wait $MSG_CID)
+
+EXIT_CODE=$(echo $MSG_RESULT | jq ".exitCode" | grep -v "null")
+
+if [ "$EXIT_CODE" -eq "0" ]; then
+   printf "Ask create success\n";
+   exit;
+else
+  printf "Ask create failed. Message:\n%s" "$MSG_RESULT"
+fi
