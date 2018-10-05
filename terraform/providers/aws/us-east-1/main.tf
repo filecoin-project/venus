f7e3342a780fd6b0d75590ecff3c7305f6e81e4b
gmasgras <gmasgras@users.noreply.github.com>
2018-10-05 15:11:46 -0700

    open port for faucet, aggregator collector. add 2nd test box

--- terraform/providers/aws/us-east-1/main.tf
+++ terraform/providers/aws/us-east-1/main.tf
@@ -45,10 +45,26 @@ resource "aws_security_group" "filecoin" {
 
   ingress {
     protocol    = "tcp"
+    from_port   = 19000
+    to_port     = 19000
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "dashboard aggregator collector"
+  }
+
+  ingress {
+    protocol    = "tcp"
     from_port   = 9080
     to_port     = 9080
     cidr_blocks = ["0.0.0.0/0"]
     description = "dashboard aggregator Websocket"
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 9797
+    to_port     = 9797
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "faucet"
   }
 
   ingress {
@@ -100,7 +116,7 @@ output "filecoin-cluster-dns" {
 module "kh-test" {
   source = "../../../modules/aws/ec2/"
 
-  docker_tag      = "982ddb"
+  docker_tag      = "${var.docker_tag}"
   instance_type   = "c5d.4xlarge"
   instance_name   = "test"
   public_key_name = "${aws_key_pair.filecoin.key_name}"
@@ -119,6 +135,32 @@ module "kh-test" {
   logstash_hosts            = "${aws_route53_record.logstash_nlb.fqdn}"
 }
 
+
+# kh2 test instance
+module "kh-test-2" {
+  source = "../../../modules/aws/ec2/"
+
+  docker_tag      = "${var.docker_tag}"
+  instance_type   = "c5d.4xlarge"
+  instance_name   = "test2"
+  public_key_name = "${aws_key_pair.filecoin.key_name}"
+  vpc_id          = "${module.vpc.vpc_id}"
+  subnet_id       = "${element(module.vpc.public_subnets, 1)}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.filecoin.id}",
+    "${aws_security_group.cadvisor.id}",
+    "${aws_security_group.node_exporter.id}",
+  ]
+
+  iam_instance_profile_name = "${aws_iam_instance_profile.filecoin_kittyhawk.name}"
+  route53_zone_name         = "${aws_route53_zone.kittyhawk.name}"
+  route53_zone_id           = "${aws_route53_zone.kittyhawk.zone_id}"
+  logstash_hosts            = "${aws_route53_record.logstash_nlb.fqdn}"
+}
+
+
+
 output "kh-test-public_ip" {
   value = "${module.kh-test.instance_public_ip}"
 }
@@ -130,4 +172,3 @@ output "kh-test-dns" {
 # output "kh-test-user_data" {
 #   value = "${module.kh-test.user_data}"
 # }
-
