filecoin-project/venus

commit 98926d4aa3fba8e8370d83f440b6ff7e06949324

fix race in TestStorageProtocolBasic (#1240)
Closes #1230 and #968

protocol/storage/storage_protocol_test.go
@@ -84,23 +84,25 @@ func TestStorageProtocolBasic(t *testing.T) {
 
 	assert.NoError(client.BlockService().AddBlock(protonode))
 
-	var foundSeal bool
+	var foundCommit bool
 	var foundPoSt bool
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	// TODO: remove this hack to get new blocks
 	old := miner.AddNewlyMinedBlock
+	var bCount, mCount int
 	miner.AddNewlyMinedBlock = func(ctx context.Context, blk *types.Block) {
+		bCount++
+		mCount += len(blk.Messages)
 		old(ctx, blk)
 
-		if !foundSeal {
+		if !foundCommit {
 			for i, msg := range blk.Messages {
 				if msg.Message.Method == "commitSector" {
-					assert.False(foundSeal, "multiple commitSector submissions must not happen")
+					assert.False(foundCommit, "multiple commitSector submissions must not happen")
 					assert.Equal(uint8(0), blk.MessageReceipts[i].ExitCode, "seal submission failed")
-					foundSeal = true
+					foundCommit = true
 					wg.Done()
 				}
 			}
@@ -119,14 +121,18 @@ func TestStorageProtocolBasic(t *testing.T) {
 
 	ref, err := c.ProposeDeal(ctx, mineraddr, protonode.Cid(), 10, types.NewAttoFILFromFIL(60))
 	assert.NoError(err)
+	requireQueryDeal := func() (DealState, string) {
+		resp, err := c.QueryDeal(ctx, ref.Proposal)
+		require.NoError(err)
+		return resp.State, resp.Message
+	}
 
 	time.Sleep(time.Millisecond * 100) // Bad dignifiedquire, bad!
 	var done bool
 	for i := 0; i < 5; i++ {
-		resp, err := c.QueryDeal(ctx, ref.Proposal)
-		assert.NoError(err)
-		assert.NotEqual(Failed, resp.State, resp.Message)
-		if resp.State == Staged {
+		state, message := requireQueryDeal()
+		assert.NotEqual(Failed, state, message)
+		if state == Staged {
 			done = true
 			break
 		}
@@ -134,8 +140,12 @@ func TestStorageProtocolBasic(t *testing.T) {
 	}
 
 	require.True(done)
-	require.False(waitTimeout(&wg, 120*time.Second), "waiting for submission timed out")
-	require.True(foundSeal, "no commitSector on chain")
+	if waitTimeout(&wg, 120*time.Second) {
+		state, message := requireQueryDeal()
+		require.NotEqual(Failed, state, message)
+		assert.Fail("waiting for submission timed out. Saw %d blocks with %d messages while waiting", bCount, mCount)
+	}
+	require.True(foundCommit, "no commitSector on chain")
 	require.True(foundPoSt, "no submitPoSt on chain")
 
 	// Now all things should be ready

protocol/storage/miner.go
@@ -47,10 +47,7 @@ type Miner struct {
 	postInProcessLk sync.Mutex
 	postInProcess   *types.BlockHeight
 
-	// dealsAwaitingSeal is a map from sector ID to a list of cids, with
-	// each cid identifying a StorageDealProposal.
-	dealsAwaitingSeal   map[uint64][]*cid.Cid
-	dealsAwaitingSealLk sync.Mutex
+	dealsAwaitingSeal *dealsAwaitingSealStruct
 
 	node node
 }
@@ -79,12 +76,15 @@ type node interface {
 // NewMiner is
 func NewMiner(ctx context.Context, minerAddr, minerOwnerAddr address.Address, nd node) (*Miner, error) {
 	sm := &Miner{
-		minerAddr:         minerAddr,
-		minerOwnerAddr:    minerOwnerAddr,
-		deals:             make(map[string]*storageDealState),
-		dealsAwaitingSeal: make(map[uint64][]*cid.Cid),
-		node:              nd,
+		minerAddr:      minerAddr,
+		minerOwnerAddr: minerOwnerAddr,
+		deals:          make(map[string]*storageDealState),
+		node:           nd,
 	}
+	sm.dealsAwaitingSeal = newDealsAwaitingSeal()
+	sm.dealsAwaitingSeal.onSuccess = sm.onCommitSuccess
+	sm.dealsAwaitingSeal.onFail = sm.onCommitFail
+
 	nd.Host().SetStreamHandler(makeDealProtocol, sm.handleMakeDeal)
 	nd.Host().SetStreamHandler(queryDealProtocol, sm.handleQueryDeal)
 
@@ -211,71 +211,140 @@ func (sm *Miner) processStorageDeal(c *cid.Cid) {
 		Size: d.proposal.Size.Uint64(),
 	}
 
+	// There is a race here that requires us to use dealsAwaitingSeal below. If the
+	// sector gets sealed and OnCommitmentAddedToChain is called right after
+	// AddPiece returns but before we record the sector/deal mapping we might
+	// miss it. Hence, dealsAwaitingSealStruct. I'm told that sealing in practice is
+	// so slow that the race only exists in tests, but tests were flaky so
+	// we fixed it with dealsAwaitingSealStruct.
+	//
+	// Also, this pattern of not being able to set up book-keeping ahead of
+	// the call is inelegant.
 	sectorID, err := sm.node.SectorBuilder().AddPiece(ctx, pi)
 	if err != nil {
 		fail("Failed to submit seal proof", fmt.Sprintf("failed to add piece: %s", err))
 		return
 	}
 
-	// TODO: handle race between OnCommitmentAddedToChain and AddPiece returning
-	// https://github.com/filecoin-project/go-filecoin/issues/968
-
-	sm.dealsAwaitingSealLk.Lock()
-	deals, ok := sm.dealsAwaitingSeal[sectorID]
-	if ok {
-		sm.dealsAwaitingSeal[sectorID] = append(deals, c)
-	} else {
-		sm.dealsAwaitingSeal[sectorID] = []*cid.Cid{c}
-	}
-	sm.dealsAwaitingSealLk.Unlock()
-
 	sm.updateDealState(c, func(resp *DealResponse) {
 		resp.State = Staged
 	})
+
+	// Careful: this might update state to success or failure so it should go after
+	// updating state to Staged.
+	sm.dealsAwaitingSeal.add(sectorID, c)
+}
+
+// dealsAwaitingSealStruct is a container for keeping track of which sectors have
+// pieces from which deals. We need it to accommodate a race condition where
+// a sector commit message is added to chain before we can add the sector/deal
+// book-keeping. It effectively caches success and failure results for sectors
+// for tardy add() calls.
+type dealsAwaitingSealStruct struct {
+	l sync.Mutex
+	// Maps from sector id to the deal cids with pieces in the sector.
+	sectorsToDeals map[uint64][]*cid.Cid
+	// Maps from sector id to sector.
+	successfulSectors map[uint64]*sectorbuilder.SealedSector
+	// Maps from sector id to seal failure error string.
+	failedSectors map[uint64]string
+
+	onSuccess func(dealCid *cid.Cid, sector *sectorbuilder.SealedSector)
+	onFail    func(dealCid *cid.Cid, message string)
+}
+
+func newDealsAwaitingSeal() *dealsAwaitingSealStruct {
+	return &dealsAwaitingSealStruct{
+		sectorsToDeals:    make(map[uint64][]*cid.Cid),
+		successfulSectors: make(map[uint64]*sectorbuilder.SealedSector),
+		failedSectors:     make(map[uint64]string),
+	}
+}
+
+func (dealsAwaitingSeal *dealsAwaitingSealStruct) add(sectorID uint64, dealCid *cid.Cid) {
+	dealsAwaitingSeal.l.Lock()
+	defer dealsAwaitingSeal.l.Unlock()
+
+	if sector, ok := dealsAwaitingSeal.successfulSectors[sectorID]; ok {
+		dealsAwaitingSeal.onSuccess(dealCid, sector)
+		// Don't keep references to sectors around forever. Assume that at most
+		// one success-before-add call will happen (eg, in a test). Sector sealing
+		// outside of tests is so slow that it shouldn't happen in practice.
+		// So now that it has happened once, clean it up. If we wanted to keep
+		// the state around for longer for some reason we need to limit how many
+		// sectors we hang onto, eg keep a fixed-length slice of successes
+		// and failures and shift the oldest off and the newest on.
+		delete(dealsAwaitingSeal.successfulSectors, sectorID)
+	} else if message, ok := dealsAwaitingSeal.failedSectors[sectorID]; ok {
+		dealsAwaitingSeal.onFail(dealCid, message)
+		// Same as above.
+		delete(dealsAwaitingSeal.failedSectors, sectorID)
+	} else {
+		deals, ok := dealsAwaitingSeal.sectorsToDeals[sectorID]
+		if ok {
+			dealsAwaitingSeal.sectorsToDeals[sectorID] = append(deals, dealCid)
+		} else {
+			dealsAwaitingSeal.sectorsToDeals[sectorID] = []*cid.Cid{dealCid}
+		}
+	}
+}
+
+func (dealsAwaitingSeal *dealsAwaitingSealStruct) success(sector *sectorbuilder.SealedSector) {
+	dealsAwaitingSeal.l.Lock()
+	defer dealsAwaitingSeal.l.Unlock()
+
+	dealsAwaitingSeal.successfulSectors[sector.SectorID] = sector
+
+	for _, dealCid := range dealsAwaitingSeal.sectorsToDeals[sector.SectorID] {
+		dealsAwaitingSeal.onSuccess(dealCid, sector)
+	}
+	delete(dealsAwaitingSeal.sectorsToDeals, sector.SectorID)
+}
+
+func (dealsAwaitingSeal *dealsAwaitingSealStruct) fail(sectorID uint64, message string) {
+	dealsAwaitingSeal.l.Lock()
+	defer dealsAwaitingSeal.l.Unlock()
+
+	dealsAwaitingSeal.failedSectors[sectorID] = message
+
+	for _, dealCid := range dealsAwaitingSeal.sectorsToDeals[sectorID] {
+		dealsAwaitingSeal.onFail(dealCid, message)
+	}
+	delete(dealsAwaitingSeal.sectorsToDeals, sectorID)
 }
 
 // OnCommitmentAddedToChain is a callback, called when a sector seal message was posted to the chain.
 func (sm *Miner) OnCommitmentAddedToChain(sector *sectorbuilder.SealedSector, err error) {
 	sectorID := sector.SectorID
 	log.Debug("Miner.OnCommitmentAddedToChain")
-	sm.dealsAwaitingSealLk.Lock()
-	defer sm.dealsAwaitingSealLk.Unlock()
-	deals, ok := sm.dealsAwaitingSeal[sectorID]
-	if !ok {
-		// nothing to do
-		return
-	}
-
-	// remove the deals
-	// TODO: reevaluate if this should be done inside the loops below
-	sm.dealsAwaitingSeal[sectorID] = nil
 
 	if err != nil {
 		// we failed to seal this sector, cancel all the deals
-		log.Errorf("failed sealing sector: %v: %s", sectorID, err)
-		for _, c := range deals {
-			go func(c *cid.Cid) {
-				sm.updateDealState(c, func(resp *DealResponse) {
-					resp.Message = "Failed to seal sector"
-					resp.State = Failed
-				})
-			}(c)
-		}
-
+		errMsg := fmt.Sprintf("failed sealing sector: %v: %s; canceling all outstanding deals", sectorID, err)
+		log.Errorf(errMsg)
+		sm.dealsAwaitingSeal.fail(sector.SectorID, errMsg)
 		return
 	}
 
-	for _, c := range deals {
-		// Success, our seal is posted on chain
-		sm.updateDealState(c, func(resp *DealResponse) {
-			resp.State = Posted
-			resp.ProofInfo = &ProofInfo{
-				SectorID: sectorID,
-				CommR:    sector.CommR[:],
-				CommD:    sector.CommD[:],
-			}
-		})
-	}
+	sm.dealsAwaitingSeal.success(sector)
+}
+
+func (sm *Miner) onCommitSuccess(dealCid *cid.Cid, sector *sectorbuilder.SealedSector) {
+	sm.updateDealState(dealCid, func(resp *DealResponse) {
+		resp.State = Posted
+		resp.ProofInfo = &ProofInfo{
+			SectorID: sector.SectorID,
+			CommR:    sector.CommR[:],
+			CommD:    sector.CommD[:],
+		}
+	})
+}
+
+func (sm *Miner) onCommitFail(dealCid *cid.Cid, message string) {
+	sm.updateDealState(dealCid, func(resp *DealResponse) {
+		resp.Message = message
+		resp.State = Failed
+	})
 }
 
 // OnNewHeaviestTipSet is a callback called by node, everytime the the latest head is updated.

protocol/storage/miner_test.go
@@ -1,0 +1,99 @@
+package storage
+
+import (
+	cid "gx/ipfs/QmZFbDTY9jfSBms2MchvYM9oYRbAF19K7Pby47yDBfpPrb/go-cid"
+	"testing"
+
+	"github.com/filecoin-project/go-filecoin/sectorbuilder"
+	"github.com/filecoin-project/go-filecoin/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDealsAwaitingSeal(t *testing.T) {
+	newCid := types.NewCidForTestGetter()
+	cid0 := newCid()
+	cid1 := newCid()
+	cid2 := newCid()
+
+	wantSectorID := uint64(42)
+	wantSector := &sectorbuilder.SealedSector{SectorID: wantSectorID}
+	someOtherSectorID := uint64(100)
+
+	wantMessage := "boom"
+
+	t.Run("add before success", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		dealsAwaitingSeal := newDealsAwaitingSeal()
+		gotCids := []*cid.Cid{}
+		dealsAwaitingSeal.onSuccess = func(dealCid *cid.Cid, sector *sectorbuilder.SealedSector) {
+			assert.Equal(sector, wantSector)
+			gotCids = append(gotCids, dealCid)
+		}
+
+		dealsAwaitingSeal.add(wantSectorID, cid0)
+		dealsAwaitingSeal.add(wantSectorID, cid1)
+		dealsAwaitingSeal.add(someOtherSectorID, cid2)
+		dealsAwaitingSeal.success(wantSector)
+
+		assert.Len(gotCids, 2, "onSuccess should've been called twice")
+	})
+
+	t.Run("add after success", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		dealsAwaitingSeal := newDealsAwaitingSeal()
+		gotCids := []*cid.Cid{}
+		dealsAwaitingSeal.onSuccess = func(dealCid *cid.Cid, sector *sectorbuilder.SealedSector) {
+			assert.Equal(sector, wantSector)
+			gotCids = append(gotCids, dealCid)
+		}
+
+		dealsAwaitingSeal.success(wantSector)
+		dealsAwaitingSeal.add(wantSectorID, cid0)
+		dealsAwaitingSeal.add(wantSectorID, cid1) // Shouldn't trigger a call, see add().
+		dealsAwaitingSeal.add(someOtherSectorID, cid2)
+
+		assert.Len(gotCids, 1, "onSuccess should've been called once")
+	})
+
+	t.Run("add before fail", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		dealsAwaitingSeal := newDealsAwaitingSeal()
+		gotCids := []*cid.Cid{}
+		dealsAwaitingSeal.onFail = func(dealCid *cid.Cid, message string) {
+			assert.Equal(message, wantMessage)
+			gotCids = append(gotCids, dealCid)
+		}
+
+		dealsAwaitingSeal.add(wantSectorID, cid0)
+		dealsAwaitingSeal.add(wantSectorID, cid1)
+		dealsAwaitingSeal.fail(wantSectorID, wantMessage)
+		dealsAwaitingSeal.fail(someOtherSectorID, "some message")
+
+		assert.Len(gotCids, 2, "onFail should've been called twice")
+	})
+
+	t.Run("add after fail", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		dealsAwaitingSeal := newDealsAwaitingSeal()
+		gotCids := []*cid.Cid{}
+		dealsAwaitingSeal.onFail = func(dealCid *cid.Cid, message string) {
+			assert.Equal(message, wantMessage)
+			gotCids = append(gotCids, dealCid)
+		}
+
+		dealsAwaitingSeal.fail(wantSectorID, wantMessage)
+		dealsAwaitingSeal.fail(someOtherSectorID, "some message")
+		dealsAwaitingSeal.add(wantSectorID, cid0)
+		dealsAwaitingSeal.add(wantSectorID, cid1) // Shouldn't trigger a call, see add().
+
+		assert.Len(gotCids, 1, "onFail should've been called once")
+	})
+}

commit 3f14fb06eed2a70f7b03b1e603d41986c73dc780

add more checks to a flaky test (#1262)
I can't figure out why #1257 happens so I'm adding checks on the errors that were previously missed. Throwing it back into the wild and will wait and see if it resurfaces, hopefully we'll have more information next time around.

Also close #1263 , a bug I introduced in #1240 where I was calling Fail without a failure message.

protocol/retrieval/retrieval_protocol_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/rand"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -71,21 +72,27 @@ func TestRetrievalProtocolHappyPath(t *testing.T) {
 	// wait for commitSector to make it into the chain
 	cancelA := make(chan struct{})
 	cancelB := make(chan struct{})
+	errCh := make(chan error)
 	defer close(cancelA)
 	defer close(cancelB)
+	defer close(errCh)
 
 	select {
-	case <-firstMatchingMsgInChain(ctx, t, minerNode.ChainReader, "commitSector", minerOwnerAddr, cancelA):
+	case <-firstMatchingMsgInChain(ctx, t, minerNode.ChainReader, "commitSector", minerOwnerAddr, cancelA, errCh):
+	case err = <-errCh:
+		require.NoError(err)
 	case <-time.After(120 * time.Second):
 		cancelA <- struct{}{}
-		t.Fatalf("timed out waiting for commitSector message (for sector of size=%d, from miner owner=%s) to appear in miner node's chain", testSectorSize, minerOwnerAddr)
+		t.Fatalf("timed out waiting for commitSector message (for sector of size=%d, from miner owner=%s) to appear in **miner** node's chain", testSectorSize, minerOwnerAddr)
 	}
 
 	select {
-	case <-firstMatchingMsgInChain(ctx, t, clientNode.ChainReader, "commitSector", minerOwnerAddr, cancelB):
+	case <-firstMatchingMsgInChain(ctx, t, clientNode.ChainReader, "commitSector", minerOwnerAddr, cancelB, errCh):
+	case err = <-errCh:
+		require.NoError(err)
 	case <-time.After(120 * time.Second):
 		cancelB <- struct{}{}
-		t.Fatalf("timed out waiting for commitSector message (for sector of size=%d, from miner owner=%s) to appear in client node's chain", testSectorSize, minerOwnerAddr)
+		t.Fatalf("timed out waiting for commitSector message (for sector of size=%d, from miner owner=%s) to appear in **client** node's chain", testSectorSize, minerOwnerAddr)
 	}
 
 	// retrieve piece by CID and compare bytes with what we sent to miner
@@ -118,7 +125,8 @@ func retrievePieceBytes(ctx context.Context, retrievalClient api.RetrievalClient
 
 // firstMatchingMsgInChain queries the blockchain history in a loop for a block
 // with matching properties, publishing the first match to the returned channel.
-func firstMatchingMsgInChain(ctx context.Context, t *testing.T, chainManager chain.ReadStore, msgMethod string, msgSenderAddr address.Address, cancelCh <-chan struct{}) <-chan *types.Block {
+// Note: this function should probably be API porcelain.
+func firstMatchingMsgInChain(ctx context.Context, t *testing.T, chainManager chain.ReadStore, msgMethod string, msgSenderAddr address.Address, cancelCh <-chan struct{}, errCh chan<- error) <-chan *types.Block {
 	out := make(chan *types.Block)
 
 	go func() {
@@ -132,13 +140,16 @@ func firstMatchingMsgInChain(ctx context.Context, t *testing.T, chainManager cha
 				select {
 				case <-cancelCh:
 					return
-				case event, ok := <-history:
-					if !ok {
+				case event, more := <-history:
+					if !more {
 						break Loop
 					}
 
 					ts, ok := event.(consensus.TipSet)
-					require.True(t, ok, "expected a TipSet")
+					if !ok {
+						errCh <- fmt.Errorf("expected a TipSet, got a %T with value %v", event, event)
+						return
+					}
 
 					for _, block := range ts.ToSlice() {
 						for _, message := range block.Messages {
@@ -161,7 +172,8 @@ func firstMatchingMsgInChain(ctx context.Context, t *testing.T, chainManager cha
 func createRandomPieceInfo(t *testing.T, blockService blockservice.BlockService, n uint64) (*sectorbuilder.PieceInfo, []byte) {
 	randomBytes := createRandomBytes(t, n)
 	node := merkledag.NewRawNode(randomBytes)
-	blockService.AddBlock(node)
+	err := blockService.AddBlock(node)
+	require.NoError(t, err)
 
 	size, err := node.Size()
 	require.NoError(t, err)

protocol/storage/storage_protocol_test.go
@@ -143,7 +143,7 @@ func TestStorageProtocolBasic(t *testing.T) {
 	if waitTimeout(&wg, 120*time.Second) {
 		state, message := requireQueryDeal()
 		require.NotEqual(Failed, state, message)
-		assert.Fail("waiting for submission timed out. Saw %d blocks with %d messages while waiting", bCount, mCount)
+		assert.Failf("TestStorageProtocolBasic failed", "waiting for submission timed out. Saw %d blocks with %d messages while waiting", bCount, mCount)
 	}
 	require.True(foundCommit, "no commitSector on chain")
 	require.True(foundPoSt, "no submitPoSt on chain")