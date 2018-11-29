c5b1c4faab93bd761228280d0c861c9d4e389957
frrist <forrest@protocol.ai>
2018-11-29 10:03:10 -0800

    remove streamto command, fixes #1411

--- commands/log.go
+++ commands/log.go
@@ -1,10 +1,6 @@
 package commands
 
 import (
-	"time"
-
-	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
-
 	"gx/ipfs/QmPTfgFTo9PFr1PvPKyKoeMgBvYPh6cX3aDP7DHKVbnCbi/go-ipfs-cmds"
 	"gx/ipfs/QmSP88ryZkHSRn1fnngAaV2Vcn63WUJzAavnRM9CVdU1Ky/go-ipfs-cmdkit"
 )
@@ -19,8 +15,7 @@ output of a running daemon.
 	},
 
 	Subcommands: map[string]*cmds.Command{
-		"tail":     logTailCmd,
-		"streamto": logStreamToCmd,
+		"tail": logTailCmd,
 	},
 }
 
@@ -35,34 +30,5 @@ Outputs event log messages (not other log messages) as they are generated.
 	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) {
 		r := GetAPI(env).Log().Tail(req.Context)
 		re.Emit(r) // nolint: errcheck
-	},
-}
-
-var logStreamToCmd = &cmds.Command{
-	Helptext: cmdkit.HelpText{
-		Tagline: "Stream the json encoded log events to a multiaddress.",
-		ShortDescription: `
-Sends all json-encoded log messages to the multiaddr.
-This command will run until its killed.
-`,
-	},
-	Arguments: []cmdkit.Argument{
-		cmdkit.StringArg("addr", true, false, "multiaddress logs will stream to"),
-	},
-	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) {
-		maddr, err := ma.NewMultiaddr(req.Arguments[0])
-		if err != nil {
-			re.SetError(err, cmdkit.ErrNormal)
-			return
-		}
-		for {
-			if req.Context.Err() != nil {
-				re.SetError(err, cmdkit.ErrNormal)
-				return
-			}
-			err := GetAPI(env).Log().StreamTo(req.Context, maddr)
-			log.Warningf("StreamTo closed, re-starting... %v", err)
-			time.Sleep(1 * time.Second)
-		}
 	},
 }
