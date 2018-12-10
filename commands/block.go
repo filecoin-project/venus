e58910699ac368cc336488998a9460882f839bf9
Rose Karr <rose@rosalinekarr.com>
2018-12-10 15:54:56 -0500

    Added formatted text output to show block command

--- commands/block.go
+++ commands/block.go
@@ -1,9 +1,15 @@
 package commands
 
 import (
+	"fmt"
+	"io"
+	"strconv"
+
 	"gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	"gx/ipfs/Qma6uuSyjkecGhMFFLfzyJDPyoDtNJSHJNweDccZhaWkgU/go-ipfs-cmds"
 	"gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+
+	"github.com/filecoin-project/go-filecoin/types"
 )
 
 var showCmd = &cmds.Command{
@@ -33,6 +39,28 @@ var showBlockCmd = &cmds.Command{
 			return err
 		}
 
-		return cmds.EmitOnce(re, block)
+		return re.Emit(block)
+	},
+	Type: types.Block{},
+	Encoders: cmds.EncoderMap{
+		cmds.Text: cmds.MakeTypedEncoder(func(req *cmds.Request, w io.Writer, block *types.Block) error {
+			output := fmt.Sprintf(`Block Details
+Miner:       %s
+Ticket:      %s
+Numerator:   %s
+Denominator: %s
+Height:      %s
+Nonce:       %s`,
+				block.Miner,
+				block.Ticket,
+				strconv.FormatUint(uint64(block.ParentWeightNum), 10),
+				strconv.FormatUint(uint64(block.ParentWeightDenom), 10),
+				strconv.FormatUint(uint64(block.Height), 10),
+				strconv.FormatUint(uint64(block.Nonce), 10),
+			)
+
+			_, err := fmt.Fprintln(w, output)
+			return err
+		}),
 	},
 }
