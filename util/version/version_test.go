b24174f0410e22abe12edeb3841fdd0a32eadef8
rkowalick <rkowalick@users.noreply.github.com>
2018-12-11 11:00:57 -0500

    Check go version on build
    We add a check during the build script to ensure that we have the
    correct go version. Additionally, we:
    
    - Update Circle CI config to use go1.11.1
    - Delete unused code in the util folder.

---
+++ util/version/version_test.go
@@ -0,0 +1,22 @@
+package version_test
+
+import (
+	"github.com/filecoin-project/go-filecoin/util/version"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheck(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(version.Check("go1.11.1"))
+	assert.True(version.Check("go1.11.2"))
+	assert.True(version.Check("go1.12"))
+	assert.True(version.Check("go1.12.1"))
+	assert.True(version.Check("go1.12.2"))
+
+	assert.False(version.Check("go1.11"))
+	assert.False(version.Check("go1.10"))
+	assert.False(version.Check("go2"))
+}
