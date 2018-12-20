d71db1f51c3b501dc13c0a2985da8a0139ff8ff8
phritz <phritz@users.noreply.github.com>
2018-12-20 09:56:59 -1000

    removed signature

--- api2/porcelain/message.go
+++ api2/porcelain/message.go
@@ -2,55 +2,44 @@ package porcelain
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	logging "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
 
-	"github.com/filecoin-project/go-filecoin/abi"
 	"github.com/filecoin-project/go-filecoin/actor/builtin/miner"
 	"github.com/filecoin-project/go-filecoin/address"
-	"github.com/filecoin-project/go-filecoin/api2/impl/mthdsigapi"
-	"github.com/filecoin-project/go-filecoin/exec"
 	"github.com/filecoin-project/go-filecoin/types"
 	vmErrors "github.com/filecoin-project/go-filecoin/vm/errors"
 	"github.com/pkg/errors"
 )
 
+// plumbing is the subset of the PlumbingAPI that MessageSendWithRetry uses.
 type plumbing interface {
-	ActorGetSignature(ctx context.Context, actorAddr address.Address, method string) (*exec.FunctionSignature, error)
 	MessageSend(ctx context.Context, from, to address.Address, value *types.AttoFIL, gasPrice types.AttoFIL, gasLimit types.GasCost, method string, params ...interface{}) (cid.Cid, error)
 	MessageWait(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error
 }
 
 var log = logging.Logger("porcelain") // nolint: deadcode
 
-// SendMessageAndWait creates a message, adds it to the mempool and waits for inclusion.
-// It will retry upto retries times, if there is a nonce error when including it.
-// It returns the deserialized results, or an error.
-
-// - re-org could bounce out
-// Bad idea:
-// - hard to know how long to wait bc null blocks
-// - could happen twice: retry creates a new one and the old one gets mined
-// - can't get access to perm and temp failures, only see if it is onchain; kinda want visibility into failures so react quicker
-// really want lanes
-// - errors could be fatal
-// - spams the network
-
+// MessageSendWithRetry sends a message and retries if it does not appear on chain. It returns
+// an error if it maxes out its retries, if the message on chain has an error receipt, or if
+// it hits an unexpected error.
+//
+// Note: this is not an awesome retry pattern because:
+// - the message could be successfully sent twice eg if attempt 1 appears to fail
+//   we will stop waiting for it and attempt a second send. If the first attempt
+//   succeeds we don't see it and continue to retry the second.
+// - it spams the network with re-tries and doesn't clean up the message pool with
+//   attempts that have been abandoned.
+// - it's hard to know how long to wait given the potential for null blocks
+// - it spends a lot of time walking back in the chain for a message that will not be there.
+//   We don't have a mechanism to just wait on future messages or to limit the walk-back.
+//
+// Access to failures might make this a little better but really the solution is
+// to not have re-tries, eg if we had nonce lanes.
 // if send errors then it errors
-
-// test send error
-// test cancel
-// test wait timeout
-// test signature errors
-
-// bad: if timeout is too low to walk back in chain
-
-// TODO verify Wiat impl deps dont return an error if the context is canceled
-
-func MessageSendWithRetry(ctx context.Context, plumbing plumbing, numRetries uint, waitDuration time.Duration, from, to address.Address, val *types.AttoFIL, method string, gasPrice types.AttoFIL, gasLimit types.GasCost, params ...interface{}) (res []interface{}, err error) {
+func MessageSendWithRetry(ctx context.Context, plumbing plumbing, numRetries uint, waitDuration time.Duration, from, to address.Address, val *types.AttoFIL, method string, gasPrice types.AttoFIL, gasLimit types.GasCost, params ...interface{}) (err error) {
 	for i := 0; i < int(numRetries); i++ {
 		log.Debugf("SendMessageAndWait (%s) retry %d/%d, waitDuration %v", method, i, numRetries, waitDuration)
 
@@ -60,53 +49,25 @@ func MessageSendWithRetry(ctx context.Context, plumbing plumbing, numRetries uin
 
 		msgCid, err := plumbing.MessageSend(ctx, from, to, val, gasPrice, gasLimit, method, params...)
 		if err != nil {
-			return nil, errors.Wrap(err, "couldn't send message")
+			return errors.Wrap(err, "couldn't send message")
 		}
 
 		waitCtx, waitCancel := context.WithDeadline(ctx, time.Now().Add(waitDuration))
 		found := false
 		err = plumbing.MessageWait(waitCtx, msgCid, func(blk *types.Block, smsg *types.SignedMessage, receipt *types.MessageReceipt) error {
 			found = true
-
 			if receipt.ExitCode != uint8(0) {
 				return vmErrors.VMExitCodeToError(receipt.ExitCode, miner.Errors)
-			}
-
-			// Note: a better pattern would be to have MessageSendWithRetry just send and retry, and
-			// leave the method signature stuff to the caller if in fact they want to do it at all.
-			mthdSig, err2 := plumbing.ActorGetSignature(waitCtx, smsg.Message.To, smsg.Message.Method)
-			if err2 == mthdsigapi.ErrNoMethod && err2 == mthdsigapi.ErrNoActorImpl {
-				fmt.Printf("\n\n-1=%#+v\n\n", receipt)
-
-				return nil
-			} else if err2 != nil {
-				fmt.Printf("\n\n-2=%#+v\n\n", receipt)
-				return err2
-			}
-			res = make([]interface{}, len(receipt.Return))
-			fmt.Printf("\n\n0=%#+v\n\n", receipt)
-			for i := 0; i < len(receipt.Return); i++ {
-				fmt.Printf("\n\n1\n\n")
-				val, err := abi.DecodeValues(receipt.Return[i], []abi.Type{mthdSig.Return[i]})
-				if err != nil {
-					return err
-				}
-				res[i] = abi.FromValues(val)
-				fmt.Printf("\nres[0]=%v\n", res[i])
 			}
 			return nil
 		})
 		waitCancel()
-		fmt.Printf("\nerr = %v\n", err)
-		if err != nil {
-			return nil, errors.Wrap(err, "got an error from MessageWait")
+		if err != nil && err != context.DeadlineExceeded {
+			return errors.Wrap(err, "got an error from MessageWait")
 		} else if found {
-			return res, nil
+			return nil
 		}
-
-		// cleanup message pool ?????
-		//node.MsgPool.Remove(msgCid)
 	}
 
-	return nil, errors.Wrapf(err, "failed to send message after waiting %v for each of %d retries ", waitDuration, numRetries)
+	return errors.Wrapf(err, "failed to send message after waiting %v for each of %d retries ", waitDuration, numRetries)
 }
--- api2/porcelain/message_test.go
+++ api2/porcelain/message_test.go
@@ -2,46 +2,21 @@ package porcelain_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
-	hamt "gx/ipfs/QmRXf2uUSdGSunRJsM9wXSUNVwLUGCY3So5fAs7h2CBJVf/go-hamt-ipld"
-	blockstore "gx/ipfs/QmS2aqUZLJp8kF1ihE5rvDGE5LvmKDPnx32w9Z1BW9xLV5/go-ipfs-blockstore"
-	datastore "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore"
 
-	"github.com/filecoin-project/go-filecoin/abi"
-	"github.com/filecoin-project/go-filecoin/actor"
-	"github.com/filecoin-project/go-filecoin/actor/builtin"
 	"github.com/filecoin-project/go-filecoin/address"
-	"github.com/filecoin-project/go-filecoin/api2/impl/mthdsigapi"
 	"github.com/filecoin-project/go-filecoin/api2/porcelain"
 	"github.com/filecoin-project/go-filecoin/exec"
-	"github.com/filecoin-project/go-filecoin/state"
-	th "github.com/filecoin-project/go-filecoin/testhelpers"
 	"github.com/filecoin-project/go-filecoin/types"
-	"github.com/filecoin-project/go-filecoin/vm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// type plumbing interface {
-// 	ActorGetSignature(ctx context.Context, actorAddr address.Address, method string) (*exec.FunctionSignature, error)
-// 	MessageSend(ctx context.Context, from, to address.Address, value *types.AttoFIL, gasPrice types.AttoFIL, gasLimit types.GasCost, method string, params ...interface{}) (cid.Cid, error)
-// 	MessageWait(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error
-// }
-
 var newCid = types.NewCidForTestGetter()
 var newAddr = address.NewForTestGetter()
-
-type fakeChainReadStore struct {
-	st state.Tree
-}
-
-func (f *fakeChainReadStore) LatestState(ctx context.Context) (state.Tree, error) {
-	return f.st, nil
-}
 
 type fakePlumbing struct {
 	assert  *assert.Assertions
@@ -87,208 +62,64 @@ func (fp *fakePlumbing) successfulMessageWait(ctx context.Context, msgCid cid.Ci
 func (fp *fakePlumbing) unsuccessfulMessageWait(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error {
 	fp.require.NotEqual(cid.Undef, fp.msgCid)
 	fp.assert.True(fp.msgCid.Equals(msgCid))
-	return nil
+	return context.DeadlineExceeded
 }
 
 func TestMessageSendWithRetry(t *testing.T) {
 	t.Parallel()
 	val, gasPrice, gasLimit := types.NewAttoFILFromFIL(0), types.NewGasPrice(0), types.NewGasCost(0)
 
-	// t.Run("succeeds on first try", func(t *testing.T) {
-	// 	require := require.New(t)
-	// 	assert := assert.New(t)
-	// 	ctx := context.Background()
-	// 	from, to := newAddr(), newAddr()
-
-	// 	fp := &fakePlumbing{assert: assert, require: require}
-	// 	fp.actorGetSignature = fp.nopActorGetSignature
-	// 	fp.messageSend = fp.successfulMessageSend
-	// 	fp.messageWait = fp.successfulMessageWait
-
-	// 	_, err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
-	// 	require.NoError(err)
-	// 	assert.Equal(1, fp.sendCnt)
-	// })
-
-	// t.Run("retries if not successful", func(t *testing.T) {
-	// 	require := require.New(t)
-	// 	assert := assert.New(t)
-	// 	ctx := context.Background()
-	// 	from, to := newAddr(), newAddr()
-
-	// 	fp := &fakePlumbing{assert: assert, require: require}
-	// 	fp.actorGetSignature = fp.nopActorGetSignature
-	// 	fp.messageSend = fp.successfulMessageSend
-	// 	fp.messageWait = fp.unsuccessfulMessageWait
-
-	// 	_, err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
-	// 	require.NoError(err)
-	// 	assert.Equal(10, fp.sendCnt)
-	// })
-
-	// t.Run("respects top-level context", func(t *testing.T) {
-	// 	require := require.New(t)
-	// 	assert := assert.New(t)
-	// 	ctx, cancel := context.WithCancel(context.Background())
-	// 	from, to := newAddr(), newAddr()
-
-	// 	fp := &fakePlumbing{assert: assert, require: require}
-	// 	fp.actorGetSignature = fp.nopActorGetSignature
-	// 	fp.messageSend = fp.successfulMessageSend
-	// 	// This MessageWait cancels and ctx and returns unsuccessfully. The effect is
-	// 	// canceling the global context during the first run; we expect it not to retry
-	// 	// if that is the case ie sendCnt to be 1.
-	// 	fp.messageWait = func(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error {
-	// 		cancel()
-	// 		return nil
-	// 	}
-
-	// 	_, err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
-	// 	require.Error(err)
-	// 	assert.Equal(1, fp.sendCnt)
-	// })
-
-	t.Run("returns sane message receipt results", func(t *testing.T) {
+	t.Run("succeeds on first try", func(t *testing.T) {
 		require := require.New(t)
 		assert := assert.New(t)
 		ctx := context.Background()
-		from := newAddr()
-
-		// We're gonna use a real signature getter so set up a To actor
-		// that has a method with a non-trivial signature.
-		// First, get a signed message and pull out the To field.
-		ki := types.MustGenerateKeyInfo(10, types.GenerateKeyInfoSeed())
-		mockSigner := types.NewMockSigner(ki)
-		signedMessage := types.NewSignedMessageForTestGetter(mockSigner)()
-		signedMessage.Method = "hasReturnValue"
-		to := signedMessage.To
-		// Now, install an actor at that address.
-		cst := hamt.NewCborStore()
-		bs := blockstore.NewBlockstore(datastore.NewMapDatastore())
-		vms := vm.NewStorageMap(bs)
-		fakeActorCodeCid := newCid()
-		builtin.Actors[fakeActorCodeCid] = &actor.FakeActor{}
-		defer func() {
-			delete(builtin.Actors, fakeActorCodeCid)
-		}()
-		fakeActor := th.RequireNewFakeActorWithTokens(require, vms, to, fakeActorCodeCid, types.NewAttoFILFromFIL(102))
-		_, st := th.RequireMakeStateTree(require, cst, map[address.Address]*actor.Actor{
-			to: fakeActor,
-		})
-		sigGetter := mthdsigapi.NewGetter(&fakeChainReadStore{st})
+		from, to := newAddr(), newAddr()
 
 		fp := &fakePlumbing{assert: assert, require: require}
-		fp.actorGetSignature = sigGetter.Get
+		fp.actorGetSignature = fp.nopActorGetSignature
 		fp.messageSend = fp.successfulMessageSend
-		expectedAddr := newAddr()
-		expectedAddrEncoded, err := abi.ToEncodedValues(expectedAddr)
-		require.NoError(err)
-		expectedAddrBytes := []types.Bytes{expectedAddrEncoded}
-		fmt.Printf("\nex=%v\n", expectedAddrBytes)
-		fp.messageWait = func(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error {
-			return cb(&types.Block{}, signedMessage, &types.MessageReceipt{ExitCode: 0, Return: expectedAddrBytes})
-		}
+		fp.messageWait = fp.successfulMessageWait
 
-		res, err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "hasReturnValue", gasPrice, gasLimit)
+		err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
 		require.NoError(err)
-		require.Equal(1, len(res))
-		fmt.Printf("\nres=%T\n", res)
-		fmt.Printf("\nres[0]=%T\n", res[0])
-		res0 := res[0]
-		require.Equal(1, len(res0))
-		gotAddr, ok := res0[0].(address.Address)
-		assert.True(ok)
-		assert.Equal(expectedAddr, gotAddr)
+		assert.Equal(1, fp.sendCnt)
 	})
 
-	// t.Run("succeeds if method exists", func(t *testing.T) {
-	// 	require := require.New(t)
+	t.Run("retries if not successful", func(t *testing.T) {
+		require := require.New(t)
+		assert := assert.New(t)
+		ctx := context.Background()
+		from, to := newAddr(), newAddr()
 
-	// 	ctx := context.Background()
-	// 	cst := hamt.NewCborStore()
-	// 	addr := address.NewForTestGetter()()
-	// 	bs := blockstore.NewBlockstore(datastore.NewMapDatastore())
-	// 	vms := vm.NewStorageMap(bs)
+		fp := &fakePlumbing{assert: assert, require: require}
+		fp.actorGetSignature = fp.nopActorGetSignature
+		fp.messageSend = fp.successfulMessageSend
+		fp.messageWait = fp.unsuccessfulMessageWait
 
-	// 	// Install the fake actor so we can query one of its method signatures.
-	// 	emptyActorCodeCid := types.NewCidForTestGetter()()
-	// 	builtin.Actors[emptyActorCodeCid] = &actor.FakeActor{}
-	// 	defer func() {
-	// 		delete(builtin.Actors, emptyActorCodeCid)
-	// 	}()
+		err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
+		require.NoError(err)
+		assert.Equal(10, fp.sendCnt)
+	})
 
-	// 	fakeActor := th.RequireNewFakeActorWithTokens(require, vms, addr, emptyActorCodeCid, types.NewAttoFILFromFIL(102))
-	// 	_, st := th.RequireMakeStateTree(require, cst, map[address.Address]*actor.Actor{
-	// 		addr: fakeActor,
-	// 	})
-	// 	getter := mthdsigapi.NewGetter(&fakeChainReadStore{st})
+	t.Run("respects top-level context", func(t *testing.T) {
+		require := require.New(t)
+		assert := assert.New(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		from, to := newAddr(), newAddr()
 
-	// 	sig, err := getter.Get(ctx, addr, "hasReturnValue")
-	// 	require.NoError(err)
-	// 	expected := &exec.FunctionSignature{Params: []abi.Type(nil), Return: []abi.Type{abi.Address}}
-	// 	require.Equal(expected, sig)
-	// })
+		fp := &fakePlumbing{assert: assert, require: require}
+		fp.actorGetSignature = fp.nopActorGetSignature
+		fp.messageSend = fp.successfulMessageSend
+		// This MessageWait cancels and ctx and returns unsuccessfully. The effect is
+		// canceling the global context during the first run; we expect it not to retry
+		// if that is the case ie sendCnt to be 1.
+		fp.messageWait = func(ctx context.Context, msgCid cid.Cid, cb func(*types.Block, *types.SignedMessage, *types.MessageReceipt) error) error {
+			cancel()
+			return nil
+		}
 
-	// t.Run("errors if no such method", func(t *testing.T) {
-	// 	require := require.New(t)
-
-	// 	ctx := context.Background()
-	// 	cst := hamt.NewCborStore()
-	// 	addr := address.NewForTestGetter()()
-
-	// 	acctActor := th.RequireNewAccountActor(require, types.NewAttoFILFromFIL(10000))
-	// 	_, st := th.RequireMakeStateTree(require, cst, map[address.Address]*actor.Actor{
-	// 		addr: acctActor,
-	// 	})
-
-	// 	getter := mthdsigapi.NewGetter(&fakeChainReadStore{st})
-
-	// 	_, err := getter.Get(ctx, addr, "NoSuchMethod")
-	// 	require.Error(err)
-	// })
-
-	// t.Run("errors with ErrNoMethod if no method", func(t *testing.T) {
-	// 	assert := assert.New(t)
-	// 	require := require.New(t)
-
-	// 	ctx := context.Background()
-	// 	cst := hamt.NewCborStore()
-	// 	addr := address.NewForTestGetter()()
-
-	// 	acctActor := th.RequireNewAccountActor(require, types.NewAttoFILFromFIL(10000))
-	// 	_, st := th.RequireMakeStateTree(require, cst, map[address.Address]*actor.Actor{
-	// 		addr: acctActor,
-	// 	})
-
-	// 	getter := mthdsigapi.NewGetter(&fakeChainReadStore{st})
-
-	// 	sig, err := getter.Get(ctx, addr, "")
-	// 	assert.Equal(mthdsigapi.ErrNoMethod, err)
-	// 	assert.Nil(sig)
-	// })
-
-	// t.Run("errors if actor undefined", func(t *testing.T) {
-	// 	require := require.New(t)
-
-	// 	ctx := context.Background()
-	// 	cst := hamt.NewCborStore()
-	// 	addr := address.NewForTestGetter()()
-
-	// 	// Install the empty actor so we can query one of its method signatures.
-	// 	emptyActorCodeCid := types.NewCidForTestGetter()()
-	// 	builtin.Actors[emptyActorCodeCid] = &actor.FakeActor{}
-	// 	defer func() {
-	// 		delete(builtin.Actors, emptyActorCodeCid)
-	// 	}()
-	// 	emptyActor := th.RequireNewEmptyActor(require, types.NewAttoFILFromFIL(0))
-
-	// 	_, st := th.RequireMakeStateTree(require, cst, map[address.Address]*actor.Actor{
-	// 		addr: emptyActor,
-	// 	})
-
-	// 	getter := mthdsigapi.NewGetter(&fakeChainReadStore{st})
-
-	// 	_, err := getter.Get(ctx, addr, "Foo")
-	// 	require.Error(err)
-	// })
+		err := porcelain.MessageSendWithRetry(ctx, fp, 10 /* retries */, 1*time.Second /* wait time*/, from, to, val, "", gasPrice, gasLimit)
+		require.Error(err)
+		assert.Equal(1, fp.sendCnt)
+	})
 }
