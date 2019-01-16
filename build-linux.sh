60747bb93ab9a93e0005e9b5e628b104e000f83f
gmasgras <gmasgras@gmail.com>
2019-01-16 15:57:28 -0800

    add publish_release job. remove unnecessary dynamic libs

--- build-linux.sh
+++ build-linux.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
 mkdir -p filecoin
-cd filecoin
-
-# library files
-mkdir lib
-cp ../proofs/rust-proofs/target/release/deps/libfilecoin_proofs.so lib/
-cp ../proofs/rust-proofs/target/release/deps/libsector_base.so lib/
 
 # binary
-cp ../go-filecoin .
-chmod +x go-filecoin
+cp go-filecoin filecoin/
+chmod +x filecoin/go-filecoin
 
 # fixture data
-mkdir fixtures
-cp ../fixtures/*.key ./fixtures/
-cp ../fixtures/*.car ./fixtures/
-cp ../fixtures/*.json ./fixtures/
-cp ../fixtures/*.toml ./fixtures/
-
-cd ..
+mkdir filecoin/fixtures
+cp fixtures/*.key filecoin/fixtures/
+cp fixtures/*.car filecoin/fixtures/
+cp fixtures/*.json filecoin/fixtures/
