a4c7af81d2220556fffb83095dca2c5a4c8df122
gmasgras <gmasgras@gmail.com>
2019-01-17 14:51:25 -0800

    remove dynamic libs from OSX builds
    add build_macos dependency for publish_release
    
    move bundling to one script
    
    add publish-release script.

---
+++ scripts/publish-release.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+RELEASE_TAG="${CIRCLE_SHA1:0:16}"
+
+# make sure we have a token set, api requests won't work otherwise
+if [ -z $GITHUB_TOKEN ]; then
+  echo "\$GITHUB_TOKEN not set, publish failed"
+  exit 1
+fi
+
+#see if the release already exists by tag
+RELEASE_RESPONSE=`
+  curl \
+    --header "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$RELEASE_TAG"
+`
+RELEASE_ID=`echo $RELEASE_RESPONSE | jq '.id'`
+
+if [ "$RELEASE_ID" = "null" ]; then
+  echo "creating release"
+
+  RELEASE_DATA="{
+    \"tag_name\": \"$RELEASE_TAG\",
+    \"target_commitish\": \"$CIRCLE_SHA1\",
+    \"name\": \"$RELEASE_TAG\",
+    \"body\": \"\"
+  }"
+
+  # create it if it doesn't exist yet
+  RELEASE_RESPONSE=`
+    curl \
+        --request POST \
+        --header "Authorization: token $GITHUB_TOKEN" \
+        --header "Content-Type: application/json" \
+        --data "$RELEASE_DATA" \
+        "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases"
+  `
+else
+  echo "release already exists"
+fi
+
+RELEASE_UPLOAD_URL=`echo $RELEASE_RESPONSE | jq -r '.upload_url' | cut -d'{' -f1`
+
+bundles=('filecoin-Linux.tar.gz' 'filecoin-Darwin.tar.gz')
+for RELEASE_FILE in "${bundles[@]}"
+do
+  echo "Uploading release bundle: $RELEASE_FILE"
+  curl \
+    --request POST \
+    --header "Authorization: token $GITHUB_TOKEN" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary "@$RELEASE_FILE" \
+    "$RELEASE_UPLOAD_URL?name=$(basename $RELEASE_FILE)"
+
+  echo "Release bundle uploaded: $RELEASE_FILE "
+done
