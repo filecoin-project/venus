489a9fbdda04a199a638265fcaa87154d5e27535
gmasgras <gmasgras@gmail.com>
2019-01-18 10:20:51 -0800

    move bundling to one script

---
+++ build-bundle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p filecoin
+
+# binary
+cp go-filecoin filecoin/
+chmod +x filecoin/go-filecoin
+
+# fixture data
+mkdir filecoin/fixtures
+cp fixtures/*.key filecoin/fixtures/
+cp fixtures/*.car filecoin/fixtures/
+cp fixtures/*.json filecoin/fixtures/
+
+
+tar -zcvf "filecoin-`uname`.tar.gz" filecoin
