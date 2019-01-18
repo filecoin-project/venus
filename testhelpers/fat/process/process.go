ed82faf5a5ebbffb39e7bd0012867647c142e444
frrist <forrest@protocol.ai>
2019-01-17 16:57:47 -0800

    add FAT processor and tests
    - add mock iptb filecoin plugin for testing

---
+++ testhelpers/fat/process/process.go
@@ -0,0 +1,103 @@
+package process
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	iptb "github.com/ipfs/iptb/testbed"
+	"github.com/ipfs/iptb/testbed/interfaces"
+
+	dockerplugin "github.com/filecoin-project/go-filecoin/tools/iptb-plugins/filecoin/docker"
+	localplugin "github.com/filecoin-project/go-filecoin/tools/iptb-plugins/filecoin/local"
+)
+
+// must register all filecoin iptb plugins first.
+func init() {
+	_, err := iptb.RegisterPlugin(iptb.IptbPlugin{
+		From:       "<builtin>",
+		NewNode:    localplugin.NewNode,
+		PluginName: localplugin.PluginName,
+		BuiltIn:    true,
+	}, false)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = iptb.RegisterPlugin(iptb.IptbPlugin{
+		From:       "<builtin>",
+		NewNode:    dockerplugin.NewNode,
+		PluginName: dockerplugin.PluginName,
+		BuiltIn:    true,
+	}, false)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Filecoin represents a wrapper around the iptb Core interface.
+type Filecoin struct {
+	core testbedi.Core
+	ctx  context.Context
+}
+
+// NewFilecoinProcess returns a pointer to a Filecoin process of type `t`, create its
+// repo under dir `d`, and wraps the IPTB core interface `c`.
+func NewFilecoinProcess(ctx context.Context, t, d string, c testbedi.Core) *Filecoin {
+	return &Filecoin{
+		core: c,
+		ctx:  ctx,
+	}
+}
+
+// RunCmdWithStdin runs `args` against Filecoin process `f`, a testbedi.Output and an error are returned.
+func (f *Filecoin) RunCmdWithStdin(ctx context.Context, stdin io.Reader, args ...string) (testbedi.Output, error) {
+	if ctx == nil {
+		ctx = f.ctx
+	}
+	out, err := f.core.RunCmd(ctx, stdin, args...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// RunCmdJSONWithStdin runs `args` against Filecoin process `f`. The '--enc=json' flag
+// is appened to the command specified by `args`, the result of the command is marshaled into `v`.
+func (f *Filecoin) RunCmdJSONWithStdin(ctx context.Context, stdin io.Reader, v interface{}, args ...string) error {
+	args = append(args, "--enc=json")
+	out, err := f.RunCmdWithStdin(ctx, stdin, args...)
+	if err != nil {
+		return err
+	}
+
+	// check command exit code
+	if out.ExitCode() > 0 {
+		return fmt.Errorf("Filecoin command: %s, exited with non-zero exitcode: %d", out.Args(), out.ExitCode())
+	}
+
+	dec := json.NewDecoder(out.Stdout())
+	return dec.Decode(v)
+}
+
+// RunCmdLDJSONWithStdin runs `args` against Filecoin process `f`. The '--enc=json' flag
+// is appened to the command specified by `args`. The result of the command is returned
+// as a json.Decoder that may be used to read and decode JSON values from the result of
+// the command.
+func (f *Filecoin) RunCmdLDJSONWithStdin(ctx context.Context, stdin io.Reader, args ...string) (*json.Decoder, error) {
+	args = append(args, "--enc=json")
+	out, err := f.RunCmdWithStdin(ctx, stdin, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	// check command exit code
+	if out.ExitCode() > 0 {
+		return nil, fmt.Errorf("Filecoin command: %s, exited with non-zero exitcode: %d", out.Args(), out.ExitCode())
+	}
+
+	return json.NewDecoder(out.Stdout()), nil
+}
---
+++ tools/iptb-plugins/filecoin/mock/mockfilecoin.go
@@ -0,0 +1,94 @@
+package pluginmockfilecoin
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"github.com/ipfs/iptb/testbed/interfaces"
+	"github.com/ipfs/iptb/util"
+)
+
+// PluginName is the name of the plugin
+var PluginName = "mockfilecoin"
+
+// Mockfilecoin is a mock structure used for testing things that use go-filecoin iptb plugins
+type Mockfilecoin struct {
+}
+
+var NewNode testbedi.NewNodeFunc // nolint: golint
+
+func init() {
+	NewNode = func(dir string, attrs map[string]string) (testbedi.Core, error) {
+		return &Mockfilecoin{}, nil
+	}
+}
+
+// Init is not implemented
+func (m *Mockfilecoin) Init(ctx context.Context, args ...string) (testbedi.Output, error) {
+	panic("not implemented")
+}
+
+// Start is not implemented
+func (m *Mockfilecoin) Start(ctx context.Context, wait bool, args ...string) (testbedi.Output, error) {
+	panic("not implemented")
+}
+
+// Stop is not implemented
+func (m *Mockfilecoin) Stop(ctx context.Context) error {
+	panic("not implemented")
+}
+
+// RunCmd will return "string" for args "", json for args "json", and ldjson for args "ldjson"
+func (m *Mockfilecoin) RunCmd(ctx context.Context, stdin io.Reader, args ...string) (testbedi.Output, error) {
+	if args[0] == "" {
+		return iptbutil.NewOutput(args, []byte("string"), []byte{}, 0, nil), nil
+	} else if args[0] == "json" {
+		//return json object
+		return iptbutil.NewOutput(args, []byte(`{"key":"value"}`), []byte{}, 0, nil), nil
+	} else if args[0] == "ldjson" {
+		// return ldjson objects
+		return iptbutil.NewOutput(args, []byte("{\"key\":\"value1\"}\n{\"key\":\"value2\"}\n"), []byte{}, 0, nil), nil
+	}
+	return nil, errors.New(`invalid mock args, can only be one of: "", "json", or "ldjson"`)
+}
+
+// Connect is not implemented
+func (m *Mockfilecoin) Connect(ctx context.Context, n testbedi.Core) error {
+	panic("not implemented")
+}
+
+// Shell is not implemented
+func (m *Mockfilecoin) Shell(ctx context.Context, ns []testbedi.Core) error {
+	panic("not implemented")
+}
+
+// Dir is not implemented
+func (m *Mockfilecoin) Dir() string {
+	panic("not implemented")
+}
+
+// Type is not implemented
+func (m *Mockfilecoin) Type() string {
+	panic("not implemented")
+}
+
+// String is not implemented
+func (m *Mockfilecoin) String() string {
+	panic("not implemented")
+}
+
+// PeerID is not implemented
+func (m *Mockfilecoin) PeerID() (string, error) {
+	panic("not implemented")
+}
+
+// APIAddr is not implemented
+func (m *Mockfilecoin) APIAddr() (string, error) {
+	panic("not implemented")
+}
+
+// SwarmAddrs is not implemented
+func (m *Mockfilecoin) SwarmAddrs() ([]string, error) {
+	panic("not implemented")
+}
