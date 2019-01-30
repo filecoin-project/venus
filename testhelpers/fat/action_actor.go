8b078f0576642574c633ea6054b30213f0905a4c
frrist <forrest@protocol.ai>
2019-01-30 09:21:00 -0800

    add actor ls action to filecoin processor
    closes #1673

---
+++ testhelpers/fat/action_actor.go
@@ -0,0 +1,19 @@
+package fat
+
+import (
+	"context"
+
+	"github.com/filecoin-project/go-filecoin/api"
+)
+
+// ActorLs runs the `actor ls` command against the filecoin process.
+func (f *Filecoin) ActorLs(ctx context.Context) (*api.ActorView, error) {
+	var out api.ActorView
+	args := []string{"go-filecoin", "actor", "ls"}
+
+	if err := f.RunCmdJSONWithStdin(ctx, nil, &out, args...); err != nil {
+		return nil, err
+	}
+
+	return &out, nil
+}
