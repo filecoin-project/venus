4b20d0345271edee363321f15f6a0f09c64d5de1
ognots <ognots@protonmail.com>
2019-02-13 10:50:58 -1000

    check for the groth parameters we care about for a more resilient check

--- scripts/install-rust-proofs.sh
+++ scripts/install-rust-proofs.sh
@@ -83,12 +83,13 @@ install_local() {
 copy_groth_params() {
   mkdir -p /tmp/filecoin-proof-parameters
   pushd /tmp/filecoin-proof-parameters
-  if ! ls v9-zigzag-proof-of-replication-*; then
-    echo "groth parameters were not found"
+  local params_file=v9-zigzag-proof-of-replication-52431242c129794fe51d373ae29953f2ff52abd94c78756e318ce45f3e4946d8
+  if [[ ! -e ${params_file} ]]; then
+    echo "groth parameter ${params_file} was not found"
     echo "try rerunning 'go run ./build/*.go deps'"
     exit 1
   fi
-  ln -s v9-zigzag-proof-of-replication-52431242c129794fe51d373ae29953f2ff52abd94c78756e318ce45f3e4946d8 params.out
+  ln -s ${params_file} params.out
   popd
 }
 
