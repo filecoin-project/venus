50be59941d8f4dc583d42c864d49efe0afc28697
ognots <ognots@protonmail.com>
2019-02-14 23:24:45 -1000

    account for FILECOIN_PARAMETER_CACHE environment var if this environment variable is set, the groth params will not be in /tmp/filecoin-proof-parameters

--- scripts/copy-groth-params.sh
+++ scripts/copy-groth-params.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 copy_groth_params() {
-  mkdir -p /tmp/filecoin-proof-parameters
-  pushd /tmp/filecoin-proof-parameters
+  local params_dir="${FILECOIN_PARAMETER_CACHE:-"/tmp/filecoin-proof-parameters/"}"
   local params_file=v9-zigzag-proof-of-replication-52431242c129794fe51d373ae29953f2ff52abd94c78756e318ce45f3e4946d8
+  mkdir -p ${params_dir}
+  pushd ${params_dir}
   if [[ ! -e ${params_file} ]]; then
     echo "groth parameter ${params_file} was not found"
     echo "try rerunning 'go run ./build/*.go deps'"
