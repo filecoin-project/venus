tomesm/go-filecoin

Move mpool from node API to plumbing and pull MessagePoolWait up to porcelain.
Includes interfaces and a fake pubsub subscription for testing the wait functionality.

plumbing/ps/subscriber.go
@@ -1,10 +1,24 @@
 package ps
 
-import "gx/ipfs/QmVRxA4J3UPQpw74dLrQ6NJkfysCA1H4GU28gVpXQt9zMU/go-libp2p-pubsub"
+import (
+	"context"
+	"gx/ipfs/QmVRxA4J3UPQpw74dLrQ6NJkfysCA1H4GU28gVpXQt9zMU/go-libp2p-pubsub"
+)
 
 // Subscriber subscribes to pubsub topics
 type Subscriber struct {
 	pubsub *pubsub.PubSub
+}
+
+// Subscription is a handle to a pubsub subscription.
+// This matches part of the interface to a libp2p.pubsub.Subscription.
+type Subscription interface {
+	// Topic returns this subscription's topic name
+	Topic() string
+	// Next returns the next message from this subscription
+	Next(ctx context.Context) (*pubsub.Message, error)
+	// Cancel cancels this subscription
+	Cancel()
 }
 
 // NewSubscriber builds a new subscriber
@@ -13,6 +27,6 @@ func NewSubscriber(sub *pubsub.PubSub) *Subscriber {
 }
 
 // Subscribe subscribes to a pubsub topic
-func (s *Subscriber) Subscribe(topic string, opts ...pubsub.SubOpt) (*pubsub.Subscription, error) {
+func (s *Subscriber) Subscribe(topic string, opts ...pubsub.SubOpt) (Subscription, error) {
 	return s.pubsub.Subscribe(topic, opts...)
 }

plumbing/ps/testing.go
@@ -1,0 +1,83 @@
+package ps
+
+import (
+	"context"
+	"gx/ipfs/QmVRxA4J3UPQpw74dLrQ6NJkfysCA1H4GU28gVpXQt9zMU/go-libp2p-pubsub"
+	"sync"
+)
+
+// FakeSubscription is a fake pubsub subscription.
+type FakeSubscription struct {
+	topic       string
+	pending     chan *pubsub.Message
+	err         error
+	cancelled   bool
+	awaitCancel sync.WaitGroup
+}
+
+// NewFakeSubsciption builds a new fake subscription to a topic.
+func NewFakeSubscription(topic string) *FakeSubscription {
+	sub := &FakeSubscription{
+		topic:       topic,
+		pending:     make(chan *pubsub.Message),
+		awaitCancel: sync.WaitGroup{},
+	}
+	sub.awaitCancel.Add(1)
+	return sub
+}
+
+// Subscription interface
+
+// Topic returns this subscription's topic.
+func (s *FakeSubscription) Topic() string {
+	return s.topic
+}
+
+// Next returns the next messages from this subscription.
+func (s *FakeSubscription) Next(ctx context.Context) (*pubsub.Message, error) {
+	if s.err != nil {
+		return nil, s.err
+	}
+	select {
+	case msg := <-s.pending:
+		return msg, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// Cancel cancels this subscription, after which no subsequently posted messages will be received.
+func (s *FakeSubscription) Cancel() {
+	if s.cancelled {
+		panic("subscription already cancelled")
+	}
+	s.cancelled = true
+	s.awaitCancel.Done()
+}
+
+// Manipulators
+
+// Post posts a new message to this subscription.
+func (s *FakeSubscription) Post(msg *pubsub.Message) {
+	if s.err != nil {
+		panic("subscription has failed")
+	}
+	if !s.cancelled {
+		s.pending <- msg
+	}
+}
+
+// Fail causes subsequent reads from this subscription to fail.
+func (s *FakeSubscription) Fail(err error) {
+	if err != nil {
+		panic("error is nil")
+	}
+	if !s.cancelled {
+		s.err = err
+	}
+}
+
+// AwaitCancellation waits for the subscription to be canceled by the subscriber.
+func (s *FakeSubscription) AwaitCancellation() {
+	s.awaitCancel.Wait()
+}

porcelain/mpool.go
@@ -1,0 +1,41 @@
+package porcelain
+
+import (
+	"context"
+
+	"gx/ipfs/QmVRxA4J3UPQpw74dLrQ6NJkfysCA1H4GU28gVpXQt9zMU/go-libp2p-pubsub"
+
+	"github.com/filecoin-project/go-filecoin/plumbing/msg"
+	"github.com/filecoin-project/go-filecoin/plumbing/ps"
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+// The subset of plumbing used by MessagePoolWait
+type mpwPlumbing interface {
+	MessagePoolPending() []*types.SignedMessage
+	PubSubSubscribe(topic string, opts ...pubsub.SubOpt) (ps.Subscription, error)
+}
+
+// MessagePoolWait waits until the message pool contains at least messageCount unmined messages.
+func MessagePoolWait(ctx context.Context, plumbing mpwPlumbing, messageCount uint) ([]*types.SignedMessage, error) {
+	pending := plumbing.MessagePoolPending()
+	if len(pending) < int(messageCount) {
+		subscription, err := plumbing.PubSubSubscribe(msg.Topic)
+		defer subscription.Cancel()
+		if err != nil {
+			return nil, err
+		}
+
+		// Poll pending again after subscribing in case a message arrived since.
+		pending = plumbing.MessagePoolPending()
+		for len(pending) < int(messageCount) {
+			_, err = subscription.Next(ctx)
+			if err != nil {
+				return nil, err
+			}
+			pending = plumbing.MessagePoolPending()
+		}
+	}
+
+	return pending, nil
+}

porcelain/mpool_test.go
@@ -1,0 +1,95 @@
+package porcelain_test
+
+import (
+	"context"
+	"gx/ipfs/QmVRxA4J3UPQpw74dLrQ6NJkfysCA1H4GU28gVpXQt9zMU/go-libp2p-pubsub"
+	"sync"
+	"testing"
+
+	"github.com/filecoin-project/go-filecoin/plumbing/msg"
+	"github.com/filecoin-project/go-filecoin/plumbing/ps"
+	"github.com/filecoin-project/go-filecoin/porcelain"
+	"github.com/filecoin-project/go-filecoin/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeMpoolWaitPlumbing struct {
+	pending []*types.SignedMessage
+	subCh   chan *ps.FakeSubscription // Receives subscription as is it opened
+}
+
+func newFakeMpoolWaitPlumbing() *fakeMpoolWaitPlumbing {
+	return &fakeMpoolWaitPlumbing{
+		subCh: make(chan *ps.FakeSubscription, 1),
+	}
+}
+
+func (plumbing *fakeMpoolWaitPlumbing) MessagePoolPending() []*types.SignedMessage {
+	return plumbing.pending
+}
+
+func (plumbing *fakeMpoolWaitPlumbing) PubSubSubscribe(topic string, opts ...pubsub.SubOpt) (ps.Subscription, error) {
+	subscription := ps.NewFakeSubscription(msg.Topic)
+	plumbing.subCh <- subscription
+	close(plumbing.subCh)
+	return subscription, nil
+}
+
+func TestMessagePoolWait(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+	assert := assert.New(t)
+	ki := types.MustGenerateKeyInfo(1, types.GenerateKeyInfoSeed())
+	signer := types.NewMockSigner(ki)
+
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+		msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 0)
+		require.NoError(e)
+		assert.Equal(0, len(msgs))
+	})
+
+	t.Run("returns immediates", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+		plumbing.pending = types.NewSignedMsgs(3, signer)
+
+		msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 3)
+		require.NoError(e)
+		assert.Equal(3, len(msgs))
+	})
+
+	t.Run("waits", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+
+		// Waits for subscription to be requested, then posts a message
+		go func() {
+			sub := <-plumbing.subCh
+			// First pubsub message doesn't affect the message pool.
+			// The waiter must wait longer.
+			sub.Post(nil)
+
+			// Add a message to the pool then bump the pubsub again.
+			plumbing.pending = types.NewSignedMsgs(1, signer)
+			sub.Post(nil)
+
+			sub.AwaitCancellation()
+			defer wg.Done()
+		}()
+
+		go func() {
+			msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 1)
+			require.NoError(e)
+			assert.Equal(1, len(msgs))
+			defer wg.Done()
+		}()
+
+		wg.Wait()
+	})
+}

filecoin-project/venus

plumbing/ps/subscriber.go
@@ -1,10 +1,25 @@
 package ps
 
-import "gx/ipfs/QmepvmmYNM6q4RaUiwEikQFhgMFHXg2PLhx2E9iaRd3jmS/go-libp2p-pubsub"
+import (
+	"context"
+
+	"gx/ipfs/QmepvmmYNM6q4RaUiwEikQFhgMFHXg2PLhx2E9iaRd3jmS/go-libp2p-pubsub"
+)
 
 // Subscriber subscribes to pubsub topics
 type Subscriber struct {
 	pubsub *pubsub.PubSub
+}
+
+// Subscription is a handle to a pubsub subscription.
+// This matches part of the interface to a libp2p.pubsub.Subscription.
+type Subscription interface {
+	// Topic returns this subscription's topic name
+	Topic() string
+	// Next returns the next message from this subscription
+	Next(ctx context.Context) (*pubsub.Message, error)
+	// Cancel cancels this subscription
+	Cancel()
 }
 
 // NewSubscriber builds a new subscriber
@@ -13,6 +28,6 @@ func NewSubscriber(sub *pubsub.PubSub) *Subscriber {
 }
 
 // Subscribe subscribes to a pubsub topic
-func (s *Subscriber) Subscribe(topic string, opts ...pubsub.SubOpt) (*pubsub.Subscription, error) {
+func (s *Subscriber) Subscribe(topic string, opts ...pubsub.SubOpt) (Subscription, error) {
 	return s.pubsub.Subscribe(topic, opts...)
 }

plumbing/ps/testing.go
@@ -1,0 +1,84 @@
+package ps
+
+import (
+	"context"
+	"sync"
+
+	"gx/ipfs/QmepvmmYNM6q4RaUiwEikQFhgMFHXg2PLhx2E9iaRd3jmS/go-libp2p-pubsub"
+)
+
+// FakeSubscription is a fake pubsub subscription.
+type FakeSubscription struct {
+	topic       string
+	pending     chan *pubsub.Message
+	err         error
+	cancelled   bool
+	awaitCancel sync.WaitGroup
+}
+
+// NewFakeSubscription builds a new fake subscription to a topic.
+func NewFakeSubscription(topic string) *FakeSubscription {
+	sub := &FakeSubscription{
+		topic:       topic,
+		pending:     make(chan *pubsub.Message),
+		awaitCancel: sync.WaitGroup{},
+	}
+	sub.awaitCancel.Add(1)
+	return sub
+}
+
+// Subscription interface
+
+// Topic returns this subscription's topic.
+func (s *FakeSubscription) Topic() string {
+	return s.topic
+}
+
+// Next returns the next messages from this subscription.
+func (s *FakeSubscription) Next(ctx context.Context) (*pubsub.Message, error) {
+	if s.err != nil {
+		return nil, s.err
+	}
+	select {
+	case msg := <-s.pending:
+		return msg, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// Cancel cancels this subscription, after which no subsequently posted messages will be received.
+func (s *FakeSubscription) Cancel() {
+	if s.cancelled {
+		panic("subscription already cancelled")
+	}
+	s.cancelled = true
+	s.awaitCancel.Done()
+}
+
+// Manipulators
+
+// Post posts a new message to this subscription.
+func (s *FakeSubscription) Post(msg *pubsub.Message) {
+	if s.err != nil {
+		panic("subscription has failed")
+	}
+	if !s.cancelled {
+		s.pending <- msg
+	}
+}
+
+// Fail causes subsequent reads from this subscription to fail.
+func (s *FakeSubscription) Fail(err error) {
+	if err != nil {
+		panic("error is nil")
+	}
+	if !s.cancelled {
+		s.err = err
+	}
+}
+
+// AwaitCancellation waits for the subscription to be canceled by the subscriber.
+func (s *FakeSubscription) AwaitCancellation() {
+	s.awaitCancel.Wait()
+}

porcelain/mpool.go
@@ -1,0 +1,41 @@
+package porcelain
+
+import (
+	"context"
+
+	"gx/ipfs/QmepvmmYNM6q4RaUiwEikQFhgMFHXg2PLhx2E9iaRd3jmS/go-libp2p-pubsub"
+
+	"github.com/filecoin-project/go-filecoin/plumbing/msg"
+	"github.com/filecoin-project/go-filecoin/plumbing/ps"
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+// The subset of plumbing used by MessagePoolWait
+type mpwPlumbing interface {
+	MessagePoolPending() []*types.SignedMessage
+	PubSubSubscribe(topic string, opts ...pubsub.SubOpt) (ps.Subscription, error)
+}
+
+// MessagePoolWait waits until the message pool contains at least messageCount unmined messages.
+func MessagePoolWait(ctx context.Context, plumbing mpwPlumbing, messageCount uint) ([]*types.SignedMessage, error) {
+	pending := plumbing.MessagePoolPending()
+	if len(pending) < int(messageCount) {
+		subscription, err := plumbing.PubSubSubscribe(msg.Topic)
+		defer subscription.Cancel()
+		if err != nil {
+			return nil, err
+		}
+
+		// Poll pending again after subscribing in case a message arrived since.
+		pending = plumbing.MessagePoolPending()
+		for len(pending) < int(messageCount) {
+			_, err = subscription.Next(ctx)
+			if err != nil {
+				return nil, err
+			}
+			pending = plumbing.MessagePoolPending()
+		}
+	}
+
+	return pending, nil
+}

porcelain/mpool_test.go
@@ -1,0 +1,96 @@
+package porcelain_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"gx/ipfs/QmepvmmYNM6q4RaUiwEikQFhgMFHXg2PLhx2E9iaRd3jmS/go-libp2p-pubsub"
+
+	"github.com/filecoin-project/go-filecoin/plumbing/msg"
+	"github.com/filecoin-project/go-filecoin/plumbing/ps"
+	"github.com/filecoin-project/go-filecoin/porcelain"
+	"github.com/filecoin-project/go-filecoin/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeMpoolWaitPlumbing struct {
+	pending []*types.SignedMessage
+	subCh   chan *ps.FakeSubscription // Receives subscription as is it opened
+}
+
+func newFakeMpoolWaitPlumbing() *fakeMpoolWaitPlumbing {
+	return &fakeMpoolWaitPlumbing{
+		subCh: make(chan *ps.FakeSubscription, 1),
+	}
+}
+
+func (plumbing *fakeMpoolWaitPlumbing) MessagePoolPending() []*types.SignedMessage {
+	return plumbing.pending
+}
+
+func (plumbing *fakeMpoolWaitPlumbing) PubSubSubscribe(topic string, opts ...pubsub.SubOpt) (ps.Subscription, error) {
+	subscription := ps.NewFakeSubscription(msg.Topic)
+	plumbing.subCh <- subscription
+	close(plumbing.subCh)
+	return subscription, nil
+}
+
+func TestMessagePoolWait(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+	assert := assert.New(t)
+	ki := types.MustGenerateKeyInfo(1, types.GenerateKeyInfoSeed())
+	signer := types.NewMockSigner(ki)
+
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+		msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 0)
+		require.NoError(e)
+		assert.Equal(0, len(msgs))
+	})
+
+	t.Run("returns immediates", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+		plumbing.pending = types.NewSignedMsgs(3, signer)
+
+		msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 3)
+		require.NoError(e)
+		assert.Equal(3, len(msgs))
+	})
+
+	t.Run("waits", func(t *testing.T) {
+		t.Parallel()
+		plumbing := newFakeMpoolWaitPlumbing()
+
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+
+		// Waits for subscription to be requested, then posts a message
+		go func() {
+			sub := <-plumbing.subCh
+			// First pubsub message doesn't affect the message pool.
+			// The waiter must wait longer.
+			sub.Post(nil)
+
+			// Add a message to the pool then bump the pubsub again.
+			plumbing.pending = types.NewSignedMsgs(1, signer)
+			sub.Post(nil)
+
+			sub.AwaitCancellation()
+			defer wg.Done()
+		}()
+
+		go func() {
+			msgs, e := porcelain.MessagePoolWait(context.Background(), plumbing, 1)
+			require.NoError(e)
+			assert.Equal(1, len(msgs))
+			defer wg.Done()
+		}()
+
+		wg.Wait()
+	})
+}