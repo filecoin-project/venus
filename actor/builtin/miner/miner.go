e492593b81e73d8a0c1159ba5cd1f73ea3d0e2d6
Erin Swenson-Healey <l@s3r.me>
2019-02-27 09:50:18 -0800

    pass config value to verify_post call instead of "sector bytes" (#2104)
    * pass config value to verify_post call
    
    * bump submodule SHA to include new version of rust-fil-proofs
    
    - use fake generate PoSt routine
    
    * don't validate the fake proofs
    
    * bump rust-fil-proofs version
    
    changes in this merge include:
    
    
    -----------------------------------------------
    commit 15663bfa22b81aedf450ef827d28e05fe610e7b8
    Author: Erin Swenson-Healey <l@s3r.me>
    Date:   Tue Feb 26 15:29:56 2019 -0800
    
        verify_post should be passed cfg + add FFI tests for PoSt generation/verification (#508)
        
        * plumb ConfiguredStore instead of sector bytes
        
        * FFI test suite to include verify_post and generate_post calls
        
        * modify paramcache to optionally generate PoSt parameters
        
        * generate PoSt parameters before FFI tests run
        
        * paramcache to be configured with arg-flags
        
        * ensure paramcache runs before FFI tests (using test config)
        
        * rustfmt
        
        * run cache populator on same channel as FFI test
    
    
    -----------------------------------------------
    commit e53392ce69a75e6b5e27430f0e0a8cf41f8c5682
    Merge: bc025a2 c95a6c4
    Author: porcuquine <1746729+porcuquine@users.noreply.github.com>
    Date:   Tue Feb 26 14:52:24 2019 -0800
    
        Merge pull request #513 from filecoin-project/zigzag-example-taper
        
        Improve zigzag example for ongoing benchmarks.
    
    
    -----------------------------------------------
    commit c95a6c44b2465e54a975a023b4365fcaa68730b0
    Author: porcuquine <porcuquine@users.noreply.github.com>
    Date:   Tue Feb 5 16:22:12 2019 -0800
    
        Improve zigzag example for ongoing benchmarks.
        
        Generate groth params first (for memory reasons).
        
        Add logging to proof generation.
        
        Receive and use taper parameters.
        
        Display all challenges.
    
    
    -----------------------------------------------
    commit bc025a2fe9c3346fa184daeec275839085917af5
    Author: Erin Swenson-Healey <l@s3r.me>
    Date:   Tue Feb 26 12:13:16 2019 -0800
    
        revert to using fake_generate_post instead of real generate_post (#512)
    
    
    -----------------------------------------------
    commit ca7b335262bee6e448579a4d41720d5da8310e39
    Merge: cba59e8 51ab3d9
    Author: Lucas Molas <schomatis@gmail.com>
    Date:   Mon Feb 25 23:37:21 2019 -0300
    
        Merge pull request #471 from filecoin-project/fix/prove/request-parents-once
        
        drgporep: add parents to `decode_domain_block`
    
    
    -----------------------------------------------
    commit 51ab3d9fd99c82f78b13ac65dd1438e3cea1dd6b
    Merge: dc1f6e6 cba59e8
    Author: Lucas Molas <schomatis@gmail.com>
    Date:   Mon Feb 25 21:20:04 2019 -0300
    
        Merge branch 'master' into fix/prove/request-parents-once
    
    
    -----------------------------------------------
    commit cba59e8643d4dd52452883050cd7ab207a896103
    Author: Erin Swenson-Healey <l@s3r.me>
    Date:   Mon Feb 25 13:58:40 2019 -0800
    
        distinguish padded/unpadded bytes at the type level (#506)
        
        * differentiate between padded and unpadded byte-amounts at type level
        
        This changeset introduces two [newtype](https://doc.rust-lang.org/1.0.0/style/features/types/newtype.html) wrappers for u64: `UnpaddedBytesAmount` and `PaddedBytesAmount` and modifies the codebase to give/receive values of these types instead of u64.
        
        * Add operation test to document behavior.
    
    * remove now-dead verifier-wrapper
    
    * delete now-dead challenge seed-creation code (which was bogus anyhow)
    
    * add note about delay function proof-verification
    
    * remove comment which was a lie

--- actor/builtin/miner/miner.go
+++ actor/builtin/miner/miner.go
@@ -660,12 +660,22 @@ func (ma *Actor) SubmitPoSt(ctx exec.VMContext, proof []byte) (uint8, error) {
 		postProof := proofs.PoStProof{}
 		copy(postProof[:], proof)
 
-		// TODO: use IsPoStValidWithProver when proofs are implemented
+		// See comment above, in CommitSector.
+		//
+		// It is undefined behavior for a miner in "Live" mode to verify a proof
+		// created by a miner in "ProofsTest" mode (and vice-versa).
+		//
+		sectorStoreType := proofs.Live
+		if os.Getenv("FIL_USE_SMALL_SECTORS") == "true" {
+			sectorStoreType = proofs.Test
+		}
+
 		req := proofs.VerifyPoSTRequest{
 			ChallengeSeed: proofs.PoStChallengeSeed{},
 			CommRs:        commRs,
 			Faults:        []uint64{},
 			Proof:         postProof,
+			StoreType:     sectorStoreType,
 		}
 
 		res, err := (&proofs.RustVerifier{}).VerifyPoST(req)
--- consensus/expected.go
+++ consensus/expected.go
@@ -265,32 +265,12 @@ func (c *Expected) RunStateTransition(ctx context.Context, ts types.TipSet, ance
 // validateMining checks validity of the block ticket, proof, and miner address.
 //    Returns an error if:
 //    	* any tipset's block was mined by an invalid miner address.
-//      * the block proof is invalid for the challenge
 //      * the block ticket is incorrectly computed
 //      * the block ticket fails the power check, i.e. is not a winning ticket
 //    Returns nil if all the above checks pass.
 // See https://github.com/filecoin-project/specs/blob/master/mining.md#chain-validation
 func (c *Expected) validateMining(ctx context.Context, st state.Tree, ts types.TipSet, parentTs types.TipSet) error {
 	for _, blk := range ts.ToSlice() {
-		parentHeight, err := parentTs.Height()
-		if err != nil {
-			return errors.Wrap(err, "failed to get parentHeight")
-		}
-
-		nullBlockCount := uint64(blk.Height) - parentHeight - 1
-		challengeSeed, err := CreateChallengeSeed(parentTs, nullBlockCount)
-		if err != nil {
-			return errors.Wrap(err, "couldn't create challengeSeed")
-		}
-
-		isValid, err := proofs.IsPoStValidWithVerifier(c.verifier, []proofs.CommR{}, challengeSeed, []uint64{}, blk.Proof)
-		if err != nil {
-			return errors.Wrap(err, "could not test the proof's validity")
-		}
-		if !isValid {
-			return errors.New("invalid proof")
-		}
-
 		computedTicket := CreateTicket(blk.Proof, blk.Miner)
 
 		if !bytes.Equal(blk.Ticket, computedTicket) {
@@ -298,6 +278,10 @@ func (c *Expected) validateMining(ctx context.Context, st state.Tree, ts types.T
 		}
 
 		// TODO: Also need to validate BlockSig
+
+		// TODO: Once we've picked a delay function (see #2119), we need to
+		// verify its proof here. The proof will likely be written to a field on
+		// the mined block.
 
 		// See https://github.com/filecoin-project/specs/blob/master/mining.md#ticket-checking
 		result, err := IsWinningTicket(ctx, c.bstore, c.PwrTableView, st, blk.Ticket, blk.Miner)
