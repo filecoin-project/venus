869e513c7f5edf731e733efa054cf931c734cab3
Rosaline Karr <rose@rosalinekarr.com>
2019-03-13 11:25:06 -0400

    Feat/addrs plum pork (#2243)
    * Removed Addrs Ls from node API
    
    * Removed Addrs New from node API
    
    * Removed Addrs Lookup from node API

--- api/address.go
+++ api/address.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"gx/ipfs/QmQmhotPUzVrMEWNK3x1R5jQ5ZHWyL7tVUrmRPjrBrvyCb/go-ipfs-files"
-	"gx/ipfs/QmTu65MVbemtUxJEWgsTtzv9Zv9P8rvmqNA4eG9TrTRGYc/go-libp2p-peer"
 
 	"github.com/filecoin-project/go-filecoin/address"
 	"github.com/filecoin-project/go-filecoin/types"
@@ -12,14 +11,6 @@ import (
 
 // Address is the interface that defines methods to manage Filecoin addresses and wallets.
 type Address interface {
-	Addrs() Addrs
 	Import(ctx context.Context, f files.File) ([]address.Address, error)
 	Export(ctx context.Context, addrs []address.Address) ([]*types.KeyInfo, error)
-}
-
-// Addrs is the interface that defines method to interact with addresses.
-type Addrs interface {
-	New(ctx context.Context) (address.Address, error)
-	Ls(ctx context.Context) ([]address.Address, error)
-	Lookup(ctx context.Context, addr address.Address) (peer.ID, error)
 }
