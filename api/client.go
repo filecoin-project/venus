2e155c5903366683462cbc53c0669c6b8df25291
Rose Karr <rose@rosalinekarr.com>
2019-03-15 16:19:03 -0400

    Moved Client ListAsks from node api to porcelain

--- api/client.go
+++ api/client.go
@@ -11,18 +11,7 @@ import (
 	"github.com/filecoin-project/go-filecoin/actor/builtin/paymentbroker"
 	"github.com/filecoin-project/go-filecoin/address"
 	"github.com/filecoin-project/go-filecoin/protocol/storage/storagedeal"
-	"github.com/filecoin-project/go-filecoin/types"
 )
-
-// Ask is a result of querying for an ask, it may contain an error
-type Ask struct {
-	Miner  address.Address
-	Price  *types.AttoFIL
-	Expiry *types.BlockHeight
-	ID     uint64
-
-	Error error
-}
 
 // Client is the interface that defines methods to manage client operations.
 type Client interface {
@@ -30,6 +19,5 @@ type Client interface {
 	ImportData(ctx context.Context, data io.Reader) (ipld.Node, error)
 	ProposeStorageDeal(ctx context.Context, data cid.Cid, miner address.Address, ask uint64, duration uint64, allowDuplicates bool) (*storagedeal.Response, error)
 	QueryStorageDeal(ctx context.Context, prop cid.Cid) (*storagedeal.Response, error)
-	ListAsks(ctx context.Context) (<-chan Ask, error)
 	Payments(ctx context.Context, dealCid cid.Cid) ([]*paymentbroker.PaymentVoucher, error)
 }
