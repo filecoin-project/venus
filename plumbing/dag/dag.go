dfaab87c863e5b319b5c88458cafb3d3977c3cd6
Rose Karr <rose@rosalinekarr.com>
2019-03-19 13:15:36 -0400

    Re-added TODO comment removed from plumbing DAGCat

--- plumbing/dag/dag.go
+++ plumbing/dag/dag.go
@@ -77,6 +77,10 @@ func (dag *DAG) GetFileSize(ctx context.Context, c cid.Cid) (uint64, error) {
 
 // Cat returns an iostream with a piece of data stored on the merkeldag with
 // the given cid.
+//
+// TODO: this goes back to 'how is data stored and referenced'
+// For now, lets just do things the ipfs way.
+// https://github.com/filecoin-project/specs/issues/136
 func (dag *DAG) Cat(ctx context.Context, c cid.Cid) (uio.DagReader, error) {
 	data, err := dag.dserv.Get(ctx, c)
 	if err != nil {
