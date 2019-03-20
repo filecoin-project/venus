dac7b6f8c803a37f814c44514db2c6d0613b014f
Sidney Keese <sidney@carbonfive.com>
2019-03-20 15:15:23 -0700

    use precompiled bls-signatures library

--- scripts/install-bls-signatures.sh
+++ scripts/install-bls-signatures.sh
@@ -1,8 +1,49 @@
 #!/usr/bin/env bash
 
 install_precompiled() {
-  echo "precompiled bls-signatures not supported yet"
-  return 1
+  RELEASE_SHA1=`git rev-parse @:./bls-signatures/bls-signatures`
+  RELEASE_NAME="bls-signatures-`uname`"
+  RELEASE_TAG="${RELEASE_SHA1:0:16}"
+
+  RELEASE_RESPONSE=`curl \
+    --location \
+    "https://api.github.com/repos/filecoin-project/bls-signatures/releases/tags/$RELEASE_TAG"
+  `
+
+  RELEASE_ID=`echo $RELEASE_RESPONSE | jq '.id'`
+
+  if [ "$RELEASE_ID" == "null" ]; then
+    echo "release ${RELEASE_TAG} does not exist, GitHub said ${RELEASE_RESPONSE}"
+    echo "ensure your access token has full repo scope"
+    return 1
+  fi
+
+  RELEASE_URL=`echo $RELEASE_RESPONSE | jq -r ".assets[] | select(.name | contains(\"$RELEASE_NAME\")) | .url"`
+
+
+  ASSET_URL=`curl \
+      --head \
+      --header "Accept:application/octet-stream" \
+      --location \
+      --output /dev/null \
+      -w %{url_effective} \
+      "$RELEASE_URL"
+  `
+  ASSET_ID=`basename ${RELEASE_URL}`
+
+  TAR_NAME="${RELEASE_NAME}_${ASSET_ID}"
+  if [ ! -f "/tmp/${TAR_NAME}.tar.gz" ]; then
+      curl --output "/tmp/${TAR_NAME}.tar.gz" "$ASSET_URL"
+      if [ $? -ne "0" ]; then
+          echo "asset failed to be downloaded"
+          return 1
+      fi
+  fi
+
+  mkdir -p bls-signatures/include
+  mkdir -p bls-signatures/lib/pkgconfig
+
+  tar -C bls-signatures -xzf /tmp/${TAR_NAME}.tar.gz
 }
 
 install_local() {
