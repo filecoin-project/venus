07cd47f605a9b1994ab7cf5519efa6204c4dbecc
shannonwells <shannonwells@users.noreply.github.com>
2019-03-20 11:46:48 -0700

    remove test that validated buggy behavior; left comment w/ issue

--- commands/dht_daemon_test.go
+++ commands/dht_daemon_test.go
@@ -1,13 +1,10 @@
 package commands_test
 
 import (
-	"strings"
 	"testing"
-	"time"
 
 	ast "gx/ipfs/QmPVkJMTeRC6iBByPWdrRkD3BE5UXsj5HPzb4kPqL186mS/testify/assert"
 
-	"github.com/filecoin-project/go-filecoin/fixtures"
 	th "github.com/filecoin-project/go-filecoin/testhelpers"
 )
 
@@ -32,63 +29,7 @@ func TestDhtFindPeer(t *testing.T) {
 	assert.Contains(d2Addr, findpeerOutput)
 }
 
-func TestDhtFindProvs(t *testing.T) {
-	t.Parallel()
-	assert := ast.New(t)
-
-	d1 := makeTestDaemonWithMinerAndStart(t)
-	defer d1.ShutdownSuccess()
-
-	d2 := th.NewDaemon(t,
-		th.WithMiner(fixtures.TestMiners[0]),
-		th.KeyFile(fixtures.KeyFilePaths()[0]),
-		th.KeyFile(fixtures.KeyFilePaths()[1])).Start()
-	defer d2.ShutdownSuccess()
-
-	d3 := th.NewDaemon(t,
-		th.WithMiner(fixtures.TestMiners[0]),
-		th.KeyFile(fixtures.KeyFilePaths()[0]),
-		th.KeyFile(fixtures.KeyFilePaths()[2])).Start()
-	defer d3.ShutdownSuccess()
-
-	d1.ConnectSuccess(d2)
-	d1.ConnectSuccess(d3)
-	d2.ConnectSuccess(d3)
-
-	d1.MineAndPropagate(2*time.Second, d2)
-	d2.MineAndPropagate(2*time.Second, d1)
-	d3.MineAndPropagate(2*time.Second, d1, d2)
-
-	op1 := d1.RunSuccess("chain", "ls")
-	results := strings.Split(op1.ReadStdout(), "\n")
-	assert.True(len(results) > 1)
-	genesisKey := results[0]
-
-	t.Run("basic command succeeds", func(t *testing.T) {
-		op1 = d1.RunSuccess("dht", "findprovs", genesisKey)
-		results = strings.Split(op1.ReadStdoutTrimNewlines(), "\n")
-		// all miners should have the genesis block key
-		assert.Len(results, 2)
-	})
-
-	t.Run("verbose option outputs expected info", func(t *testing.T) {
-		actual := d1.RunSuccess("dht", "findprovs", genesisKey, "-v").ReadStdoutTrimNewlines()
-
-		expectedStrs := []string{
-			"provider: Qm",
-			"querying <peer.ID Qm*",
-			"adding peer to query: <peer.ID Qm*",
-			"/ip4/127.0.0.1/tcp",
-		}
-		for _, expected := range expectedStrs {
-			assert.Contains(actual, expected)
-		}
-
-	})
-
-	t.Run("number of providers option limits the providers found", func(t *testing.T) {
-		op1 = d1.RunSuccess("dht", "findprovs", genesisKey, "-n", "1")
-		results = strings.Split(op1.ReadStdoutTrimNewlines(), "\n")
-		assert.Len(results, 1)
-	})
-}
+// TODO: findprovs will have to be untested until
+//  https://github.com/filecoin-project/go-filecoin/issues/2357
+//  original tests were flaky; testing may need to be omitted entirely
+//  unless it can consistently pass.
