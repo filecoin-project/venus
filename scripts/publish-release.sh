85e3f50f2df93f1ae6c49071d1666ccc528ba62a
ognots <ognots@protonmail.com>
2019-03-21 09:21:40 -0400

    update script to mostly conform to shellcheck
    did not implement warnings on replacing  with  as we use that style in several scripts

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -3,68 +3,68 @@ set -e
 
 pushd bundle
 
-SHORT_GIT_SHA=${CIRCLE_SHA1:0:6}
+SHORT_GIT_SHA="${CIRCLE_SHA1:0:6}"
 RELEASE_TAG="${CIRCLE_TAG:-$SHORT_GIT_SHA}"
-BUNDLE_NAME=$RELEASE_TAG
+BUNDLE_NAME="${RELEASE_TAG}"
 PRERELEASE=false
 
-if [ ! -z FILECOIN_BINARY_VERSION ]
+if [ -n "${FILECOIN_BINARY_VERSION}" ]
 then
-  # nightly-${CIRCLE_BUILD_NUM}-$(echo $CIRCLE_SHA1 | cut -c -6)
-  RELEASE_TAG=$FILECOIN_BINARY_VERSION
+  # nightly-${CIRCLE_BUILD_NUM}-$(echo ${CIRCLE_SHA1} | cut -c -6)
+  RELEASE_TAG="${FILECOIN_BINARY_VERSION}"
   PRERELEASE=true
 fi
 
 # make sure we have a token set, api requests won't work otherwise
-if [ -z $GITHUB_TOKEN ]; then
-  echo "\$GITHUB_TOKEN not set, publish failed"
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "\${GITHUB_TOKEN} not set, publish failed"
   exit 1
 fi
 
 #see if the release already exists by tag
 RELEASE_RESPONSE=`
   curl \
-    --header "Authorization: token $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$RELEASE_TAG"
+    --header "Authorization: token ${GITHUB_TOKEN}" \
+    "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/tags/${RELEASE_TAG}"
 `
-RELEASE_ID=`echo $RELEASE_RESPONSE | jq '.id'`
+RELEASE_ID=`echo "${RELEASE_RESPONSE}" | jq '.id'`
 
-if [ "$RELEASE_ID" = "null" ]; then
+if [ "${RELEASE_ID}" = "null" ]; then
   echo "creating release"
 
   RELEASE_DATA="{
-    \"tag_name\": \"$RELEASE_TAG\",
-    \"target_commitish\": \"$CIRCLE_SHA1\",
-    \"name\": \"$RELEASE_TAG\",
+    \"tag_name\": \"${RELEASE_TAG}\",
+    \"target_commitish\": \"${CIRCLE_SHA1}\",
+    \"name\": \"${RELEASE_TAG}\",
     \"body\": \"\",
-    \"prerelease\": $PRERELEASE
+    \"prerelease\": ${PRERELEASE}
   }"
 
   # create it if it doesn't exist yet
   RELEASE_RESPONSE=`
     curl \
         --request POST \
-        --header "Authorization: token $GITHUB_TOKEN" \
+        --header "Authorization: token ${GITHUB_TOKEN}" \
         --header "Content-Type: application/json" \
-        --data "$RELEASE_DATA" \
-        "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases"
+        --data "${RELEASE_DATA}" \
+        "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/${CIRCLE_PROJECT_REPONAME}/releases"
   `
 else
   echo "release already exists"
 fi
 
-RELEASE_UPLOAD_URL=`echo $RELEASE_RESPONSE | jq -r '.upload_url' | cut -d'{' -f1`
+RELEASE_UPLOAD_URL=`echo "${RELEASE_RESPONSE}" | jq -r '.upload_url' | cut -d'{' -f1`
 
-bundles=("filecoin-$BUNDLE_NAME-Linux.tar.gz" "filecoin-$BUNDLE_NAME-Darwin.tar.gz")
+bundles=("filecoin-${BUNDLE_NAME}-Linux.tar.gz" "filecoin-${BUNDLE_NAME}-Darwin.tar.gz")
 for RELEASE_FILE in "${bundles[@]}"
 do
-  echo "Uploading release bundle: $RELEASE_FILE"
+  echo "Uploading release bundle: ${RELEASE_FILE}"
   curl \
     --request POST \
-    --header "Authorization: token $GITHUB_TOKEN" \
+    --header "Authorization: token ${GITHUB_TOKEN}" \
     --header "Content-Type: application/octet-stream" \
-    --data-binary "@$RELEASE_FILE" \
-    "$RELEASE_UPLOAD_URL?name=$(basename $RELEASE_FILE)"
+    --data-binary "@${RELEASE_FILE}" \
+    "$RELEASE_UPLOAD_URL?name=$(basename "${RELEASE_FILE}")"
 
-  echo "Release bundle uploaded: $RELEASE_FILE "
+  echo "Release bundle uploaded: ${RELEASE_FILE}"
 done
