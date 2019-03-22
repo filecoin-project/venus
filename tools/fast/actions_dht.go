2a21ac40edb4958f62f27e7e3a5c14eaf05eb18d
Rose Karr <rose@rosalinekarr.com>
2019-03-22 12:03:39 -0400

    Added a fast method for DHTFindProvs

--- tools/fast/actions_dht.go
+++ tools/fast/actions_dht.go
@@ -5,7 +5,9 @@ import (
 	"io"
 
 	"gx/ipfs/QmNTCey11oxhb1AxDnQBRHtdhap6Ctud872NjAYPYYXPuc/go-multiaddr"
+	"gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	"gx/ipfs/QmTu65MVbemtUxJEWgsTtzv9Zv9P8rvmqNA4eG9TrTRGYc/go-libp2p-peer"
+	"gx/ipfs/QmWaDSNoSdSXU9b6udyaq9T8y6LkzMwqWxECznFqvtcTsk/go-libp2p-routing/notifications"
 )
 
 // DHTFindPeer runs the `dht findpeer` command against the filecoin process
@@ -32,6 +34,19 @@ func (f *Filecoin) DHTFindPeer(ctx context.Context, pid peer.ID) ([]multiaddr.Mu
 		}
 
 		out = append(out, ma)
+	}
+
+	return out, nil
+}
+
+// DHTFindProvs runs the `dht findprovs` command against the filecoin process
+func (f *Filecoin) DHTFindProvs(ctx context.Context, key cid.Cid) ([]notifications.QueryEvent, error) {
+	var out []notifications.QueryEvent
+
+	args := []string{"go-filecoin", "swarm", "findprovs", key.String()}
+
+	if err := f.RunCmdJSONWithStdin(ctx, nil, &out, args...); err != nil {
+		return nil, err
 	}
 
 	return out, nil
