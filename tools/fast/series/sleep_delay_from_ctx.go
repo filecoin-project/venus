1d1a87afced491878e2b94210be986bffe33831f
Travis Person <travisperson@users.noreply.github.com>
2019-04-08 12:18:29 -0700

    review feedback

--- tools/fast/series/sleep_delay_from_ctx.go
+++ tools/fast/series/sleep_delay_from_ctx.go
@@ -21,10 +21,10 @@ func SetSleepDelay(ctx context.Context, d time.Duration) context.Context {
 	return context.WithValue(ctx, sleepDelayKey, d)
 }
 
-// SleepDelay is a helper method to make sure people don't call `time.Sleep`
+// CtxSleep is a helper method to make sure people don't call `time.Sleep`
 // themselves in series. It will use the time.Duration in the context, or
 // default to `mining.DefaultBlockTime` from the go-filecoin/mining package.
-func SleepDelayFromCtx(ctx context.Context) {
+func CtxSleep(ctx context.Context) {
 	d, ok := ctx.Value(sleepDelayKey).(time.Duration)
 	if !ok {
 		d = defaultSleepDelay
