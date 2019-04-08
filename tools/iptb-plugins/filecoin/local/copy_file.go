748f8b3b18273713bd2bc2ca0b771ed8947146b0
Travis Person <travisperson@users.noreply.github.com>
2019-04-08 12:05:13 -0700

    IPTB: Work around for golang bug #22315

---
+++ tools/iptb-plugins/filecoin/local/copy_file.go
@@ -0,0 +1,35 @@
+// +build !linux
+
+package pluginlocalfilecoin
+
+import (
+	"io"
+	"os"
+)
+
+func copyFileContents(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+
+	defer in.Close() // nolint: errcheck
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+
+	err = out.Sync()
+	return
+}
