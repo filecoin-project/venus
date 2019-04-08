1d6d4836a848f39781a43de68d2582ee9ce60f67
Travis Person <travisperson@users.noreply.github.com>
2019-04-08 13:33:25 -0700

    review feedback

--- tools/fast/series/sleep_delay_from_ctx.go
+++ tools/fast/series/sleep_delay_from_ctx.go
@@ -9,23 +9,23 @@ import (
 
 var (
 	// Key used to set the time.Duration in the context
-	sleepDelayKey = struct{}{}
+	ctxSleepDelayKey = struct{}{}
 
 	// Default delay
 	defaultSleepDelay = mining.DefaultBlockTime
 )
 
-// SetSleepDelay returns a context with `d` set in the context. To sleep with
-// the value, call SleepDelayFromCtx with the context.
-func SetSleepDelay(ctx context.Context, d time.Duration) context.Context {
-	return context.WithValue(ctx, sleepDelayKey, d)
+// SetCtxSleepDelay returns a context with `d` set in the context. To sleep with
+// the value, call CtxSleepDelay with the context.
+func SetCtxSleepDelay(ctx context.Context, d time.Duration) context.Context {
+	return context.WithValue(ctx, ctxSleepDelayKey, d)
 }
 
-// CtxSleep is a helper method to make sure people don't call `time.Sleep`
+// CtxSleepDelay is a helper method to make sure people don't call `time.Sleep`
 // themselves in series. It will use the time.Duration in the context, or
 // default to `mining.DefaultBlockTime` from the go-filecoin/mining package.
-func CtxSleep(ctx context.Context) {
-	d, ok := ctx.Value(sleepDelayKey).(time.Duration)
+func CtxSleepDelay(ctx context.Context) {
+	d, ok := ctx.Value(ctxSleepDelayKey).(time.Duration)
 	if !ok {
 		d = defaultSleepDelay
 	}
