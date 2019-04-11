filecoin-project/venus

commit 79bba96d9426a1b2ebba62fc12a5dd76c63f1c93

rename "sector store type" to "proofs mode" (#2534)
* rename "sector store type" to "proofs mode"

* fix comments

* conform to import spec

* add Mode suffix for disambiguation purposes

* update pesky go.mod file which everyone sees as having changed

proofs/sectorbuilder/rustsectorbuilder.go
@@ -49,7 +49,7 @@ func elapsed(what string) func() {
 	}
 }
 
-// RustSectorBuilder is a struct which serves as a proxy for a SectorStore in Rust.
+// RustSectorBuilder is a struct which serves as a proxy for a SectorBuilder in Rust.
 type RustSectorBuilder struct {
 	blockService bserv.BlockService
 	ptr          unsafe.Pointer
@@ -77,7 +77,7 @@ type RustSectorBuilderConfig struct {
 	MetadataDir      string
 	MinerAddr        address.Address
 	SealedSectorDir  string
-	SectorStoreType  proofs.SectorStoreType
+	ProofsMode       proofs.Mode
 	StagedSectorDir  string
 }
 
@@ -99,9 +99,9 @@ func NewRustSectorBuilder(cfg RustSectorBuilderConfig) (*RustSectorBuilder, erro
 	cSealedSectorDir := C.CString(cfg.SealedSectorDir)
 	defer C.free(unsafe.Pointer(cSealedSectorDir))
 
-	scfg, err := proofs.CSectorStoreType(cfg.SectorStoreType)
+	scfg, err := proofs.CProofsMode(cfg.ProofsMode)
 	if err != nil {
-		return nil, errors.Errorf("unknown sector store type: %v", cfg.SectorStoreType)
+		return nil, errors.Errorf("unknown sector store type: %v", cfg.ProofsMode)
 	}
 
 	resPtr := (*C.InitSectorBuilderResponse)(unsafe.Pointer(C.init_sector_builder(

actor/builtin/miner/miner.go
@@ -460,12 +460,12 @@ func (ma *Actor) CommitSector(ctx exec.VMContext, sectorID uint64, commD, commR,
 		// the PoRep verification operation needs to know some things (e.g. size)
 		// about the sector for which the proof was generated in order to verify.
 		//
-		// It is undefined behavior for a miner in "Live" mode to verify a proof
-		// created by a miner in "ProofsTest" mode (and vice-versa).
+		// It is undefined behavior for a miner using "LiveMode" to verify a
+		// proof created by a miner using "TestMode" (and vice-versa).
 		//
-		sectorStoreType := proofs.Live
+		proofsMode := proofs.LiveMode
 		if os.Getenv("FIL_USE_SMALL_SECTORS") == "true" {
-			sectorStoreType = proofs.Test
+			proofsMode = proofs.TestMode
 		}
 
 		req := proofs.VerifySealRequest{}
@@ -475,7 +475,7 @@ func (ma *Actor) CommitSector(ctx exec.VMContext, sectorID uint64, commD, commR,
 		copy(req.Proof[:], proof)
 		req.ProverID = sectorbuilder.AddressToProverID(ctx.Message().To)
 		req.SectorID = sectorbuilder.SectorIDToBytes(sectorID)
-		req.StoreType = sectorStoreType
+		req.ProofsMode = proofsMode
 
 		res, err := (&proofs.RustVerifier{}).VerifySeal(req)
 		if err != nil {
@@ -672,12 +672,12 @@ func (ma *Actor) SubmitPoSt(ctx exec.VMContext, postProofs []proofs.PoStProof) (
 		if !ma.Bootstrap {
 			// See comment above, in CommitSector.
 			//
-			// It is undefined behavior for a miner in "Live" mode to verify a proof
-			// created by a miner in "ProofsTest" mode (and vice-versa).
+			// It is undefined behavior for a miner using "LiveMode" to verify a
+			// proof created by a miner using "TestMode" (and vice-versa).
 			//
-			sectorStoreType := proofs.Live
+			proofsMode := proofs.LiveMode
 			if os.Getenv("FIL_USE_SMALL_SECTORS") == "true" {
-				sectorStoreType = proofs.Test
+				proofsMode = proofs.TestMode
 			}
 
 			seed, err := currentProvingPeriodPoStChallengeSeed(ctx, state)
@@ -695,7 +695,7 @@ func (ma *Actor) SubmitPoSt(ctx exec.VMContext, postProofs []proofs.PoStProof) (
 				CommRs:        commRs,
 				Faults:        []uint64{},
 				Proofs:        postProofs,
-				StoreType:     sectorStoreType,
+				ProofsMode:    proofsMode,
 			}
 
 			res, err := (&proofs.RustVerifier{}).VerifyPoST(req)

go.mod
@@ -79,6 +79,7 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.1.0
 	go.opencensus.io v0.19.2
+	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 	gotest.tools v2.2.0+incompatible // indirect

node/node.go
@@ -586,13 +586,13 @@ func (node *Node) setupHeartbeatServices(ctx context.Context) error {
 
 func (node *Node) setupMining(ctx context.Context) error {
 	// configure the underlying sector store, defaulting to the non-test version
-	sectorStoreType := proofs.Live
+	proofsMode := proofs.LiveMode
 	if os.Getenv("FIL_USE_SMALL_SECTORS") == "true" {
-		sectorStoreType = proofs.Test
+		proofsMode = proofs.TestMode
 	}
 
 	// initialize a sector builder
-	sectorBuilder, err := initSectorBuilderForNode(ctx, node, sectorStoreType)
+	sectorBuilder, err := initSectorBuilderForNode(ctx, node, proofsMode)
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize sector builder")
 	}
@@ -898,7 +898,7 @@ func (node *Node) getLastUsedSectorID(ctx context.Context, minerAddr address.Add
 	return lastUsedSectorID, nil
 }
 
-func initSectorBuilderForNode(ctx context.Context, node *Node, sectorStoreType proofs.SectorStoreType) (sectorbuilder.SectorBuilder, error) {
+func initSectorBuilderForNode(ctx context.Context, node *Node, proofsMode proofs.Mode) (sectorbuilder.SectorBuilder, error) {
 	minerAddr, err := node.miningAddress()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get node's mining address")
@@ -919,7 +919,7 @@ func initSectorBuilderForNode(ctx context.Context, node *Node, sectorStoreType p
 		MetadataDir:      node.Repo.StagingDir(),
 		MinerAddr:        minerAddr,
 		SealedSectorDir:  node.Repo.SealedDir(),
-		SectorStoreType:  sectorStoreType,
+		ProofsMode:       proofsMode,
 		StagedSectorDir:  node.Repo.StagingDir(),
 	}
 

proofs/interface.go
@@ -2,13 +2,13 @@ package proofs
 
 // VerifySealRequest represents a request to verify the output of a Seal() operation.
 type VerifySealRequest struct {
-	CommD     CommD           // returned from seal
-	CommR     CommR           // returned from seal
-	CommRStar CommRStar       // returned from seal
-	Proof     SealProof       // returned from Seal
-	ProverID  [31]byte        // uniquely identifies miner
-	SectorID  [31]byte        // uniquely identifies sector
-	StoreType SectorStoreType // used to control sealing/verification performance
+	CommD      CommD     // returned from seal
+	CommR      CommR     // returned from seal
+	CommRStar  CommRStar // returned from seal
+	Proof      SealProof // returned from Seal
+	ProverID   [31]byte  // uniquely identifies miner
+	SectorID   [31]byte  // uniquely identifies sector
+	ProofsMode Mode      // used to control sealing/verification performance
 }
 
 // VerifyPoSTRequest represents a request to generate verify a proof-of-spacetime.
@@ -17,7 +17,7 @@ type VerifyPoSTRequest struct {
 	CommRs        []CommR
 	Faults        []uint64
 	Proofs        []PoStProof
-	StoreType     SectorStoreType // used to control sealing/verification performance
+	ProofsMode    Mode // used to control sealing/verification performance
 }
 
 // VerifyPoSTResponse communicates the validity of a provided proof-of-spacetime.
@@ -36,13 +36,15 @@ type Verifier interface {
 	VerifySeal(VerifySealRequest) (VerifySealResponse, error)
 }
 
-// SectorStoreType configures the behavior of the SectorStore used by the SectorBuilder.
-type SectorStoreType int
+// Mode configures sealing, sector packing, PoSt generation and other behaviors
+// of libfilecoin_proofs. Use Test mode to seal and generate PoSts quickly over
+// tiny sectors. Use Live when operating a real Filecoin node.
+type Mode int
 
 const (
-	// Live configures the SectorBuilder to be used by someone operating a real
+	// TestMode configures the SectorBuilder to be used with large sectors, in tests.
+	TestMode = Mode(iota)
+	// LiveMode configures the SectorBuilder to be used by someone operating a real
 	// Filecoin node.
-	Live = SectorStoreType(iota)
-	// Test configures the SectorBuilder to be used with large sectors, in tests.
-	Test
+	LiveMode
 )

proofs/rustverifier.go
@@ -50,14 +50,14 @@ func (rp *RustVerifier) VerifySeal(req VerifySealRequest) (VerifySealResponse, e
 	sectorIDCbytes := C.CBytes(req.SectorID[:])
 	defer C.free(sectorIDCbytes)
 
-	cfg, err := CSectorStoreType(req.StoreType)
+	mode, err := CProofsMode(req.ProofsMode)
 	if err != nil {
 		return VerifySealResponse{}, err
 	}
 
 	// a mutable pointer to a VerifySealResponse C-struct
 	resPtr := (*C.VerifySealResponse)(unsafe.Pointer(C.verify_seal(
-		cfg,
+		mode,
 		(*[32]C.uint8_t)(commRCBytes),
 		(*[32]C.uint8_t)(commDCBytes),
 		(*[32]C.uint8_t)(commRStarCBytes),
@@ -100,7 +100,7 @@ func (rp *RustVerifier) VerifyPoST(req VerifyPoSTRequest) (VerifyPoSTResponse, e
 	faultsPtr, faultsSize := cUint64s(req.Faults)
 	defer C.free(unsafe.Pointer(faultsPtr))
 
-	cfg, err := CSectorStoreType(req.StoreType)
+	cfg, err := CProofsMode(req.ProofsMode)
 	if err != nil {
 		return VerifyPoSTResponse{}, err
 	}
@@ -163,13 +163,12 @@ func cUint64s(src []uint64) (*C.uint64_t, C.size_t) {
 	return (*C.uint64_t)(cUint64s), srcCSizeT
 }
 
-// CSectorStoreType marshals from SectorStoreType to the FFI type
-// *C.ConfiguredStore.
-func CSectorStoreType(cfg SectorStoreType) (*C.ConfiguredStore, error) {
+// CProofsMode marshals from Mode to the FFI type *C.ConfiguredStore.
+func CProofsMode(cfg Mode) (*C.ConfiguredStore, error) {
 	var scfg C.ConfiguredStore
-	if cfg == Live {
+	if cfg == LiveMode {
 		scfg = C.ConfiguredStore(C.Live)
-	} else if cfg == Test {
+	} else if cfg == TestMode {
 		scfg = C.ConfiguredStore(C.Test)
 	} else {
 		return nil, errors.Errorf("unknown sector store type: %v", cfg)

proofs/sectorbuilder/testing/builder.go
@@ -74,9 +74,7 @@ func (b *Builder) Build() Harness {
 		panic(err)
 	}
 
-	// TODO: Replace this with proofs.Live plus a sector size (in this case,
-	// "small" or 127 (bytes).
-	sectorStoreType := proofs.Test
+	proofsMode := proofs.TestMode
 
 	sb, err := sectorbuilder.NewRustSectorBuilder(sectorbuilder.RustSectorBuilderConfig{
 		BlockService:     blockService,
@@ -84,7 +82,7 @@ func (b *Builder) Build() Harness {
 		MetadataDir:      memRepo.StagingDir(),
 		MinerAddr:        minerAddr,
 		SealedSectorDir:  memRepo.SealedDir(),
-		SectorStoreType:  sectorStoreType,
+		ProofsMode:       proofsMode,
 		StagedSectorDir:  memRepo.StagingDir(),
 	})
 	require.NoError(b.t, err)
@@ -99,6 +97,6 @@ func (b *Builder) Build() Harness {
 		SectorBuilder:     sb,
 		MinerAddr:         minerAddr,
 		MaxBytesPerSector: n,
-		SectorConfig:      sectorStoreType,
+		ProofsMode:        proofsMode,
 	}
 }

proofs/sectorbuilder/testing/harness.go
@@ -27,7 +27,7 @@ type Harness struct {
 	MinerAddr         address.Address
 	repo              repo.Repo
 	SectorBuilder     sb.SectorBuilder
-	SectorConfig      proofs.SectorStoreType
+	ProofsMode        proofs.Mode
 	t                 *testing.T
 }
 

commit a1331ee6f9a25aacc1b5a2c0b9f6600188103d9b

Fix broken references to old sector store type.
Broken in 79bba96d9426a1b2ebba62fc12a5dd76c63f1c93

proofs/sectorbuilder/rustsectorbuilder.go
@@ -64,7 +64,7 @@ type RustSectorBuilder struct {
 
 	// sectorStoreType configures behavior of libfilecoin_proofs, including
 	// sector packing, sector sizes, sealing and PoSt generation performance.
-	sectorStoreType proofs.SectorStoreType
+	sectorStoreType proofs.Mode
 }
 
 var _ SectorBuilder = &RustSectorBuilder{}
@@ -123,7 +123,7 @@ func NewRustSectorBuilder(cfg RustSectorBuilderConfig) (*RustSectorBuilder, erro
 		blockService:      cfg.BlockService,
 		ptr:               unsafe.Pointer(resPtr.sector_builder),
 		sectorSealResults: make(chan SectorSealResult),
-		sectorStoreType:   cfg.SectorStoreType,
+		sectorStoreType:   cfg.ProofsMode,
 	}
 
 	// load staged sector metadata and use it to initialize the poller
@@ -151,7 +151,7 @@ func NewRustSectorBuilder(cfg RustSectorBuilderConfig) (*RustSectorBuilder, erro
 func (sb *RustSectorBuilder) GetMaxUserBytesPerStagedSector() (numBytes uint64, err error) {
 	defer elapsed("GetMaxUserBytesPerStagedSector")()
 
-	scfg, err := proofs.CSectorStoreType(sb.sectorStoreType)
+	scfg, err := proofs.CProofsMode(sb.sectorStoreType)
 	if err != nil {
 		return 0, errors.Wrap(err, "CSectorStoreType failed")
 	}