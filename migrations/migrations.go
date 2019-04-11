734ec88feaa70122b156cc67080d685846dae11e
shannonwells <shannonwells@users.noreply.github.com>
2019-04-10 17:51:21 -0700

    WIP sketching out migration behavior 4

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,7 +1,11 @@
 package migrations
 
 import (
+	"fmt"
+	"os"
+
 	cmds "github.com/ipfs/go-ipfs-cmds"
+	logging "github.com/ipfs/go-log"
 )
 
 // runner
@@ -15,6 +19,12 @@ type Migrator interface {
 }
 
 func Run(req *cmds.Request) {
+	var err error
+	log := logging.Logger("Migration runner")
+	// TODO: proper log path
+	logpath := "/tmp/migration_log.txt"
+	// set up logfile & writing to logfile
+
 	// 1. do version checking
 	// look at this version
 	// look at repo version
@@ -23,19 +33,59 @@ func Run(req *cmds.Request) {
 	// look at the options
 	// repodir is:
 	// 1) FIL_PATH 2) default
-	// open repodir read-only
-	// make new repodir read-write
-	// instantiate migrator with new repo, old repo, verbose
-	// switch on command & call appropriate  dir
-	var mig Migrator
+	var oldRepo, newRepo *os.File
+	var oldpath string
+	filpath := os.Getenv("FIL_PATH")
+	if filpath == "" {
+		homeDir := os.Getenv("HOME")
+		oldpath = fmt.Sprintf("%s/.filecoin", homeDir)
+	} else {
+		oldpath = filpath
+	}
+	oldRepo, err = os.Open(oldpath)
+	if err != nil {
+		log.Error(err)
+		return // or return error, something.
+	}
 
+	// open repodir read-only
+	// create new repodir & open read-write
+	newRepo, err = os.Create("/tmp/somedir")
+	if err != nil {
+		log.Error("failed to create dir: ")
+		log.Error(err)
+		return
+	}
+
+	var mig Migrator
+	// figure out which migrator to run and run it ?
+	// or do we figure out what migration + runner to build and build it, and then run?
+	// or does this line get updated to the latest migrator?
+	// or ?
+	mig = NewMigrator_1_2(oldRepo, newRepo, true)
+
+	// TODO: do these calls with reflections?
 	switch req.Arguments[0] {
 	case "Describe":
 		mig.Describe()
+		err = nil
 	case "BuildOnly":
-		mig.BuildOnly()
+		err = mig.BuildOnly()
+	case "Install":
+		// runner controls the installation, not migrator:
+		err = mig.Install()
 	case "Run":
-		mig.Migrate()
+		if err = mig.Migrate(); err != nil {
+			log.Error(err)
+		} else {
+			err = mig.Install()
+		}
 	}
-	// runner controls the installation, not migrator
+	if err != nil {
+		// don't panic
+		log.Errorf(err.Error())
+	}
+	log.Info("============================================================================================")
+	log.Infof("Done.  Please see log for more detail: %s", logpath)
+	log.Info("============================================================================================")
 }
