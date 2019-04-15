ddbc43a1f45508731e6e11ea08a7e421e72a04b3
Rose Karr <rose@rosalinekarr.com>
2019-04-15 14:36:47 -0400

    Added a command for fetching the protocol parameters from plumbing

---
+++ commands/protocol.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"fmt"
+	"io"
+
+	cmdkit "github.com/ipfs/go-ipfs-cmdkit"
+	cmds "github.com/ipfs/go-ipfs-cmds"
+
+	"github.com/filecoin-project/go-filecoin/plumbing/cfg"
+)
+
+var protocolCmd = &cmds.Command{
+	Helptext: cmdkit.HelpText{
+		Tagline: "Show protocol parameter details",
+	},
+	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) error {
+		params, err := GetPorcelainAPI(env).ProtocolParams()
+		if err != nil {
+			return err
+		}
+		return re.Emit(params)
+	},
+	Type: cfg.ProtocolParams{},
+	Encoders: cmds.EncoderMap{
+		cmds.Text: cmds.MakeTypedEncoder(func(req *cmds.Request, w io.Writer, pp *cfg.ProtocolParams) error {
+			_, err := fmt.Fprintf(w, "Auto-Seal Interval: %d seconds\n", pp.AutoSealInterval)
+			return err
+		}),
+	},
+}
