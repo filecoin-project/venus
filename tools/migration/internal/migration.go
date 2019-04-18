53df4a221d30d57ddeed58d699d4d8a8449c43a2
shannonwells <shannonwells@users.noreply.github.com>
2019-04-18 16:57:50 -0700

    add some comments describing the interface

--- tools/migration/internal/migration.go
+++ tools/migration/internal/migration.go
@@ -4,9 +4,29 @@ import (
 	"os"
 )
 
-type Migrator interface {
+// Migration is the interface to all repo migration versions.
+type Migration interface {
+	// Migrate performs all migration steps for the Migration that implements the interface.
+	// Migrate  expects newRepo to be:
+	//		a directory
+	// 		read/writeable by this process,
+	//      contain a copy of the old repo.
 	Migrate(newRepo *os.File) error
+
+	// Describe returns a list of steps, as a formatted string, that a given Migration will take.
+	// These should correspond to named functions in the given Migration.
 	Describe() string
+
+	// Validate performs validation operations, using oldRepo for comparison.
+	// Validation requirements will be different for every migration.
+	// Validate expects newRepo to be
+	//		a directory
+	//      readable by this process
+	//      already migrated
+	//  It expects oldRepo to be
+	//		a directory
+	//      read-only by this process
+	//  A successful validation returns nil.
 	Validate(oldRepo, newRepo *os.File) error
 }
 
