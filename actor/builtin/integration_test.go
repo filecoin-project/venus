ee564e848db1b80998ee58b4b2230e5a4e5428c0
rkowalick <rkowalick@users.noreply.github.com>
2019-04-25 12:05:47 -0400

    more test cases

--- actor/builtin/integration_test.go
+++ actor/builtin/integration_test.go
@@ -46,7 +46,7 @@ func TestVerifyPieceInclusionInRedeem(t *testing.T) {
 	payerActor := th.RequireNewAccountActor(require.New(t), types.NewAttoFILFromFIL(50000))
 	state.MustSetActor(st, payer, payerActor)
 
-	channelID := establishChannel(ctx, st, vms, payer, target, 0, types.NewAttoFILFromFIL(1000), types.NewBlockHeight(20000))
+	channelID := establishChannel(st, vms, payer, target, 0, types.NewAttoFILFromFIL(1000), types.NewBlockHeight(20000))
 
 	t.Run("Voucher with piece inclusion condition and correct proof succeeds", func(t *testing.T) {
 		// create voucher with piece inclusion condition
@@ -70,6 +70,63 @@ func TestVerifyPieceInclusionInRedeem(t *testing.T) {
 
 		require.NoError(t, err)
 		require.NoError(t, appResult.ExecutionError)
+	})
+
+	t.Run("Voucher with piece inclusion condition and wrong miner fails", func(t *testing.T) {
+		differentMinerAddr := addrGetter()
+		differentMinerActor := miner.NewActor()
+		storage := vms.NewStorage(differentMinerAddr, differentMinerActor)
+		require.NoError(t, (&miner.Actor{}).InitializeState(storage, &miner.State{}))
+		require.NoError(t, st.SetActor(ctx, differentMinerAddr, differentMinerActor))
+
+		// create voucher with piece inclusion condition
+		condition := &types.Predicate{To: differentMinerAddr, Method: "verifyPieceInclusion", Params: []interface{}{commP}}
+
+		amt := types.NewAttoFILFromFIL(100)
+		sig, err := paymentbroker.SignVoucher(channelID, amt, defaultValidAt, payer, condition, mockSigner)
+		require.NoError(t, err)
+		signature := ([]byte)(sig)
+
+		// make redeem request with correct sector id and PIP
+		// TODO: this pip is very fake
+		pip := []byte{}
+		pip = append(pip, commP[:]...)
+		pip = append(pip, commD[:]...)
+		suppliedParams := []interface{}{sectorID, pip}
+		pdata := core.MustConvertParams(payer, channelID, amt, types.NewBlockHeight(0), condition, signature, suppliedParams)
+		msg := types.NewMessage(target, address.PaymentBrokerAddress, 0, types.NewAttoFILFromFIL(0), "redeem", pdata)
+
+		appResult, err := th.ApplyTestMessage(st, vms, msg, types.NewBlockHeight(0))
+
+		require.NoError(t, err)
+		require.Error(t, appResult.ExecutionError)
+		require.Contains(t, appResult.ExecutionError.Error(), "failed to validate voucher condition: sector not committed")
+	})
+
+	t.Run("Voucher with piece inclusion condition and incorrect PIP fails", func(t *testing.T) {
+		// create voucher with piece inclusion condition
+		condition := &types.Predicate{To: minerAddr, Method: "verifyPieceInclusion", Params: []interface{}{commP}}
+
+		amt := types.NewAttoFILFromFIL(100)
+		sig, err := paymentbroker.SignVoucher(channelID, amt, defaultValidAt, payer, condition, mockSigner)
+		require.NoError(t, err)
+		signature := ([]byte)(sig)
+
+		// make redeem request with correct sector id and PIP
+		// TODO: this pip is very fake
+		pip := []byte{}
+		pip = append(pip, commP[:]...)
+		pip = append(pip, commD[:]...)
+		pip[12]++ // Make PIP wrong
+		suppliedParams := []interface{}{sectorID, pip}
+		pdata := core.MustConvertParams(payer, channelID, amt, types.NewBlockHeight(0), condition, signature, suppliedParams)
+		msg := types.NewMessage(target, address.PaymentBrokerAddress, 0, types.NewAttoFILFromFIL(0), "redeem", pdata)
+
+		appResult, err := th.ApplyTestMessage(st, vms, msg, types.NewBlockHeight(0))
+
+		require.NoError(t, err)
+		require.Error(t, appResult.ExecutionError)
+		require.Contains(t, appResult.ExecutionError.Error(), "failed to validate voucher condition: invalid inclusion proof")
 	})
 }
 
@@ -114,7 +171,7 @@ func requireGenesis(ctx context.Context, t *testing.T, targetAddresses ...addres
 	return cst, st, vms
 }
 
-func establishChannel(ctx context.Context, st state.Tree, vms vm.StorageMap, from address.Address, target address.Address, nonce uint64, amt *types.AttoFIL, eol *types.BlockHeight) *types.ChannelID {
+func establishChannel(st state.Tree, vms vm.StorageMap, from address.Address, target address.Address, nonce uint64, amt *types.AttoFIL, eol *types.BlockHeight) *types.ChannelID {
 	pdata := core.MustConvertParams(target, eol)
 	msg := types.NewMessage(from, address.PaymentBrokerAddress, nonce, amt, "createChannel", pdata)
 	result, err := th.ApplyTestMessage(st, vms, msg, types.NewBlockHeight(0))
