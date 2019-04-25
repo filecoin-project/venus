7a0f3ec88c5f428e8f02b35f3445bdf339f1416b
Shannon Wells <shannonwells@users.noreply.github.com>
2019-04-24 18:05:06 -0700

    Repo migrations: CLI support for repo options (#2651)
    * Support CLI options for old repo and new repo prefix:
    * make --old-repo required parameter
    * Better usage
    * Use RepoWrangler interface as deps for MigrationRunner to encapsulate environment access
    * RepoWrangler opens the old repo with readonly access, and creates new repo with timestamp and migration versions
    * RepoMigrationWrangler is provided with migration versions, which it
    stores
    * All Migration versioning and detection is left to the Runner
    * Comment/document interface better

--- tools/migration/main_test.go
+++ tools/migration/main_test.go
@@ -18,7 +18,7 @@ func TestUsage(t *testing.T) {
 	require := req.New(t)
 	assert := ast.New(t)
 	command := mustGetMigrationBinary(require)
-	expected := "Usage:  go-filecoin-migrate (describe|buildonly|migrate) [--verbose]"
+	expected := "go-filecoin-migrate (describe|buildonly|migrate) --old-repo=<repodir> [--new-repo=<newrepo-prefix] [-h|--help] [-v|--verbose]"
 
 	t.Run("bare invocation prints usage but exits with 1", func(t *testing.T) {
 		out, err := exec.Command(command).CombinedOutput()
@@ -44,25 +44,33 @@ func TestOptions(t *testing.T) {
 	require := req.New(t)
 	assert := ast.New(t)
 	command := mustGetMigrationBinary(require)
-	usage := "Usage:  go-filecoin-migrate (describe|buildonly|migrate) [--verbose]"
+	usage := "go-filecoin-migrate (describe|buildonly|migrate) --old-repo=<repodir> [--new-repo=<newrepo-prefix] [-h|--help] [-v|--verbose]"
 
 	t.Run("error when calling with invalid command", func(t *testing.T) {
-		out, err := exec.Command(command, "foo").CombinedOutput()
+		out, err := exec.Command(command, "foo", "--old-repo=something").CombinedOutput()
 		assert.Contains(string(out), "Error: Invalid command: foo")
 		assert.Contains(string(out), usage)
 		assert.Error(err)
 	})
 
 	t.Run("accepts --verbose with valid command", func(t *testing.T) {
-		out, err := exec.Command(command, "describe", "--verbose").CombinedOutput()
-		assert.Contains(string(out), "") // should include describe output when implemented
+		out, err := exec.Command(command, "describe", "--old-repo=something", "--verbose").CombinedOutput()
 		assert.NoError(err)
+		assert.Contains(string(out), "") // should include describe output when implemented
 	})
 
 	t.Run("accepts -v with valid command", func(t *testing.T) {
-		out, err := exec.Command(command, "describe", "--verbose").CombinedOutput()
-		assert.Contains(string(out), "") // should include describe output when implemented
+		out, err := exec.Command(command, "describe", "--old-repo=something", "-v").CombinedOutput()
 		assert.NoError(err)
+		assert.Contains(string(out), "") // should include describe output when implemented
+	})
+
+	t.Run("requires --old-repo argument", func(t *testing.T) {
+		out, err := exec.Command(command, "describe").CombinedOutput()
+		expected := "Error: --old-repo is required"
+		assert.Error(err)
+		assert.Contains(string(out), expected) // should include describe output when implemented
+		assert.Contains(string(out), usage)
 	})
 }
 
