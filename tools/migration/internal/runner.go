522cefefb2b6db8537d8742a4b86801b5ed4e939
Shannon Wells <shannonwells@users.noreply.github.com>
2019-05-09 15:22:34 -0700

    Repo migration version detection and decisioning (#2708)
    * Check the fsrepo version and the repo.Version and figure out what migration to run
    * Running the correct command.
    * Validation is run before performing an install.
    * Use the logger
    * The version on disk is always updated on a successful validation.
    * Bunch of test helper funcs
    
    Also:
    * Not finding a migration is not an error.
    * MigrationRunner.Run returns a struct so main and tests can be smarter about run results
    * Before running the install command we check the new repo version to try to make sure it has been migrated -- just in case.
    * Updates to USAGE

--- tools/migration/internal/runner.go
+++ tools/migration/internal/runner.go
@@ -1,7 +1,22 @@
 package internal
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
+
+	"github.com/filecoin-project/go-filecoin/repo"
+)
+
 // Migration is the interface to all repo migration versions.
 type Migration interface {
+	// Describe returns a list of steps, as a formatted string, that a given Migration will take.
+	// These should correspond to named functions in the given Migration.
+	Describe() string
+
 	// Migrate performs all migration steps for the Migration that implements the interface.
 	// Migrate  expects newRepo to be:
 	//		a directory
@@ -9,9 +24,8 @@ type Migration interface {
 	//      contain a copy of the old repo.
 	Migrate(newRepoPath string) error
 
-	// Describe returns a list of steps, as a formatted string, that a given Migration will take.
-	// These should correspond to named functions in the given Migration.
-	Describe() string
+	// Versions returns valid from and to migration versions for this migration.
+	Versions() (from, to uint)
 
 	// Validate performs validation operations, using oldRepo for comparison.
 	// Validation requirements will be different for every migration.
@@ -26,27 +40,221 @@ type Migration interface {
 	Validate(oldRepoPath, newRepoPath string) error
 }
 
-// MigrationRunner represent a migration command
+// MigrationRunner represents a migration command
 type MigrationRunner struct {
-	logger     *Logger
-	command    string
+	// logger logs to stdout/err and a logfile.
+	logger *Logger
+
+	// command is the migration command to run, passed from the CLI
+	command string
+
+	// oldRepoOpt is the value of --old-repo passed  from the CLI,
+	// expanded homedir where needed
 	oldRepoOpt string
+
+	// newRepoPath is where the to-be-migrated/migrated repo is located,
+	// expanded homedir where needed
+	newRepoPath string
+
+	// MigrationsProvider is a dependency for fetching available migrations
+	// to allow unit tests to supply test migrations without creating test fixtures.
+	MigrationsProvider func() []Migration
 }
 
-// NewMigrationRunner builds a MirgrationRunner for the given command and repo options
-func NewMigrationRunner(logger *Logger, command, oldRepoOpt string) *MigrationRunner {
-	// TODO: Issue #2585 Implement repo migration version detection and upgrade decisioning
-
-	return &MigrationRunner{
-		logger:     logger,
-		command:    command,
-		oldRepoOpt: oldRepoOpt,
+// NewMigrationRunner builds a MigrationRunner for the given command and repo options
+// Returns an error if homepath expansion fails for oldRepoOpt or newRepoOpt
+func NewMigrationRunner(logger *Logger, command, oldRepoOpt, newRepoOpt string) (*MigrationRunner, error) {
+	oldPath, err := homedir.Expand(oldRepoOpt)
+	if err != nil {
+		return nil, err
 	}
+	newPath, err := homedir.Expand(newRepoOpt)
+	if err != nil {
+		return nil, err
+	}
+	return &MigrationRunner{
+		logger:             logger,
+		command:            command,
+		oldRepoOpt:         oldPath,
+		newRepoPath:        newPath,
+		MigrationsProvider: DefaultMigrationsProvider,
+	}, nil
+}
+
+// RunResult stores the needed results of calling Run()
+type RunResult struct {
+	// full path to new repo (migrated or not).
+	// This is blank unless repo was cloned -- if it errors out too early or for "describe"
+	NewRepoPath string
+
+	// Old version and new version. If no applicable migration is found, these will be equal,
+	// and if errors early they will = 0.
+	OldVersion, NewVersion uint
+
+	// Any errors encountered by Run
+	Err error
 }
 
 // Run executes the MigrationRunner
-func (m *MigrationRunner) Run() error {
-	// TODO: Issue #2595 Implement first repo migration
+func (m *MigrationRunner) Run() RunResult {
+	repoVersion, err := m.repoVersion(m.oldRepoOpt)
+	if err != nil {
+		return RunResult{Err: err}
+	}
 
-	return m.logger.Close()
+	targetVersion := m.getTargetMigrationVersion()
+	if repoVersion == targetVersion {
+		m.logger.Print(fmt.Sprintf("Repo up-to-date: binary version %d = repo version %d", repoVersion, m.getTargetMigrationVersion()))
+		return RunResult{OldVersion: repoVersion, NewVersion: repoVersion}
+	}
+
+	var mig Migration
+	if mig, err = m.findMigration(repoVersion); err != nil {
+		return RunResult{
+			Err:        errors.Wrapf(err, "migration check failed"),
+			OldVersion: repoVersion,
+			NewVersion: repoVersion,
+		}
+	}
+	// We just didn't find a migration that applies. This is fine.
+	if mig == nil {
+		return RunResult{
+			OldVersion: repoVersion,
+			NewVersion: repoVersion,
+		}
+	}
+	err = m.runCommand(mig)
+	return RunResult{
+		Err:         err,
+		OldVersion:  repoVersion,
+		NewVersion:  targetVersion,
+		NewRepoPath: m.newRepoPath,
+	}
+}
+
+// runCommand runs the migration command set in the Migration runner.
+func (m *MigrationRunner) runCommand(mig Migration) error {
+	var err error
+
+	_, to := mig.Versions()
+
+	switch m.command {
+	case "describe":
+		m.logger.Print(mig.Describe())
+	case "migrate":
+		if m.newRepoPath, err = CloneRepo(m.oldRepoOpt); err != nil {
+			return errors.Wrap(err, "clone repo failed")
+		}
+		m.logger.Print(fmt.Sprintf("new repo will be at %s", m.newRepoPath))
+
+		if err = mig.Migrate(m.newRepoPath); err != nil {
+			return errors.Wrap(err, "migration failed")
+		}
+		if err = m.validateAndUpdateVersion(to, m.newRepoPath, mig); err != nil {
+			return errors.Wrap(err, "validation failed")
+		}
+		if err = InstallNewRepo(m.oldRepoOpt, m.newRepoPath); err != nil {
+			return errors.Wrap(err, "installation failed")
+		}
+	case "buildonly":
+		if m.newRepoPath, err = CloneRepo(m.oldRepoOpt); err != nil {
+			return err
+		}
+		m.logger.Print(fmt.Sprintf("new repo will be at %s", m.newRepoPath))
+
+		if err = mig.Migrate(m.newRepoPath); err != nil {
+			return errors.Wrap(err, "migration failed")
+		}
+		if err = m.validateAndUpdateVersion(to, m.newRepoPath, mig); err != nil {
+			return errors.Wrap(err, "validation failed")
+		}
+	case "install":
+		if m.newRepoPath == "" {
+			return errors.New("installation failed: new repo path not specified")
+		}
+
+		repoVer, err := repo.ReadVersion(m.newRepoPath)
+		if err != nil {
+			return errors.Wrap(err, "installation failed")
+		}
+		// quick check of version to help prevent install after a failed migration
+		newVer := strconv.FormatUint(uint64(to), 10) // to forestall parsing errors of version
+		if repoVer != newVer {
+			return fmt.Errorf("installation failed: repo has version %s, expected version %s", repoVer, newVer)
+		}
+
+		if err = InstallNewRepo(m.oldRepoOpt, m.newRepoPath); err != nil {
+			return errors.Wrap(err, "installation failed")
+		}
+	}
+	return nil
+}
+
+// repoVersion opens the version file for the given version,
+// gets the version and validates it
+func (m *MigrationRunner) repoVersion(repoPath string) (uint, error) {
+	strVersion, err := repo.ReadVersion(repoPath)
+	if err != nil {
+		return 0, err
+	}
+
+	version, err := strconv.Atoi(strVersion)
+	if err != nil {
+		return 0, errors.Wrap(err, "repo version is corrupt")
+	}
+
+	if version < 0 || version > 10000 {
+		return 0, errors.Errorf("repo version out of range: %s", strVersion)
+	}
+
+	return uint(version), nil
+}
+
+// validateAndUpdateVersion calls the migration's validate function and then bumps
+// the version number in the new repo.
+func (m *MigrationRunner) validateAndUpdateVersion(toVersion uint, newRepoPath string, mig Migration) error {
+	if err := mig.Validate(m.oldRepoOpt, newRepoPath); err != nil {
+		return err
+	}
+	return repo.WriteVersion(newRepoPath, toVersion)
+}
+
+// getTargetMigrationVersion returns the maximum resulting version of any migration available from the provider.
+func (m *MigrationRunner) getTargetMigrationVersion() uint {
+	targetVersion := uint(0)
+	migrations := m.MigrationsProvider()
+	for _, mig := range migrations {
+		_, to := mig.Versions()
+		if to > targetVersion {
+			targetVersion = to
+		}
+	}
+	return targetVersion
+}
+
+// findMigration finds the list of migrations in the MigrationsProvder that is valid for
+//       upgrading current repoVersion
+// returns:
+//       nil + error if >1 valid migration is found, or
+//       the migration to run + nil error
+func (m *MigrationRunner) findMigration(repoVersion uint) (mig Migration, err error) {
+	var applicableMigs []Migration
+	for _, mig := range m.MigrationsProvider() {
+		from, to := mig.Versions()
+		if to != from+1 {
+			log.Printf("refusing multi-version migration from %d to %d", from, to)
+		} else if from == repoVersion {
+			applicableMigs = append(applicableMigs, mig)
+		} else {
+			log.Printf("skipping migration from %d to %d", from, to)
+		}
+	}
+	if len(applicableMigs) == 0 {
+		log.Print(fmt.Sprintf("did not find valid repo migration for version %d", repoVersion))
+		return nil, nil
+	}
+	if len(applicableMigs) > 1 {
+		return nil, fmt.Errorf("found >1 migration for version %d; cannot proceed", repoVersion)
+	}
+	return applicableMigs[0], nil
 }
--- tools/migration/internal/runner_test.go
+++ tools/migration/internal/runner_test.go
@@ -1,26 +1,217 @@
 package internal_test
 
 import (
+	"errors"
 	"io/ioutil"
-	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/go-filecoin/repo"
 	tf "github.com/filecoin-project/go-filecoin/testhelpers/testflags"
 	. "github.com/filecoin-project/go-filecoin/tools/migration/internal"
 )
 
-// TODO: Issue #2595 Implement first repo migration
 func TestMigrationRunner_Run(t *testing.T) {
 	tf.UnitTest(t)
-	dummyLogFile, err := ioutil.TempFile("", "logfile")
-	require.NoError(t, err)
-	logger := NewLogger(dummyLogFile, false)
-	defer func() {
-		require.NoError(t, os.Remove(dummyLogFile.Name()))
-	}()
-	runner := NewMigrationRunner(logger, "describe", "/home/filecoin-symlink")
-	assert.NoError(t, runner.Run())
+
+	repoDir, repoSymlink := RequireSetupTestRepo(t, 0)
+	defer RequireRemoveAll(t, repoDir)
+	defer RequireRemoveAll(t, repoSymlink)
+
+	t.Run("valid command returns error if repo not found", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", "/home/filecoin-symlink", "doesnt/matter")
+		require.NoError(t, err)
+		assert.Error(t, runner.Run().Err, "no filecoin repo found in /home/filecoin-symlink.")
+	})
+
+	t.Run("can set MigrationsProvider", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+		runner.MigrationsProvider = testProviderPasses
+
+		migrations := runner.MigrationsProvider()
+		assert.NotEmpty(t, migrations)
+		assert.NoError(t, runner.Run().Err)
+	})
+
+	t.Run("Does not run the migration if the repo is already up to date", func(t *testing.T) {
+		require.NoError(t, repo.WriteVersion(repoDir, 1))
+
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+		runner.MigrationsProvider = testProviderPasses
+		assert.NoError(t, runner.Run().Err)
+		AssertLogged(t, dummyLogFile, "Repo up-to-date: binary version 1 = repo version 1")
+	})
+
+	t.Run("Returns error when a valid migration is not found", func(t *testing.T) {
+		require.NoError(t, repo.WriteVersion(repoDir, 199))
+
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+		runner.MigrationsProvider = testProviderPasses
+
+		out := CaptureOutput(func() {
+			assert.NoError(t, runner.Run().Err)
+		})
+		assert.Contains(t, out, "skipping migration from 0 to 1")
+		assert.Contains(t, out, "did not find valid repo migration for version 199")
+	})
+
+	t.Run("Returns error when repo version is invalid", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+		runner.MigrationsProvider = testProviderPasses
+
+		require.NoError(t, ioutil.WriteFile(filepath.Join(repoDir, "version"), []byte("-1"), 0644))
+		assert.EqualError(t, runner.Run().Err, "repo version out of range: -1")
+
+		require.NoError(t, repo.WriteVersion(repoDir, 32767))
+		assert.EqualError(t, runner.Run().Err, "repo version out of range: 32767")
+	})
+
+	t.Run("Returns error if version file does not contain an integer string", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+		runner.MigrationsProvider = testProviderPasses
+
+		require.NoError(t, ioutil.WriteFile(filepath.Join(repoDir, "version"), []byte("foo"), 0644))
+		assert.EqualError(t, runner.Run().Err, "repo version is corrupt: strconv.Atoi: parsing \"foo\": invalid syntax")
+	})
+
+	t.Run("describe does not clone repo", func(t *testing.T) {
+		require.NoError(t, repo.WriteVersion(repoSymlink, 0))
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+
+		runner.MigrationsProvider = testProviderPasses
+
+		runResult := runner.Run()
+		require.NoError(t, runResult.Err)
+		assert.Equal(t, "", runResult.NewRepoPath)
+	})
+
+	t.Run("run fails if there is more than 1 applicable migration", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+
+		runner.MigrationsProvider = func() []Migration {
+			return []Migration{
+				&TestMigDoesNothing,
+				&TestMigDoesNothing,
+			}
+		}
+		assert.EqualError(t, runner.Run().Err, "migration check failed: found >1 migration for version 0; cannot proceed")
+	})
+
+	t.Run("run skips multiversion", func(t *testing.T) {
+		dummyLogFile, dummyLogPath := RequireOpenTempFile(t, "logfile")
+		defer RequireRemoveAll(t, dummyLogPath)
+		logger := NewLogger(dummyLogFile, false)
+		runner, err := NewMigrationRunner(logger, "describe", repoSymlink, "")
+		require.NoError(t, err)
+
+		runner.MigrationsProvider = func() []Migration {
+			return []Migration{
+				&TestMigDoesNothing,
+				&TestMigMultiversion,
+			}
+		}
+		output := CaptureOutput(func() {
+			assert.NoError(t, runner.Run().Err)
+		})
+		assert.Contains(t, output, "refusing multi-version migration from 1 to 3")
+	})
+
+	t.Run("newRepoOpt is ignored for commands other than install", func(t *testing.T) {
+	})
+}
+
+func testProviderPasses() []Migration {
+	return []Migration{&TestMigDoesNothing}
+}
+
+func testProviderValidationFails() []Migration {
+	return []Migration{&TestMigFailsValidation}
+}
+
+func testProviderMigrationFails() []Migration {
+	return []Migration{&TestMigFailsMigration}
+}
+
+type TestMigration struct {
+	describeFunc func() string
+	migrateFunc  func(string) error
+	versionsFunc func() (uint, uint)
+	validateFunc func(string, string) error
+}
+
+func (m *TestMigration) Describe() string {
+	return m.describeFunc()
+}
+
+func (m *TestMigration) Migrate(newRepoPath string) error {
+	return m.migrateFunc(newRepoPath)
+}
+func (m *TestMigration) Versions() (from, to uint) {
+	return m.versionsFunc()
+}
+
+func (m *TestMigration) Validate(oldRepoPath, newRepoPath string) error {
+	return m.validateFunc(oldRepoPath, newRepoPath)
+}
+
+var TestMigFailsValidation = TestMigration{
+	describeFunc: func() string { return "migration fails validation step" },
+	versionsFunc: func() (uint, uint) { return 0, 1 },
+	migrateFunc:  func(string) error { return nil },
+	validateFunc: func(string, string) error { return errors.New("validation has failed") },
+}
+
+var TestMigDoesNothing = TestMigration{
+	describeFunc: func() string { return "the migration that doesn't do anything" },
+	versionsFunc: func() (uint, uint) { return 0, 1 },
+	migrateFunc:  func(string) error { return nil },
+	validateFunc: func(string, string) error { return nil },
+}
+
+var TestMigFailsMigration = TestMigration{
+	describeFunc: func() string { return "migration fails migration step" },
+	versionsFunc: func() (uint, uint) { return 0, 1 },
+	migrateFunc:  func(string) error { return errors.New("migration has failed") },
+	validateFunc: func(string, string) error { return nil },
+}
+
+var TestMigMultiversion = TestMigration{
+	describeFunc: func() string { return "the migration that skips a version" },
+	versionsFunc: func() (uint, uint) { return 1, 3 },
+	migrateFunc:  func(string) error { return nil },
+	validateFunc: func(string, string) error { return nil },
 }
