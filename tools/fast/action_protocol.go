5df376c2c8d87a34ceda1146f9a75d1f8bf3239e
Rose Karr <rose@rosalinekarr.com>
2019-06-14 08:58:05 -0400

    Added fast actions for the protocol command

---
+++ tools/fast/action_protocol.go
@@ -0,0 +1,18 @@
+package fast
+
+import (
+	"context"
+
+	"github.com/filecoin-project/go-filecoin/porcelain"
+)
+
+// Protocol runs the `protocol` command against the filecoin process
+func (f *Filecoin) Protocol(ctx context.Context) (*porcelain.ProtocolParams, error) {
+	var out porcelain.ProtocolParams
+
+	if err := f.RunCmdJSONWithStdin(ctx, nil, &out, "go-filecoin", "protocol"); err != nil {
+		return nil, err
+	}
+
+	return &out, nil
+}
