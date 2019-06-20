cb8db951502548738c8fc54f27b672f469dfbe36
Alex North <445306+anorth@users.noreply.github.com>
2019-06-20 11:18:32 +1000

    Add a timer for syncer.SyncOne, the duration to validate a tipset (#2957)

--- metrics/timer_test.go
+++ metrics/timer_test.go
@@ -14,7 +14,7 @@ func TestTimerSimple(t *testing.T) {
 
 	ctx := context.Background()
 
-	testTimer := NewTimer("testName", "testDesc")
+	testTimer := NewTimerMs("testName", "testDesc")
 	// some view state is kept around after tests exit, doing this to clean that up.
 	// e.g. view will remain registered after a test exits.
 	defer view.Unregister(testTimer.view)
@@ -40,8 +40,8 @@ func TestDuplicateTimersPanics(t *testing.T) {
 		// we pass
 	}()
 
-	NewTimer("testName", "testDesc")
-	testTimer := NewTimer("testName", "testDesc")
+	NewTimerMs("testName", "testDesc")
+	testTimer := NewTimerMs("testName", "testDesc")
 	assert.Equal(t, "testName", testTimer.view.Name)
 	assert.Equal(t, "testDesc", testTimer.view.Description)
 
@@ -57,8 +57,8 @@ func TestMultipleTimers(t *testing.T) {
 	ctx1 := context.Background()
 	ctx2 := context.Background()
 
-	tt1 := NewTimer("tt1", "ttd1")
-	tt2 := NewTimer("tt2", "ttd2")
+	tt1 := NewTimerMs("tt1", "ttd1")
+	tt2 := NewTimerMs("tt2", "ttd2")
 
 	sw1 := tt1.Start(ctx1)
 	sw2 := tt2.Start(ctx2)
