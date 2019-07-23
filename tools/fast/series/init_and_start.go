e5044ba7e60a360b408dc6db44e87aad9bf228c9
Travis Person <travisperson@users.noreply.github.com>
2019-07-23 10:35:51 -0700

    Add optional func argument to InitAndStart series
    go-filecoin can only be configured while the daemon is running using the
    cli. However, there are times when we need to change configuration prior
    to starting the daemon. This additional func argument allows for making
    changes to the on disk configuration using process.Config /
    process.WriteConfig.

--- tools/fast/series/init_and_start.go
+++ tools/fast/series/init_and_start.go
@@ -6,10 +6,18 @@ import (
 	"github.com/filecoin-project/go-filecoin/tools/fast"
 )
 
-// InitAndStart is a quick way to run Init and Start for a filecoin process.
-func InitAndStart(ctx context.Context, node *fast.Filecoin) error {
+// InitAndStart is a quick way to run Init and Start for a filecoin process, between calls
+// to init and start, an optional function can be passed to perform any actions (generally
+// configuration) prior to the daemon starting.
+func InitAndStart(ctx context.Context, node *fast.Filecoin, fns ...func(context.Context, *fast.Filecoin) error) error {
 	if _, err := node.InitDaemon(ctx); err != nil {
 		return err
+	}
+
+	for _, fn := range fns {
+		if err := fn(ctx, node); err != nil {
+			return err
+		}
 	}
 
 	if _, err := node.StartDaemon(ctx, true); err != nil {
