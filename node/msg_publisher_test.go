165532e4b229b9cdff5a4a1620a0573698770c27
shannonwells <shannonwells@users.noreply.github.com>
2019-07-24 10:34:14 -0700

    use test cases struct, check that messages are always added to pool

--- node/msg_publisher_test.go
+++ node/msg_publisher_test.go
@@ -20,20 +20,29 @@ func TestDefaultMessagePublisher_Publish(t *testing.T) {
 	msg := types.NewMessage(ms.Addresses[0], ms.Addresses[1], 0, types.ZeroAttoFIL, "", []byte{})
 	signed, err := types.NewSignedMessage(*msg, ms, types.ZeroAttoFIL, types.NewGasUnits(0))
 	require.NoError(t, err)
+	msgCid, err := signed.Cid()
+	require.NoError(t, err)
 
-	t.Run("Publish is called when bcast is true", func(t *testing.T) {
-		mnp := mockNetworkPublisher{}
-		pub := newDefaultMessagePublisher(&mnp, "Stuff", pool)
-		assert.NoError(t, pub.Publish(context.Background(), signed, 0, true))
-		assert.Equal(t, "Stuff", mnp.Topic)
-	})
+	testCases := []struct {
+		name     string
+		bcast    bool
+		expected string
+	}{
+		{"Msg added to pool and Publish is called when bcast is true", true, "Stuff"},
+		{"Msg added to pool and Publish is NOT called when bcast is false", false, ""},
+	}
 
-	t.Run("Publish is not called when bcast is false", func(t *testing.T) {
-		mnp := mockNetworkPublisher{}
-		pub := newDefaultMessagePublisher(&mnp, "Stuff", pool)
-		assert.NoError(t, pub.Publish(context.Background(), signed, 0, false))
-		assert.Empty(t, mnp.Topic)
-	})
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			mnp := mockNetworkPublisher{}
+			pub := newDefaultMessagePublisher(&mnp, "Stuff", pool)
+			assert.NoError(t, pub.Publish(context.Background(), signed, 0, test.bcast))
+			smsg, ok := pool.Get(msgCid)
+			assert.True(t, ok)
+			assert.NotNil(t, smsg)
+			assert.Equal(t, test.expected, mnp.Topic)
+		})
+	}
 }
 
 type mockNetworkPublisher struct {
