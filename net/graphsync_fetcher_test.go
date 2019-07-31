d5a0138ce59f0e9ccd28c02a3c8535ab290362fb
hannahhoward <hannah@hannahhoward.net>
2019-07-31 15:19:19 -0700

    test(net): improve graphsync fetcher tests
    Add more unit testing for graphsync fetcher

--- net/graphsync_fetcher_test.go
+++ net/graphsync_fetcher_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"reflect"
 	"testing"
 
 	"github.com/filecoin-project/go-filecoin/address"
@@ -14,6 +15,7 @@ import (
 	tf "github.com/filecoin-project/go-filecoin/testhelpers/testflags"
 	"github.com/filecoin-project/go-filecoin/types"
 	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	dss "github.com/ipfs/go-datastore/sync"
 	"github.com/ipfs/go-graphsync"
@@ -30,8 +32,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type requestKey struct {
-	link     ipld.Link
+type mockRequest struct {
+	root     ipld.Link
 	selector selector.Selector
 }
 
@@ -40,31 +42,17 @@ type mockResponse struct {
 	errs      []error
 }
 
-type mockableGraphsync struct {
-	mockedResponses  map[requestKey]mockResponse
-	receivedRequests []requestKey
+type requestResponse struct {
+	request  mockRequest
+	response mockResponse
 }
 
-func (mgs *mockableGraphsync) Request(ctx context.Context, p peer.ID, root ipld.Link, selectorSpec ipld.Node) (<-chan graphsync.ResponseProgress, <-chan error) {
-	parsed, err := selector.ParseSelector(selectorSpec)
-	if err != nil {
-		errChan := make(chan error, 1)
-		errChan <- fmt.Errorf("Invalid selector")
-		close(errChan)
-		responseChan := make(chan graphsync.ResponseProgress)
-		close(responseChan)
-		return responseChan, errChan
-	}
-	mgs.receivedRequests = append(mgs.receivedRequests, requestKey{root, parsed})
-	mr, ok := mgs.mockedResponses[requestKey{root, parsed}]
-	if !ok {
-		errChan := make(chan error, 1)
-		errChan <- fmt.Errorf("Failed Request")
-		close(errChan)
-		responseChan := make(chan graphsync.ResponseProgress)
-		close(responseChan)
-		return responseChan, errChan
-	}
+type mockableGraphsync struct {
+	stubs            []requestResponse
+	receivedRequests []mockRequest
+}
+
+func toChans(mr mockResponse) (<-chan graphsync.ResponseProgress, <-chan error) {
 	errChan := make(chan error, len(mr.errs))
 	for _, err := range mr.errs {
 		errChan <- err
@@ -80,38 +68,120 @@ func (mgs *mockableGraphsync) Request(ctx context.Context, p peer.ID, root ipld.
 	return responseChan, errChan
 }
 
-func TestGraphsyncFetcherGetSingleLayerHappyPath(t *testing.T) {
+func (mgs *mockableGraphsync) Request(ctx context.Context, p peer.ID, root ipld.Link, selectorSpec ipld.Node) (<-chan graphsync.ResponseProgress, <-chan error) {
+	parsed, err := selector.ParseSelector(selectorSpec)
+	if err != nil {
+		return toChans(mockResponse{nil, []error{fmt.Errorf("Invalid selector")}})
+	}
+	request := mockRequest{root, parsed}
+	mgs.receivedRequests = append(mgs.receivedRequests, request)
+	for _, stub := range mgs.stubs {
+		if reflect.DeepEqual(stub.request, request) {
+			return toChans(stub.response)
+		}
+	}
+	return toChans(mockResponse{nil, []error{fmt.Errorf("Failed Request")}})
+}
+
+func makeGsResponse(path string, blockCid cid.Cid) graphsync.ResponseProgress {
+	return graphsync.ResponseProgress{
+		Path: ipld.ParsePath(path),
+		LastBlock: struct {
+			Path ipld.Path
+			Link ipld.Link
+		}{
+			Link: cidlink.Link{Cid: blockCid},
+		},
+	}
+}
+
+func TestGraphsyncFetcher(t *testing.T) {
 	tf.UnitTest(t)
 	ctx := context.Background()
 	bs := bstore.NewBlockstore(dss.MutexWrap(datastore.NewMapDatastore()))
 	bv := th.NewFakeBlockValidator()
 
-	block1 := types.NewBlockForTest(nil, uint64(0))
-	block2 := types.NewBlockForTest(nil, uint64(1))
-	block3 := types.NewBlockForTest(nil, uint64(3))
-
-	originalCids := types.NewTipSetKey(block1.Cid(), block2.Cid(), block3.Cid())
-
-	mockedResponses := make(map[requestKey]mockResponse)
-	selector, err := selector.NewSelectorSpecBuilder(ipldfree.NodeBuilder()).Matcher().Selector()
+	ssb := selector.NewSelectorSpecBuilder(ipldfree.NodeBuilder())
+	layer1Selector, err := ssb.Matcher().Selector()
 	require.NoError(t, err)
-	mockedResponses[requestKey{cidlink.Link{Cid: block1.Cid()}, selector}] = mockResponse{}
-	mockedResponses[requestKey{cidlink.Link{Cid: block2.Cid()}, selector}] = mockResponse{}
-	mockedResponses[requestKey{cidlink.Link{Cid: block3.Cid()}, selector}] = mockResponse{}
-	mgs := &mockableGraphsync{mockedResponses: mockedResponses}
-
-	fetcher := net.NewGraphSyncFetcher(ctx, mgs, bs, bv)
-
-	err = fetcher.GetSingleLayer(ctx, originalCids.ToSlice(), peer.ID("fake"))
+	gsSelector, err := ssb.ExploreRecursive(1, ssb.ExploreFields(func(efsb selector.ExploreFieldsSpecBuilder) {
+		efsb.Insert("parents", ssb.ExploreUnion(
+			ssb.ExploreAll(ssb.Matcher()),
+			ssb.ExploreIndex(0, ssb.ExploreRecursiveEdge()),
+		))
+	})).Selector()
 	require.NoError(t, err)
-	require.Equal(t, 3, len(mgs.receivedRequests))
-	fetchedCids := types.NewTipSetKey(
-		mgs.receivedRequests[0].link.(cidlink.Link).Cid,
-		mgs.receivedRequests[1].link.(cidlink.Link).Cid,
-		mgs.receivedRequests[2].link.(cidlink.Link).Cid,
-	)
 
-	require.True(t, originalCids.Equals(fetchedCids))
+	t.Run("happy path returns correct tipsets", func(t *testing.T) {
+		parentBlock := types.NewBlockForTest(nil, uint64(0))
+		block1 := types.NewBlockForTest(parentBlock, uint64(3))
+		block2 := types.NewBlockForTest(parentBlock, uint64(5))
+		block3 := types.NewBlockForTest(parentBlock, uint64(6))
+		requireBlockStorePut(t, bs, parentBlock.ToNode())
+		requireBlockStorePut(t, bs, block1.ToNode())
+		requireBlockStorePut(t, bs, block2.ToNode())
+		requireBlockStorePut(t, bs, block3.ToNode())
+		originalCids := types.NewTipSetKey(block1.Cid(), block2.Cid(), block3.Cid())
+		parentCids := types.NewTipSetKey(parentBlock.Cid())
+
+		stubs := []requestResponse{
+			requestResponse{mockRequest{cidlink.Link{Cid: block1.Cid()}, layer1Selector}, mockResponse{}},
+			requestResponse{mockRequest{cidlink.Link{Cid: block2.Cid()}, layer1Selector}, mockResponse{}},
+			requestResponse{mockRequest{cidlink.Link{Cid: block3.Cid()}, layer1Selector}, mockResponse{}},
+			requestResponse{mockRequest{cidlink.Link{Cid: originalCids.ToSlice()[0]}, gsSelector}, mockResponse{
+				responses: []graphsync.ResponseProgress{
+					makeGsResponse("", block1.Cid()),
+					makeGsResponse("parents", block1.Cid()),
+					makeGsResponse("parents/0", parentBlock.Cid()),
+				},
+			}},
+		}
+		mgs := &mockableGraphsync{stubs: stubs}
+
+		fetcher := net.NewGraphSyncFetcher(ctx, mgs, bs, bv)
+
+		done := func(ts types.TipSet) (bool, error) {
+			if ts.Key().Equals(parentCids) {
+				return true, nil
+			}
+			return false, nil
+		}
+
+		ts, err := fetcher.FetchTipSets(ctx, originalCids, peer.ID("fake"), done)
+		require.NoError(t, err, "the request completes successfully")
+		require.Equal(t, 4, len(mgs.receivedRequests), "all expected graphsync requests are made")
+		require.Equal(t, 2, len(ts), "the right number of tipsets is returned")
+		require.True(t, originalCids.Equals(ts[0].Key()), "the initial tipset is correct")
+		require.True(t, parentCids.Equals(ts[1].Key()), "the remaining tipsets are correct")
+	})
+
+	t.Run("value returned with non block format", func(t *testing.T) {
+		notABlock := types.NewMsgs(1)[0]
+		notABlockObj, err := notABlock.ToNode()
+		require.NoError(t, err)
+
+		requireBlockStorePut(t, bs, notABlockObj)
+		notABlockCid, err := notABlock.Cid()
+		require.NoError(t, err)
+		originalCids := types.NewTipSetKey(notABlockCid)
+
+		stubs := []requestResponse{
+			requestResponse{mockRequest{cidlink.Link{Cid: notABlockCid}, layer1Selector}, mockResponse{}},
+		}
+		mgs := &mockableGraphsync{stubs: stubs}
+
+		fetcher := net.NewGraphSyncFetcher(ctx, mgs, bs, bv)
+
+		done := func(ts types.TipSet) (bool, error) {
+			if ts.Key().Equals(originalCids) {
+				return true, nil
+			}
+			return false, nil
+		}
+		ts, err := fetcher.FetchTipSets(ctx, originalCids, peer.ID("fake"), done)
+		require.Errorf(t, err, "fetched data (cid %s) was not a block", notABlockCid.String())
+		require.Nil(t, ts)
+	})
 }
 
 func TestRealWorldGraphsyncFetchAcrossNetwork(t *testing.T) {
