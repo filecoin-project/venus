810e29c5de12183f2d26c913b21f28deba9fee16
Travis Person <travisperson@users.noreply.github.com>
2019-08-13 10:07:16 -0700

    Add FAST actions show messages / show receipts

--- tools/fast/action_show.go
+++ tools/fast/action_show.go
@@ -20,3 +20,29 @@ func (f *Filecoin) ShowHeader(ctx context.Context, ref cid.Cid) (*types.Block, e
 
 	return &out, nil
 }
+
+// ShowMessages runs the `show messages` command against the filecoin process
+func (f *Filecoin) ShowMessages(ctx context.Context, ref cid.Cid) ([]*types.SignedMessage, error) {
+	var out []*types.SignedMessage
+
+	sRef := ref.String()
+
+	if err := f.RunCmdJSONWithStdin(ctx, nil, &out, "go-filecoin", "show", "messages", sRef); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+// ShowReceipts runs the `show receipts` command against the filecoin process
+func (f *Filecoin) ShowReceipts(ctx context.Context, ref cid.Cid) ([]*types.MessageReceipt, error) {
+	var out []*types.MessageReceipt
+
+	sRef := ref.String()
+
+	if err := f.RunCmdJSONWithStdin(ctx, nil, &out, "go-filecoin", "show", "receipts", sRef); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
