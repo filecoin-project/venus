22aff8be65db93e54ce730cdd53c95a7bffc7a72
Ignacio Corderi <icorderi@msn.com>
2019-08-16 15:23:37 +0200

    misc: download parameters only for 1kb sector size
    Closes #3152

--- scripts/install-filecoin-parameters.sh
+++ scripts/install-filecoin-parameters.sh
@@ -3,11 +3,11 @@
 set -Eeo pipefail
 
 fetch_params() {
-  ./proofs/bin/paramfetch --all --verbose --json=./proofs/misc/parameters.json || true
+  ./proofs/bin/paramfetch -z 1024 --verbose --json=./proofs/misc/parameters.json || true
 }
 
 generate_params() {
-  RUST_LOG=info ./proofs/bin/paramcache
+  RUST_LOG=info ./proofs/bin/paramcache --test-only
 }
 
 fetch_params
