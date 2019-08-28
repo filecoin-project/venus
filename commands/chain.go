150c84e0a0fb3e0e12b07d8eefe4a3d7f226cb0a
frrist <forrest@protocol.ai>
2019-08-28 14:47:11 -0700

    expose chain syncer on CLI

--- commands/chain.go
+++ commands/chain.go
@@ -10,6 +10,8 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-ipfs-cmdkit"
 	"github.com/ipfs/go-ipfs-cmds"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/pkg/errors"
 
 	"github.com/filecoin-project/go-filecoin/types"
 )
@@ -19,12 +21,14 @@ var chainCmd = &cmds.Command{
 		Tagline: "Inspect the filecoin blockchain",
 	},
 	Subcommands: map[string]*cmds.Command{
-		"head": chainHeadCmd,
-		"ls":   chainLsCmd,
+		"head":        storeHeadCmd,
+		"ls":          storeLsCmd,
+		"sync-status": syncStatusCmd,
+		"sync-tipset": syncTipSetCmd,
 	},
 }
 
-var chainHeadCmd = &cmds.Command{
+var storeHeadCmd = &cmds.Command{
 	Helptext: cmdkit.HelpText{
 		Tagline: "Get heaviest tipset CIDs",
 	},
@@ -49,7 +53,7 @@ var chainHeadCmd = &cmds.Command{
 	},
 }
 
-var chainLsCmd = &cmds.Command{
+var storeLsCmd = &cmds.Command{
 	Helptext: cmdkit.HelpText{
 		Tagline:          "List blocks in the blockchain",
 		ShortDescription: `Provides a list of blocks in order from head to genesis. By default, only CIDs are returned for each block.`,
@@ -106,5 +110,56 @@ var chainLsCmd = &cmds.Command{
 
 			return nil
 		}),
+	},
+}
+
+var syncStatusCmd = &cmds.Command{
+	Helptext: cmdkit.HelpText{
+		Tagline: "Show status of chain sync operation.",
+	},
+	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) error {
+		syncStatus := GetPorcelainAPI(env).ChainSyncStatus()
+		if err := re.Emit(syncStatus); err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+var syncTipSetCmd = &cmds.Command{
+	Helptext: cmdkit.HelpText{
+		Tagline: "Sync and validate a chain.",
+	},
+	Arguments: []cmdkit.Argument{
+		cmdkit.StringArg("peerid", true, false, "Base58-encoded libp2p peer ID that a chain will be synced from"),
+		cmdkit.StringArg("cid", true, false, "CID of the TipSet to sync"),
+	},
+	Options: []cmdkit.Option{
+		cmdkit.BoolOption("trusted", "Trust the TipSet when syncing").WithDefault(true),
+		cmdkit.Uint64Option("height", "Height of the TipSet to sync").WithDefault(0),
+	},
+	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) error {
+		peer, err := peer.IDB58Decode(req.Arguments[0])
+		if err != nil {
+			return err
+		}
+
+		tsCid, err := cid.Parse(req.Arguments[1])
+		if err != nil {
+			return errors.Wrap(err, "invalid cid "+req.Arguments[0])
+		}
+		head := types.NewTipSetKey(tsCid)
+
+		trusted := req.Options["trusted"].(bool)
+		height, err := strconv.ParseUint(req.Options["height"].(string), 10, 64)
+		if err != nil {
+			return err
+		}
+		ci := types.NewChainInfo(peer, head, height)
+		syncStatus := GetPorcelainAPI(env).ChainSyncHandleNewTipSet(req.Context, ci, trusted)
+		if err := re.Emit(syncStatus); err != nil {
+			return err
+		}
+		return nil
 	},
 }
