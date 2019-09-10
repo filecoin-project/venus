3b7ec0cda61b9abb5b08b909bd2490a872a344d0
ognots <ognots@protonmail.com>
2019-09-10 09:44:31 -0400

    disable paramfetch until optimizations are made
    paramfetch can sometimes timeout, causing builds to fail and blocking general development pipeline.
    problems are especially prevelant for outside committers during CircleCI builds, as they are unable to use the CircleCI cache storing the parameters.

--- scripts/install-filecoin-parameters.sh
+++ scripts/install-filecoin-parameters.sh
@@ -10,5 +10,11 @@ generate_params() {
   RUST_LOG=info ./proofs/bin/paramcache --test-only
 }
 
-fetch_params
+# Disabling paramfetch until optimizations can be completed
+# Fetching parameters, even small ones, has a long history of being slow and unreliable.
+# It's disabled now in favour of local generation of the small parameter files needed
+# for tests.
+# On a 2015 MacBook pro, generation takes about a minute, one-time.
+#
+# fetch_params
 generate_params
