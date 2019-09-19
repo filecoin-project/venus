5ee4bd4de6da6604309918179fd91efca617f203
hannahhoward <hannah@hannahhoward.net>
2019-09-19 13:25:47 -0700

    feat(clock): add timer functionality
    Adds timer functionality to clock and fake clock, so we can use timers but still test them easily

--- clock/clock.go
+++ clock/clock.go
@@ -2,11 +2,18 @@ package clock
 
 import (
 	"github.com/jonboulle/clockwork"
+	"time"
 )
 
 // Clock provides an interface that packages can use instead of directly
 // using the time module, so that chronology-related behavior can be tested.
 type Clock interface {
+	clockwork.Clock
+	NewTimer(d time.Duration) Timer
+	AfterFunc(d time.Duration, f func()) Timer
+}
+
+type realClock struct {
 	clockwork.Clock
 }
 
@@ -14,5 +21,13 @@ type Clock interface {
 // SystemClock is a Clock which simply delegates calls to the actual time
 // package; it should be used by packages in production.
 func NewSystemClock() Clock {
-	return clockwork.NewRealClock()
+	return &realClock{Clock: clockwork.NewRealClock()}
+}
+
+func (rc *realClock) NewTimer(d time.Duration) Timer {
+	return &realTimer{time.NewTimer(d)}
+}
+
+func (rc *realClock) AfterFunc(d time.Duration, f func()) Timer {
+	return &realTimer{time.AfterFunc(d, f)}
 }
