d2e0a39d370846b67c33eb9cbc123a62506d3d7f
hannahhoward <hannah@hannahhoward.net>
2019-09-26 11:44:37 -0700

    WIP

--- net/graphsync_session_test.go
+++ net/graphsync_session_test.go
@@ -1,8 +1,13 @@
 package net_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
+	"github.com/ipfs/go-datastore"
+	dss "github.com/ipfs/go-datastore/sync"
+	bstore "github.com/ipfs/go-ipfs-blockstore"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/stretchr/testify/require"
 
@@ -14,6 +19,10 @@ import (
 
 func TestDefaultRequestPeerFinder(t *testing.T) {
 	tf.UnitTest(t)
+	bs := bstore.NewBlockstore(dss.MutexWrap(datastore.NewMapDatastore()))
+	clock := th.NewFakeClock(time.Now())
+	ctx := context.Background()
+	mgs := newMockableGraphsync(ctx, bs, clock, t)
 	pid0 := th.RequireIntPeerID(t, 0)
 	pid1 := th.RequireIntPeerID(t, 1)
 	pid2 := th.RequireIntPeerID(t, 2)
@@ -22,9 +31,11 @@ func TestDefaultRequestPeerFinder(t *testing.T) {
 	chain2 := types.NewChainInfo(pid2, types.UndefTipSet.Key(), 0)
 
 	t.Run("when originating peer is self", func(t *testing.T) {
+
 		fpt := th.NewFakePeerTracker(pid1, chain0, chain1, chain2)
-		rpff := net.MakeDefaultRequestPeerFinderFactory(fpt)
-		rpf, err := rpff(pid1)
+		gse := net.MakeDefaultGraphsyncSessionExchange(mgs, clock, fpt)
+		gss, err := gse.NewSession(ctx, pid1)
+		rpf := gss.(*net.DefaultGraphsyncSession)
 		require.NoError(t, err)
 		require.Equal(t, rpf.CurrentPeers(), []peer.ID{pid1})
 		err = rpf.FindNextPeers()
@@ -39,8 +50,9 @@ func TestDefaultRequestPeerFinder(t *testing.T) {
 
 	t.Run("when originating peer is not self", func(t *testing.T) {
 		fpt := th.NewFakePeerTracker(pid1, chain0, chain1, chain2)
-		rpff := net.MakeDefaultRequestPeerFinderFactory(fpt)
-		rpf, err := rpff(pid2)
+		gse := net.MakeDefaultGraphsyncSessionExchange(mgs, clock, fpt)
+		gss, err := gse.NewSession(ctx, pid1)
+		rpf := gss.(*net.DefaultGraphsyncSession)
 		require.NoError(t, err)
 		require.Contains(t, []peer.ID{pid0, pid1, pid2}, rpf.CurrentPeers()[0])
 		err = rpf.FindNextPeers()
@@ -55,8 +67,9 @@ func TestDefaultRequestPeerFinder(t *testing.T) {
 
 	t.Run("when more peers are added to peer tracker", func(t *testing.T) {
 		fpt := th.NewFakePeerTracker(pid1, chain0)
-		rpff := net.MakeDefaultRequestPeerFinderFactory(fpt)
-		rpf, err := rpff(pid2)
+		gse := net.MakeDefaultGraphsyncSessionExchange(mgs, clock, fpt)
+		gss, err := gse.NewSession(ctx, pid2)
+		rpf := gss.(*net.DefaultGraphsyncSession)
 		require.NoError(t, err)
 		require.Equal(t, []peer.ID{pid0}, rpf.CurrentPeers())
 		fpt.SetList(chain1)
@@ -73,9 +86,9 @@ func TestDefaultRequestPeerFinder(t *testing.T) {
 
 	t.Run("when no peers are available", func(t *testing.T) {
 		fpt := th.NewFakePeerTracker(pid1)
-		rpff := net.MakeDefaultRequestPeerFinderFactory(fpt)
-		rpf, err := rpff(pid2)
-		require.Nil(t, rpf)
+		gse := net.MakeDefaultGraphsyncSessionExchange(mgs, clock, fpt)
+		gss, err := gse.NewSession(ctx, pid2)
+		require.Nil(t, gss)
 		require.EqualError(t, err, "Unable to find any untried peers")
 	})
 }
