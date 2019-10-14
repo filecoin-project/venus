6774553ef03975f1799e64bcfb8e1c68fe337d8f
ZenGround0 <5515260+ZenGround0@users.noreply.github.com>
2019-10-14 09:26:34 -0400

    Remove old chain building code (#3549)
    - Removes TestWaitConflicting, the last consumer

--- plumbing/msg/waiter_test.go
+++ plumbing/msg/waiter_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-filecoin/chain"
-	"github.com/filecoin-project/go-filecoin/consensus"
 	th "github.com/filecoin-project/go-filecoin/testhelpers"
 	tf "github.com/filecoin-project/go-filecoin/testhelpers/testflags"
 	"github.com/filecoin-project/go-filecoin/types"
@@ -46,11 +45,6 @@ type smsgsSet [][]*types.SignedMessage
 
 func setupTest(t *testing.T) (*hamt.CborIpldStore, *chain.Store, *chain.MessageStore, *Waiter) {
 	d := requiredCommonDeps(t, th.DefaultGenesis)
-	return d.cst, d.chainStore, d.messages, NewWaiter(d.chainStore, d.messages, d.blockstore, d.cst)
-}
-
-func setupTestWithGif(t *testing.T, gif consensus.GenesisInitFunc) (*hamt.CborIpldStore, *chain.Store, *chain.MessageStore, *Waiter) {
-	d := requiredCommonDeps(t, gif)
 	return d.cst, d.chainStore, d.messages, NewWaiter(d.chainStore, d.messages, d.blockstore, d.cst)
 }
 
@@ -128,98 +122,6 @@ func testWaitError(ctx context.Context, t *testing.T, cst *hamt.CborIpldStore, c
 	assert.Nil(t, err)
 
 	testWaitHelp(nil, t, waiter, m2, true, nil)
-}
-
-func TestWaitConflicting(t *testing.T) {
-	tf.UnitTest(t)
-
-	ctx := context.Background()
-
-	addr1, addr2, addr3 := mockSigner.Addresses[0], mockSigner.Addresses[1], mockSigner.Addresses[2]
-	worker1, worker2 := mockSigner.Addresses[0], mockSigner.Addresses[1]
-	// create a valid miner
-	minerAddr := mockSigner.Addresses[3]
-
-	testGen := consensus.MakeGenesisFunc(
-		consensus.ActorAccount(addr1, types.NewAttoFILFromFIL(10000)),
-		consensus.ActorAccount(addr2, types.NewAttoFILFromFIL(0)),
-		consensus.ActorAccount(addr3, types.NewAttoFILFromFIL(0)),
-		consensus.MinerActor(minerAddr, addr3, th.RequireRandomPeerID(t), types.ZeroAttoFIL, types.OneKiBSectorSize),
-	)
-	cst, chainStore, msgStore, waiter := setupTestWithGif(t, testGen)
-
-	// Create conflicting messages
-	m1 := types.NewMessage(addr1, addr3, 0, types.NewAttoFILFromFIL(6000), "", nil)
-	sm1, err := types.NewSignedMessage(*m1, &mockSigner, types.NewGasPrice(1), types.NewGasUnits(0))
-	require.NoError(t, err)
-
-	m2 := types.NewMessage(addr1, addr2, 0, types.NewAttoFILFromFIL(6000), "", nil)
-	sm2, err := types.NewSignedMessage(*m2, &mockSigner, types.NewGasPrice(1), types.NewGasUnits(0))
-	require.NoError(t, err)
-
-	head := chainStore.GetHead()
-	headTipSet, err := chainStore.GetTipSet(head)
-	require.NoError(t, err)
-	baseTS := headTipSet
-	require.Equal(t, 1, baseTS.Len())
-	baseBlock := baseTS.ToSlice()[0]
-
-	require.NotNil(t, chainStore.GenesisCid())
-
-	emptyReceiptsCid, err := msgStore.StoreReceipts(ctx, []*types.MessageReceipt{})
-	require.NoError(t, err)
-
-	b1 := th.RequireMkFakeChild(t,
-		th.FakeChildParams{
-			MinerAddr:   minerAddr,
-			Parent:      baseTS,
-			GenesisCid:  chainStore.GenesisCid(),
-			StateRoot:   baseBlock.StateRoot,
-			Signer:      mockSigner,
-			MinerWorker: worker1,
-		})
-	sm1Cid, err := msgStore.StoreMessages(ctx, []*types.SignedMessage{sm1})
-	require.NoError(t, err)
-	b1.Messages = sm1Cid
-	b1.MessageReceipts = emptyReceiptsCid
-	b1.Tickets = []types.Ticket{{VRFProof: []byte{0}}} // block 1 comes first in message application
-	mustPut(cst, b1)
-
-	b2 := th.RequireMkFakeChild(t,
-		th.FakeChildParams{
-			MinerAddr:   minerAddr,
-			Parent:      baseTS,
-			GenesisCid:  chainStore.GenesisCid(),
-			StateRoot:   baseBlock.StateRoot,
-			Signer:      mockSigner,
-			MinerWorker: worker2,
-		})
-	sm2Cid, err := msgStore.StoreMessages(ctx, []*types.SignedMessage{sm2})
-	require.NoError(t, err)
-	b2.Messages = sm2Cid
-	b2.MessageReceipts = emptyReceiptsCid
-	b2.Tickets = []types.Ticket{{VRFProof: []byte{1}}}
-	mustPut(cst, b2)
-
-	ts := th.RequireNewTipSet(t, b1, b2)
-	require.NoError(t, chainStore.PutTipSetAndState(ctx, &chain.TipSetAndState{
-		TipSet:          ts,
-		TipSetStateRoot: baseBlock.StateRoot,
-	}))
-	assert.NoError(t, chainStore.SetHead(ctx, ts))
-	msgApplySucc := func(b *types.Block, msg *types.SignedMessage,
-		rcp *types.MessageReceipt) error {
-		assert.NotNil(t, rcp)
-		return nil
-	}
-	msgApplyFail := func(b *types.Block, msg *types.SignedMessage,
-		rcp *types.MessageReceipt) error {
-		assert.Nil(t, rcp)
-		return nil
-	}
-
-	testWaitHelp(nil, t, waiter, sm1, false, msgApplySucc)
-	testWaitHelp(nil, t, waiter, sm2, false, msgApplyFail)
 }
 
 func TestWaitRespectsContextCancel(t *testing.T) {
