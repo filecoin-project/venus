d9c0a1e881729c7ed3168025e7d872552b8952d7
Ignacio Corderi <icorderi@msn.com>
2019-10-18 14:50:56 -0700

    xxx gen main

--- tools/encoding_gen/main.go
+++ tools/encoding_gen/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/filecoin-project/go-filecoin/encoding/gen"
 	"github.com/filecoin-project/go-filecoin/types"
+	whygen "github.com/whyrusleeping/cbor-gen"
 )
 
 func main() {
@@ -14,6 +15,17 @@ func main() {
 		types.Message{},
 		types.SignedMessage{},
 		types.MessageReceipt{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = whygen.WriteTupleEncodersToFile("/tmp/types_whygen.go", "types",
+		types.Ticket{},
+		// types.Message{}, AttoFil needs to be part of it too
+		// types.SignedMessage{},
+		// types.MessageReceipt{}, XXX: it has a uint8 that is not supproted by whygen
 	)
 	if err != nil {
 		fmt.Println(err)
