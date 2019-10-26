49b42ab08d26391edbfab1c8403419781be5ba27
Ignacio Corderi <icorderi@msn.com>
2019-10-26 12:43:55 -0400

    xxx

--- build/main.go
+++ build/main.go
@@ -307,9 +307,8 @@ func test(userArgs ...string) {
 
 	begin := time.Now()
 	runCmd(cmd(
-		"bash", "-c", fmt.Sprintf("go test %s %s %s",
+		"bash", "-c", fmt.Sprintf("go test %s %s",
 			strings.Replace(packages, "\n", " ", -1),
-			flags(),
 			strings.Join(userArgs, " "))))
 	end := time.Now()
 	log.Printf("Tests finished in %.1f seconds\n", end.Sub(begin).Seconds())
