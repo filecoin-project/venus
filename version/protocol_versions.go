db01e3c8d82d397268d4c334d3f8d8d0e8f74441
anorth <anorth@users.noreply.github.com>
2019-11-01 13:50:00 -0400

    Apply the new weight function from round 0 on all networks

--- version/protocol_versions.go
+++ version/protocol_versions.go
@@ -27,10 +27,8 @@ const Protocol1 = 1
 // network upgrades need to be represented here. See #3491.
 func ConfigureProtocolVersions(network string) (*ProtocolVersionTable, error) {
 	return NewProtocolVersionTableBuilder(network).
-		Add(USER, Protocol0, types.NewBlockHeight(0)).
-		Add(USER, Protocol1, types.NewBlockHeight(43000)).
-		Add(DEVNET4, Protocol0, types.NewBlockHeight(0)).
-		Add(DEVNET4, Protocol1, types.NewBlockHeight(300)).
+		Add(USER, Protocol1, types.NewBlockHeight(0)).
+		Add(DEVNET4, Protocol1, types.NewBlockHeight(0)).
 		Add(LOCALNET, Protocol1, types.NewBlockHeight(0)).
 		Add(TEST, Protocol1, types.NewBlockHeight(0)).
 		Build()
