59b8378ece830094c137c571a9db86390fa25b93
Ignacio Corderi <icorderi@msn.com>
2019-11-11 18:54:50 -0500

    xxx new interfaces

---
+++ internal/pkg/vm/internal/result/result.go
@@ -0,0 +1,38 @@
+package result
+
+import (
+	"github.com/filecoin-project/go-filecoin/internal/pkg/encoding"
+	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/internal/exitcode"
+)
+
+// InvocOutput is what is returned by an actor executing a method.
+type InvocOutput struct {
+	ExitCode    exitcode.ExitCode
+	ReturnValue []byte
+}
+
+// Success returns an empty succesfull result.
+func Success() InvocOutput {
+	return InvocOutput{ExitCode: 0}
+}
+
+// Value returns a successfull code with the value encoded.
+//
+// Callers do NOT need to encode the value before calling this method.
+// Review: for @spec, @go-filecoin, changed this to an interface so the caller doesnt have to do the serialization himself
+func Value(obj interface{}) InvocOutput {
+	aux, err := encoding.Encode(obj)
+	if err != nil {
+		return InvocOutput{ExitCode: exitcode.EncodingError}
+	}
+
+	return InvocOutput{
+		ExitCode:    0,
+		ReturnValue: aux,
+	}
+}
+
+// Error returns an error output.
+func Error(exitCode exitcode.ExitCode) InvocOutput {
+	return InvocOutput{ExitCode: exitCode}
+}
--- internal/pkg/vm/internal/runtime/runtime.go
+++ internal/pkg/vm/internal/runtime/runtime.go
@@ -8,74 +8,85 @@ import (
 	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/address"
 )
 
-
-
 // Runtime2 is the VM.
-// Review: the object that implements this outlives actor calls.
 type Runtime2 struct {
 	CurerntEpoch() Epoch
-	// Review: should we turn the offset into a RandomnessEvent?
+	// Review: for @go-filecoin, should we turn the offset into a RandomnessEvent?
 	Randomness(epoch Epoch, offset uint64) Randomness
-	// Review: not sure why the original has a messagereceipt instead of an InvocOutput
-	Send(input InvocInput) InvocOutput
-	// Review: how does this work? What qualifies as an "error"?
-	// Review: how is this any different than the caller just ignoring a returned error?
-	// SendAllowingErrors(input msg.InvocInput) msg.MessageReceipt
-
-	// Review: we might want a global Storage() Storage here
+	Send(input InvocInput) result.InvocOutput
+	// SharedStorage is an IPLD store shared by all actors.
+	// Review: for @go-filecoin, we can skip this until we have actor code that actually needs this
+	// Review: for @spec, Add expiration (Epoch's) on PUT? (the question is mostly for global, but the local store might want to advise on this too).
+	// Review: for @spec, how will gas accounting work on global? PUT fee, GET fee, OWNER Fee?
+	SharedStorage() IpldStorage2
 }
 
-// InvocationContext is passed to the actors on each method call.
-// Review: this type + Runtime2 is mostly what the spec defines as Runtime.
+// DispatchContext is the context the actor dispatcher receives.
+// Review: for @go-filecoin, do we want to have the `ctx.ValidateCaller(blahblah)` at the top of each method, or hidden away on the dispatch table?
+type DispatchContext interface {
+	// Review: for @go-filecoin, simplified the API to be just one method.
+	ValidateCaller(CallerPattern)
+	// InvocationContext is the context to be passed down the actor method.
+	InvocationContext() InvocationContext
+}
+
+// InvocationContext is passed to the actors on each method call..
 type InvocationContext interface {
-	// Review: grouped the VM level things out of the invocation itself
+	// Review: for @go-filecoin, composition or inheritance?
 	Runtime() Runtime2
 	Caller() address.Address
-	// Review: (is in spec, dont think we need it) idem to Caller().Equal(addr) :/
-	// ValidateCallerIs(caller addr.Address)
-	// Review: not sure what all this pattern matching adds up to, the "singleton" pattern does an equality check
-	// ValidateCallerMatches(CallerPattern)
-	// Review: the spec handles state and state changes a bit differently
-	// Review: our Storage is a bit of a mix of AcquireState and the Ipld{Get-Put}
-	// AcquireState() ActorStateHandle
+	StateHandler() ActorStateHandler
 
-	// Review: The spec suggest handling return construction by the runtime, interesting but not strictly necessary
-	// SuccessReturn()     msg.InvocOutput
-	// ValueReturn(Bytes)  msg.InvocOutput
-	// ErrorReturn(exitCode exitcode.ExitCode) msg.InvocOutput
-
-	// Review: how does this work? is the spec expecting us to Panic? or just terminate the message execution?
-	// Review: this should be `Abort(string) -> msg.InvocOutput` if we follow the pattern
-	// Abort(string)
-
-	// Review: this is a bit trickier than abort in Go. This is a conditional early exit on the function which we cannot do.
-	// Review: `if <cond> { return AssertFailure("blah") }` is a bit closer to go if we are to try to follow this pattern.
-	// Assert(bool)
-
-	// Review: we didnt have this before, why does the actor need to know the value supplied?
-	ValueSupplied() types.AttoFIL
-
-	// Review: is the expectation fo storage to be global or actor specific? this has security implications down the line
-	Storage() Storage
-
-	// Dragons: this was not in the spce, but it was likely a bug
-	// Note: @jzimmerman I think that's an error in the spec -- balance should be accessible via the runtime.
+	// Review: for @go-filecoin+@spec, renamed `ValueSupplied` to `ValueReceived`. The "supplied" is not clear by whom.
+	ValueReceived() types.AttoFIL
 	Balance() types.AttoFIL
+
+	LocalStorage() IpldStorage2
 }
 
 // initInvocationContext is a more powerfull context for the "init" actor.
 type initInvocationContext interface {
 	InvocationContext
-	// Review: added an error that is not yet on the spec but should be
+	// Review: for @spec, added an error that is not yet on the spec but should be
 	// Note: @jzimmerman: "Agreed, this should support an error return."
 	CreateActor(cid cid.Cid, addr address.Address, constructorParams MethodParams) error
 }
 
-// Storage2 defines the storage module exposed to actors.
-// Review: simpler API (check type Storage for comments on the things that got removed)
-type Storage2 interface {
+// minerInvocationContext has some special sauce for the miner.
+// Review: for @go-filecoin, do we want custom interfaces for the miners instead of grouping a bunch fo methods on the InvocationContext?
+type minerInvocationContext interface {
+	Verifier() verification.Verifier
+}
+
+// ActorStateHandler handles the actor state, allowing actors to lock on the state.
+type ActorStateHandler interface {
+	// Take returns the `Cid` for the state and locks the state.
+	//
+	// Any futre calls to `Take` by other instances will block until `Release` or `UpdateRelease` is called.
+	// Review: for @spec, the spec impl of `Take` is not doing any blocking, just panic'ing on the second call on the SAME object instance (this objects are not kept around, so it is always a new instance..).
+	Take() cid.Cid
+	// UpdateRelease updates the actor state and releases the lock on it.
+	//
+	// No future calls to `Take` are allowed on this object.
+	// Review: for @spec, this method can currently be called without having called `Take` first.
+	UpdateRelease(newStateCID ActorSubstateCID)
+	// Release asserts that the state has not changed and releases the lock on it.
+	//
+	// No future calls to `Take` are allowed on this object.
+	// Review: for @spec, this method can currently be called without having called `Take` first.
+	// Review: for @spec, why is this method required?
+	Release(checkStateCID ActorSubstateCID)
+}
+
+// IpldStorage2 defines the storage module exposed to actors.
+type IpldStorage2 interface {
 	Put(interface{}) (cid.Cid, error)
 	Get(cid.Cid, interface{}) error
+}
+
+// CallerPattern checks if the caller matches the pattern.
+type CallerPattern interface {
+	IsMatch() bool
 }
 
 // Epoch is the current chain epoch.
@@ -88,8 +99,6 @@ type Epoch types.BlockHeight
 type Randomness []byte
 
 // InvocInput are the params to invoke a method in an Actor.
-// Review: unless anyone in @go-filecoin has a strong preference for arguments, we are chaging to the struct the spec suggests.
-// Note: @jzimmerman mentioned we might want to reffer to the inputs by Cid in the future
 type InvocInput struct {
     To      address.Address
     Method  types.MethodID
@@ -97,63 +106,33 @@ type InvocInput struct {
     Value   types.AttoFIL
 }
 
-// ExitCode is the exit code of a method executing inside the VM.
-type ExitCode types.Int64
-
-// InvocOutput is waht os returned by an actor executing a method.
-// Review: multiple returns vs a return struct. There are some minor implications with swapping vm implementations written in another language.
-type InvocOutput struct {
-	ExitCode     ExitCode
-    ReturnValue  []byte
-}
-
 // MethodParam is the parameter to an Actor method already encoded for execution in the VM.
 type MethodParam []byte
 // MethodParams is a list of `MethodParam` to be passed into an Actor method.
 type MethodParams []MethodParam
+
+// Abort will stop the VM execution and return an abort error to the caller.
+// Review: @go-filecoin, do we want to handle this with panics?
+func Abort(msg string) {
+	panic("byte me")
+}
+
+// Assert will abort if the condition is `False` and return an abort error to the caller.
+func Assert(cond bool) {
+	if !cond {
+		Abort("assertion failure in actor code.")
+	}
+}
+
+//
+// Dragons: this will get deleted, still here while we finish some discussions on the PR.
+//
 
 // Runtime defines the ABI interface exposed to actors.
 type Runtime interface {
 	// Review: this is not in the spec ~anymore~ yet..
 	// Note: @jzimmerman: still a WIP, things like the miner actor might need it
 	Message() *types.UnsignedMessage
-
-	// The spec splits this up and brings the get/put to be first class,
-	// Review: go-filecoin should keep it as a composition
-	Storage() Storage
-
-	// Changed from arguments to a struct
-	// Note: we will likely go with the struct, check comments on `type InvocInput`
-	Send(to address.Address, method types.MethodID, value types.AttoFIL, params []interface{}) ([][]byte, uint8, error)
-
-	// This is not in the spec anymore
-	// Note: @jzimmerman: this functionality is delicate enough security-wise (especially with respect to different addresses on different forks) that I think it makes sense not to expose it directly.
-	AddressForNewActor() (address.Address, error)
-
- 	// Review: renamed and has a new type
-	BlockHeight() *types.BlockHeight
-
-	// This is not in the spec anymore, but it should be
-	// Note: @jzimmerman I think that's an error in the spec -- balance should be accessible via the runtime.
-	MyBalance() types.AttoFIL
-
-	// got replaceed by ValidateCallerIs and ValidateCallerMAtches
-	IsFromAccountActor() bool
-
-	// This is not in the spec anymore.
-	// Note: @jzimmerman: "Gas accounting is still WIP"
-	// TODO: come back to this when Gas accounting lands on spec
-	Charge(cost types.GasUnits) error
-
-	// Note: this got renamed and gained an offset arg
-	SampleChainRandomness(sampleHeight *types.BlockHeight) ([]byte, error)
-
-	// unchanged
-	CreateNewActor(addr address.Address, code cid.Cid, initalizationParams interface{}) error
-
-	// Review: this is not in the spec anymore
-	// Note: the spec seems to do a direct call, that has some implications (see github comments).
-	Verifier() verification.Verifier
 }
 
 // Storage defines the storage module exposed to actors.
