d91accbc95d0790d06f2fd1c9ed14afa045b8f70
laser <l@s3r.me>
2020-01-22 14:37:09 -0800

    rename retrieval miner to retrieval provider

---
+++ internal/pkg/protocol/retrieval/provider.go
@@ -0,0 +1,12 @@
+package retrieval
+
+// Miner is...
+type Miner struct {
+}
+
+// NewMiner is ...
+func NewMiner() *Miner {
+	panic("replace this with retrieval market from go-fil-markets")
+
+	return &Miner{} // nolint:govet
+}
