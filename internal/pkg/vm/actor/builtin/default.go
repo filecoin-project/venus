d2e7b302fa089ba8bb4036671466714a8b5770ab
Ignacio Corderi <icorderi@msn.com>
2020-02-19 14:05:29 -0800

    xxx

--- internal/pkg/vm/actor/builtin/default.go
+++ internal/pkg/vm/actor/builtin/default.go
@@ -4,14 +4,14 @@ import (
 	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/internal/dispatch"
 	specs "github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/account"
+	notinit "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	"github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	"github.com/filecoin-project/specs-actors/actors/builtin/power"
-	notinit "github.com/filecoin-project/specs-actors/actors/builtin/init"
 )
 
 // DefaultActors is list of all actors that ship with Filecoin.
 // They are indexed by their CID.
-// Dragons: add the rest of the actors 
+// Dragons: add the rest of the actors
 var DefaultActors = dispatch.NewBuilder().
 	Add(specs.InitActorCodeID, &notinit.Actor{}).
 	Add(specs.AccountActorCodeID, &account.Actor{}).
