5c8e3615ac79ff7063c841c082c193f1e3fece33
Ignacio Corderi <icorderi@msn.com>
2020-02-21 12:15:16 -0800

    misc: remove legacy code (#3798)
    * misc: delete actor storage lookup
    * misc: remove unused actor code

--- internal/pkg/vm/actor/actor.go
+++ internal/pkg/vm/actor/actor.go
@@ -8,7 +8,6 @@ import (
 
 	fxamackercbor "github.com/fxamacker/cbor"
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/pkg/errors"
 
@@ -16,8 +15,6 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 
 	e "github.com/filecoin-project/go-filecoin/internal/pkg/enccid"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/encoding"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/types"
 )
 
 // DefaultGasCost is default gas cost for the actor calls.
@@ -71,43 +68,18 @@ func (a *Actor) IncrementSeqNum() {
 	a.CallSeqNum = a.CallSeqNum + 1
 }
 
-// Cid returns the canonical CID for the actor.
-// TODO: can we avoid returning an error?
-func (a *Actor) Cid() (cid.Cid, error) {
-	bs, err := encoding.Encode(a)
-	if err != nil {
-		return cid.Undef, errors.Wrap(err, "failed to marshal to cbor")
-	}
-	obj, err := cbor.Decode(bs, types.DefaultHashFunction, -1)
-	if err != nil {
-		return cid.Undef, errors.Wrap(err, "failed to cast cbor bytes to ipld node")
-	}
-
-	return obj.Cid(), nil
-}
-
-// Unmarshal a actor from the given bytes.
-func (a *Actor) Unmarshal(b []byte) error {
-	return fxamackercbor.Unmarshal(b, a)
-}
-
-// Marshal the actor into bytes.
-func (a *Actor) Marshal() ([]byte, error) {
-	return fxamackercbor.Marshal(a, fxamackercbor.EncOptions{})
-}
-
 // UnmarshalCBOR must implement cbg.Unmarshaller to insert this into a hamt.
 func (a *Actor) UnmarshalCBOR(r io.Reader) error {
 	bs, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
-	return a.Unmarshal(bs)
+	return fxamackercbor.Unmarshal(bs, a)
 }
 
 // MarshalCBOR must implement cbg.Marshaller to insert this into a hamt.
 func (a *Actor) MarshalCBOR(w io.Writer) error {
-	bs, err := a.Marshal()
+	bs, err := fxamackercbor.Marshal(a, fxamackercbor.EncOptions{})
 	if err != nil {
 		return err
 	}
@@ -117,7 +89,7 @@ func (a *Actor) MarshalCBOR(w io.Writer) error {
 
 // Format implements fmt.Formatter.
 func (a *Actor) Format(f fmt.State, c rune) {
-	f.Write([]byte(fmt.Sprintf("<%s (%p); balance: %v; nonce: %d>", NameByCode(a.Code.Cid), a, a.Balance, a.CallSeqNum))) // nolint: errcheck
+	f.Write([]byte(fmt.Sprintf("<%s (%p); balance: %v; nonce: %d>", builtin.ActorNameByCode(a.Code.Cid), a, a.Balance, a.CallSeqNum))) // nolint: errcheck
 }
 
 // NextNonce returns the nonce value for an account actor, which is the nonce expected on the
@@ -131,10 +103,4 @@ func NextNonce(actor *Actor) (uint64, error) {
 		return 0, errors.New("next nonce only defined for account or empty actors")
 	}
 	return actor.CallSeqNum, nil
-}
-
-// NameByCode returns the (string) name of the Go type of the actor with cid, code.
-// Dragons: remove once we get rid of the bootstrap actor
-func NameByCode(code cid.Cid) string {
-	return builtin.ActorNameByCode(code)
 }
--- internal/pkg/vm/actor/actor_test.go
+++ internal/pkg/vm/actor/actor_test.go
@@ -4,34 +4,14 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
-	e "github.com/filecoin-project/go-filecoin/internal/pkg/enccid"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/types"
 	. "github.com/filecoin-project/go-filecoin/internal/pkg/vm/actor"
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 
 	tf "github.com/filecoin-project/go-filecoin/internal/pkg/testhelpers/testflags"
 )
-
-func TestActorCid(t *testing.T) {
-	tf.UnitTest(t)
-
-	actor1 := NewActor(builtin.AccountActorCodeID, abi.NewTokenAmount(0))
-	actor2 := NewActor(builtin.AccountActorCodeID, abi.NewTokenAmount(5))
-	actor2.Head = e.NewCid(requireCid(t, "Actor 2 State"))
-	actor1.IncrementSeqNum()
-
-	c1, err := actor1.Cid()
-	assert.NoError(t, err)
-	c2, err := actor2.Cid()
-	assert.NoError(t, err)
-
-	assert.NotEqual(t, c1.String(), c2.String())
-}
 
 func TestActorFormat(t *testing.T) {
 	tf.UnitTest(t)
@@ -50,11 +30,4 @@ func TestActorFormat(t *testing.T) {
 	storageMarketActor := NewActor(builtin.StorageMarketActorCodeID, abi.NewTokenAmount(5))
 	formatted = fmt.Sprintf("%v", storageMarketActor)
 	assert.Contains(t, formatted, "market")
-}
-
-func requireCid(t *testing.T, data string) cid.Cid {
-	prefix := cid.V1Builder{Codec: cid.Raw, MhType: types.DefaultHashFunction}
-	cid, err := prefix.Sum([]byte(data))
-	require.NoError(t, err)
-	return cid
 }
