c87cc6aa78246769f3c3f8bd3c58d0b82868e8f1
frrist <forrest@protocol.ai>
2020-03-02 11:24:03 -0800

    update chain-validation and add test skips

--- internal/pkg/vm/internal/vmcontext/validation_test.go
+++ internal/pkg/vm/internal/vmcontext/validation_test.go
@@ -1,36 +1,61 @@
 package vmcontext
 
 import (
+	"fmt"
+	"reflect"
+	"runtime"
 	"testing"
 
-	vsuites "github.com/filecoin-project/chain-validation/suites"
+	"github.com/filecoin-project/chain-validation/suites"
+	"github.com/filecoin-project/chain-validation/suites/message"
 )
 
-func TestChainValidationSuite(t *testing.T) {
-	f := NewFactories()
-
-	vsuites.TestValueTransferSimple(t, f)
-	vsuites.TestValueTransferAdvance(t, f)
-	vsuites.TestAccountActorCreation(t, f)
-
-	vsuites.TestInitActorSequentialIDAddressCreate(t, f)
-
-	// Skipping since multisig address resolution breaks tests
-	// https://github.com/filecoin-project/specs-actors/issues/184
-	// vsuites.TestMultiSigActor(t, f)
-
-	// Skipping since payment channel because runtime sys calls are not implemented in runtime adapter
-	// vsuites.TestPaych(t, f)
+// TestSkipper contains a list of test cases skipped by the implementation.
+type TestSkipper struct {
+	testSkips []suites.TestCase
 }
 
-func TestMessageApplication(t *testing.T) {
-	f := NewFactories()
-
-	vsuites.TestMessageApplicationEdgecases(t, f)
+// Skip return true if the sutire.TestCase should be skipped.
+func (ts *TestSkipper) Skip(test suites.TestCase) bool {
+	for _, skip := range ts.testSkips {
+		if reflect.ValueOf(skip).Pointer() == reflect.ValueOf(test).Pointer() {
+			fmt.Printf("=== SKIP   %v\n", runtime.FuncForPC(reflect.ValueOf(test).Pointer()).Name())
+			return true
+		}
+	}
+	return false
 }
 
-func TestTipSetApplication(t *testing.T) {
-	f := NewFactories()
+// TestSuiteSkips contains tests we wish to skip.
+var TestSuiteSkipper TestSkipper
 
-	vsuites.TestBlockMessageInfoApplication(t, f)
+func init() {
+	// initialize the test skipper with tests being skipped
+	TestSuiteSkipper = TestSkipper{testSkips: []suites.TestCase{
+		// Skipping since payment channel because runtime sys calls are not implemented in runtime adapter
+		message.TestPaych,
+		// Skipping since multisig address resolution breaks tests
+		// https://github.com/filecoin-project/specs-actors/issues/184
+		message.TestMultiSigActor,
+	}}
+}
+
+func TestChainValidationMessageSuite(t *testing.T) {
+	f := NewFactories()
+	for _, testCase := range suites.MessageTestCases() {
+		if TestSuiteSkipper.Skip(testCase) {
+			continue
+		}
+		testCase(t, f)
+	}
+}
+
+func TestChainValidationTipSetSuite(t *testing.T) {
+	f := NewFactories()
+	for _, testCase := range suites.TipSetTestCases() {
+		if TestSuiteSkipper.Skip(testCase) {
+			continue
+		}
+		testCase(t, f)
+	}
 }
