72a67291a10f4f571fa0803e17b4b18474d9f53e
laser <l@s3r.me>
2020-03-03 15:52:40 -0800

    fix comment

--- internal/pkg/constants/sector_size.go
+++ internal/pkg/constants/sector_size.go
@@ -5,5 +5,5 @@ import "github.com/filecoin-project/specs-actors/actors/abi"
 // DevSectorSize is a tiny sector useful only for testing.
 var DevSectorSize = abi.SectorSize(2048)
 
-// TwoHundredFiftySixMiBSectorSize contain 256MiB after sealing.
+// FiveHundredTwelveMiBSectorSize contain 512MiB after sealing.
 var FiveHundredTwelveMiBSectorSize = abi.SectorSize(512 << 20)
