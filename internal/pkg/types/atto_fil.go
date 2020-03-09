81aa0e9d51c225e11149b4d05fbf467fc30d3451
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-03-09 11:54:07 -0400

    Remove fixed point altogether

--- internal/pkg/types/atto_fil.go
+++ internal/pkg/types/atto_fil.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"math/big"
 	"strings"
 
@@ -86,4 +87,18 @@ func NewAttoFILFromString(s string, base int) (AttoFIL, bool) {
 	out := specsbig.NewInt(0)
 	_, isErr := out.Int.SetString(s, base)
 	return out, isErr
+}
+
+// BigToUint64 converts a big Int to a uint64.  It will error if
+// the Int is too big to fit into 64 bits or is negative
+func BigToUint64(bi fbig.Int) (uint64, error) {
+	if !bi.Int.IsUint64() {
+		return 0, fmt.Errorf("Int: %s could not be represented as uint64", bi.String())
+	}
+	return bi.Uint64(), nil
+}
+
+// Uint64ToBig converts a uint64 to a big Int.  Precodition: don't overflow int64.
+func Uint64ToBig(u uint64) fbig.Int {
+	return fbig.NewInt(int64(u))
 }
