9a736662d5b45b4d20659a466a674d7dc3f17202
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-03-09 13:41:04 -0400

    Fix import path

--- internal/pkg/types/atto_fil.go
+++ internal/pkg/types/atto_fil.go
@@ -91,7 +91,7 @@ func NewAttoFILFromString(s string, base int) (AttoFIL, bool) {
 
 // BigToUint64 converts a big Int to a uint64.  It will error if
 // the Int is too big to fit into 64 bits or is negative
-func BigToUint64(bi fbig.Int) (uint64, error) {
+func BigToUint64(bi specsbig.Int) (uint64, error) {
 	if !bi.Int.IsUint64() {
 		return 0, fmt.Errorf("Int: %s could not be represented as uint64", bi.String())
 	}
@@ -99,6 +99,6 @@ func BigToUint64(bi fbig.Int) (uint64, error) {
 }
 
 // Uint64ToBig converts a uint64 to a big Int.  Precodition: don't overflow int64.
-func Uint64ToBig(u uint64) fbig.Int {
-	return fbig.NewInt(int64(u))
+func Uint64ToBig(u uint64) specsbig.Int {
+	return specsbig.NewInt(int64(u))
 }
