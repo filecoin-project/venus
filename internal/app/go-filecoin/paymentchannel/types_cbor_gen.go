a46472bc2adb78b325a922959965afab5a285933
shannonwells <shannonwells@users.noreply.github.com>
2020-03-10 11:20:35 -0700

    regen types_cbor_gen

--- internal/app/go-filecoin/paymentchannel/types_cbor_gen.go
+++ internal/app/go-filecoin/paymentchannel/types_cbor_gen.go
@@ -18,17 +18,12 @@ func (t *ChannelInfo) MarshalCBOR(w io.Writer) error {
 		_, err := w.Write(cbg.CborNull)
 		return err
 	}
-	if _, err := w.Write([]byte{134}); err != nil {
+	if _, err := w.Write([]byte{133}); err != nil {
 		return err
 	}
 
 	// t.UniqueAddr (address.Address) (struct)
 	if err := t.UniqueAddr.MarshalCBOR(w); err != nil {
-		return err
-	}
-
-	// t.IDAddr (address.Address) (struct)
-	if err := t.IDAddr.MarshalCBOR(w); err != nil {
 		return err
 	}
 
@@ -74,7 +69,7 @@ func (t *ChannelInfo) UnmarshalCBOR(r io.Reader) error {
 		return fmt.Errorf("cbor input should be of type array")
 	}
 
-	if extra != 6 {
+	if extra != 5 {
 		return fmt.Errorf("cbor input had wrong number of fields")
 	}
 
@@ -83,15 +78,6 @@ func (t *ChannelInfo) UnmarshalCBOR(r io.Reader) error {
 	{
 
 		if err := t.UniqueAddr.UnmarshalCBOR(br); err != nil {
-			return err
-		}
-
-	}
-	// t.IDAddr (address.Address) (struct)
-
-	{
-
-		if err := t.IDAddr.UnmarshalCBOR(br); err != nil {
 			return err
 		}
 
