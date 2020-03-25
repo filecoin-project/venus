976bbcdbdf9dfa5a948a339d9320af2d2fe5f8e1
shannonwells <shannonwells@users.noreply.github.com>
2020-03-25 14:41:42 -0700

    updates after rebase

--- internal/app/go-filecoin/paymentchannel/fake_message_sender.go
+++ internal/app/go-filecoin/paymentchannel/fake_message_sender.go
@@ -15,6 +15,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-filecoin/internal/pkg/types"
+	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/gas"
 )
 
 // FakeMessageSender is a message sender that parses message parameters and invokes the
@@ -58,7 +59,7 @@ func (fms *FakeMessageSender) Send(ctx context.Context,
 	from, to address.Address,
 	value types.AttoFIL,
 	gasPrice types.AttoFIL,
-	gasLimit types.GasUnits,
+	gasLimit gas.Unit,
 	bcast bool,
 	method abi.MethodNum,
 	params interface{}) (out cid.Cid, pubErrCh chan error, err error) {
