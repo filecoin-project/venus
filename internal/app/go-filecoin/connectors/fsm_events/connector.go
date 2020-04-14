b815b2e93d6123b6a853c41d58809ae076ee32cf
acruikshank <acruikshank@example.com>
2020-04-14 14:34:19 -0400

    add chain sampler

--- internal/app/go-filecoin/connectors/fsm_events/connector.go
+++ internal/app/go-filecoin/connectors/fsm_events/connector.go
@@ -1,14 +1,17 @@
 package fsmeventsconnector
 
 import (
+	"github.com/filecoin-project/go-filecoin/internal/pkg/chainsampler"
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	fsm "github.com/filecoin-project/storage-fsm"
 )
 
-type FiniteStateMachineEventsConnector struct{}
+type FiniteStateMachineEventsConnector struct {
+	scheduler chainsampler.HeightThresholdScheduler
+}
 
 func (f FiniteStateMachineEventsConnector) ChainAt(hnd fsm.HeightHandler, rev fsm.RevertHandler, confidence int, h abi.ChainEpoch) error {
-	panic("implement me")
+	l := f.scheduler.AddListener(h)
 }
 
 var _ fsm.Events = new(FiniteStateMachineEventsConnector)
