254e201921443362af3a6c86add0211ab6bea7ce
acruikshank <acruikshank@example.com>
2020-05-11 17:10:45 -0400

    WIP

--- internal/pkg/mining/scheduler.go
+++ internal/pkg/mining/scheduler.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/pkg/errors"
-	"gopkg.in/src-d/go-log.v1"
 
 	"github.com/filecoin-project/go-filecoin/internal/pkg/block"
 	"github.com/filecoin-project/go-filecoin/internal/pkg/clock"
@@ -71,7 +70,7 @@ func (s *timingScheduler) Start(miningCtx context.Context) (<-chan Output, *sync
 	return outCh, &doneWg
 }
 
-func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output, doneWg *sync.WaitGroup) {
+func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output, doneWg *sync.WaitGroup) error {
 	// The main event loop for the timing scheduler.
 	// Waits for a new epoch to start, polls the heaviest head, includes the correct number
 	// of null blocks and starts a mining job async.
@@ -81,18 +80,15 @@ func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output,
 	// The scheduler will skip mining jobs if the skipping flag is set
 	prevBase, err := s.pollHeadFunc()
 	if err != nil {
-		log.Errorf("error polling head from mining scheduler %s", err)
-		// FIXME return error
+		return errors.Wrap(err, "error polling head from mining scheduler")
 	}
 	nullCount := uint64(0)
 
-	workContext, workCancel := context.WithCancel(miningCtx)
 	for {
 		// Check for a new base tipset, and reset null count if one is found.
 		base, err := s.pollHeadFunc()
 		if err != nil {
-			log.Errorf("error polling head from mining scheduler %s", err)
-			// FIXME return error
+			return errors.Wrap(err, "error polling head from mining scheduler")
 		}
 		if !base.Key().Equals(prevBase.Key()) {
 			nullCount = 0
@@ -109,7 +105,7 @@ func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output,
 			// There appears to be a gap at the end of the chain.
 			// Rather than leave the gap, attempt to mine blocks to fill it.
 			doneWg.Add(1)
-			s.worker.Mine(workContext, base, nullCount, outCh)
+			s.worker.Mine(miningCtx, base, nullCount, outCh)
 			doneWg.Done()
 
 			// The use of a channel output prevents this code from knowing whether the block it may have just produced
@@ -121,10 +117,9 @@ func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output,
 			select { // check for interrupt during waiting
 			case <-miningCtx.Done():
 				s.isStarted = false
-				return // nolint:govet
+				return nil
 			default:
 			}
-			workCancel() // cancel any late work from last epoch
 		}
 
 		// check if we are skipping and don't mine if so
@@ -133,15 +128,9 @@ func (s *timingScheduler) mineLoop(miningCtx context.Context, outCh chan Output,
 			continue
 		}
 
-		workContext, workCancel = context.WithCancel(miningCtx) // nolint: govet
 		doneWg.Add(1)
-		go func(ctx context.Context) {
-			// Mine is not intended to be re-entrant, but using a go-routine here makes it impossible to enforce
-			// single-threaded access.
-			// Some context in https://github.com/filecoin-project/go-filecoin/issues/4065
-			s.worker.Mine(ctx, base, nullCount, outCh)
-			doneWg.Done()
-		}(workContext)
+		s.worker.Mine(miningCtx, base, nullCount, outCh)
+		doneWg.Done()
 	}
 }
 
