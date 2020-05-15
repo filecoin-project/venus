47741407e7f182a5f452e532c895c92f36787f72
Hannah Howard <hannah@hannahhoward.net>
2020-05-15 13:50:07 -0700

    feat(commands): implement deals list & verify deal (#4132)
    implement two new commands, write integration test for deals list

--- cmd/go-filecoin/deals.go
+++ cmd/go-filecoin/deals.go
@@ -1,7 +1,10 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/ipfs/go-cid"
@@ -31,7 +34,9 @@ type DealsListResult struct {
 	Miner       address.Address `json:"minerAddress"`
 	PieceCid    cid.Cid         `json:"pieceCid"`
 	ProposalCid cid.Cid         `json:"proposalCid"`
+	IsMiner     bool            `json:"isMiner"`
 	State       string          `json:"state"`
+	Message     string          `json:"message"`
 }
 
 var dealsListCmd = &cmds.Command{
@@ -47,10 +52,47 @@ deals, active deals, finished deals and cancelled deals.
 		cmdkit.BoolOption(minerOnly, "m", "only return deals made as a miner"),
 	},
 	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) error {
-		//minerAddress, _ := GetPorcelainAPI(env).ConfigGet("mining.minerAddress")
-		panic("implement me in terms of the storage market module")
+		isClientOnly, _ := req.Options[clientOnly].(bool)
+		isMinerOnly, _ := req.Options[minerOnly].(bool)
+		var clientDeals []storagemarket.ClientDeal
+		var minerDeals []storagemarket.MinerDeal
+		var err error
+		if !isMinerOnly {
+			clientDeals, err = GetStorageAPI(env).GetClientDeals(req.Context)
+			if err != nil {
+				return fmt.Errorf("error reading client deals: %w", err)
+			}
+		}
+		if !isClientOnly {
+			minerDeals, err = GetStorageAPI(env).GetProviderDeals(req.Context)
+			if err != nil {
+				return fmt.Errorf("error reading miner deals: %w", err)
+			}
+		}
+		var formattedDeals []DealsListResult
+		for _, deal := range clientDeals {
+			formattedDeals = append(formattedDeals, DealsListResult{
+				Miner:       deal.Proposal.Provider,
+				PieceCid:    deal.Proposal.PieceCID,
+				ProposalCid: deal.ProposalCid,
+				IsMiner:     false,
+				State:       storagemarket.DealStates[deal.State],
+				Message:     deal.Message,
+			})
+		}
+		for _, deal := range minerDeals {
+			formattedDeals = append(formattedDeals, DealsListResult{
+				Miner:       deal.Proposal.Provider,
+				PieceCid:    deal.Proposal.PieceCID,
+				ProposalCid: deal.ProposalCid,
+				IsMiner:     true,
+				State:       storagemarket.DealStates[deal.State],
+				Message:     deal.Message,
+			})
+		}
+		return re.Emit(formattedDeals)
 	},
-	Type: DealsListResult{},
+	Type: []DealsListResult{},
 }
 
 // DealsShowResult contains Deal output with Payment Vouchers.
