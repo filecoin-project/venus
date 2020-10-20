711e923722dc2f8552ead517c80d262961e8d2f4
hunjixin <1084400399@qq.com>
2020-10-20 14:39:47 +0800

    add file

---
+++ internal/pkg/vm/state/snapshot.go
@@ -0,0 +1,106 @@
+package state
+
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/actor"
+)
+
+type stateSnaps struct {
+	layers                        []*stateSnapLayer
+	lastMaybeNonEmptyResolveCache int
+}
+
+type stateSnapLayer struct {
+	actors       map[address.Address]streeOp
+	resolveCache map[address.Address]address.Address
+}
+
+func newStateSnapLayer() *stateSnapLayer {
+	return &stateSnapLayer{
+		actors:       make(map[address.Address]streeOp),
+		resolveCache: make(map[address.Address]address.Address),
+	}
+}
+
+type streeOp struct {
+	Act    actor.Actor
+	Delete bool
+}
+
+func newStateSnaps() *stateSnaps {
+	ss := &stateSnaps{}
+	ss.addLayer()
+	return ss
+}
+
+func (ss *stateSnaps) addLayer() {
+	ss.layers = append(ss.layers, newStateSnapLayer())
+}
+
+func (ss *stateSnaps) dropLayer() {
+	ss.layers[len(ss.layers)-1] = nil // allow it to be GCed
+
+	ss.layers = ss.layers[:len(ss.layers)-1]
+
+	if ss.lastMaybeNonEmptyResolveCache == len(ss.layers) {
+		ss.lastMaybeNonEmptyResolveCache = len(ss.layers) - 1
+	}
+}
+
+func (ss *stateSnaps) mergeLastLayer() {
+	last := ss.layers[len(ss.layers)-1]
+	nextLast := ss.layers[len(ss.layers)-2]
+
+	for k, v := range last.actors {
+		nextLast.actors[k] = v
+	}
+
+	for k, v := range last.resolveCache {
+		nextLast.resolveCache[k] = v
+	}
+
+	ss.dropLayer()
+}
+
+func (ss *stateSnaps) resolveAddress(addr address.Address) (address.Address, bool) {
+	for i := ss.lastMaybeNonEmptyResolveCache; i >= 0; i-- {
+		if len(ss.layers[i].resolveCache) == 0 {
+			if ss.lastMaybeNonEmptyResolveCache == i {
+				ss.lastMaybeNonEmptyResolveCache = i - 1
+			}
+			continue
+		}
+		resa, ok := ss.layers[i].resolveCache[addr]
+		if ok {
+			return resa, true
+		}
+	}
+	return address.Undef, false
+}
+
+func (ss *stateSnaps) cacheResolveAddress(addr, resa address.Address) {
+	ss.layers[len(ss.layers)-1].resolveCache[addr] = resa
+	ss.lastMaybeNonEmptyResolveCache = len(ss.layers) - 1
+}
+
+func (ss *stateSnaps) getActor(addr address.Address) (*actor.Actor, error) {
+	for i := len(ss.layers) - 1; i >= 0; i-- {
+		act, ok := ss.layers[i].actors[addr]
+		if ok {
+			if act.Delete {
+				return nil, actor.ErrActorNotFound
+			}
+
+			return &act.Act, nil
+		}
+	}
+	return nil, nil
+}
+
+func (ss *stateSnaps) setActor(addr address.Address, act *actor.Actor) {
+	ss.layers[len(ss.layers)-1].actors[addr] = streeOp{Act: *act}
+}
+
+func (ss *stateSnaps) deleteActor(addr address.Address) {
+	ss.layers[len(ss.layers)-1].actors[addr] = streeOp{Delete: true}
+}
