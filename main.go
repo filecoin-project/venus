1cea4ecd2e99d769c8b1341831b333f7fcd12575
一页素书 <2931107265@qq.com>
2020-10-26 15:03:17 +0800

    fix log level

--- main.go
+++ main.go
@@ -12,8 +12,68 @@ import (
 func main() {
 
 	// set default log level if no flags given
+	var level logging.LogLevel
+	var err error
+	lvl := os.Getenv("GO_FILECOIN_LOG_LEVEL")
+	if lvl == "" {
+		level = logging.LevelInfo
+	} else {
+		level, err = logging.LevelFromString(lvl)
+		if err != nil {
+			level = logging.LevelInfo
+		}
+	}
 
-	logging.SetAllLoggers(logging.LevelFatal)
+	errCode := 1
+	logging.SetAllLoggers(level)
+	err = logging.SetLogLevel("dht", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("bitswap", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("graphsync", "info")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("heartbeat", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("blockservice", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("peerqueue", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("swarm", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("swarm2", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("basichost", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("dht_net", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("pubsub", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
+	err = logging.SetLogLevel("relay", "error")
+	if err != nil {
+		os.Exit(errCode)
+	}
 
 	code, _ := commands.Run(context.Background(), os.Args, os.Stdin, os.Stdout, os.Stderr)
 	os.Exit(code)
