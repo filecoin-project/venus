81ba677487687d8b317a126a91b0e751aef67d5a
一页素书 <2931107265@qq.com>
2020-11-25 10:42:25 +0800

    add Marshal/UnMarshal for StateInfo0

--- pkg/vm/state/state.go
+++ pkg/vm/state/state.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -67,6 +68,41 @@ type StateRoot struct { //nolint
 
 // TODO: version this.
 type StateInfo0 struct{} //nolint
+
+var lengthBufStateInfo0 = []byte{128}
+
+func (t *StateInfo0) MarshalCBOR(w io.Writer) error {
+	if t == nil {
+		_, err := w.Write(cbg.CborNull)
+		return err
+	}
+	if _, err := w.Write(lengthBufStateInfo0); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *StateInfo0) UnmarshalCBOR(r io.Reader) error {
+	*t = StateInfo0{}
+
+	br := cbg.GetPeeker(r)
+	scratch := make([]byte, 8)
+
+	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
+	if err != nil {
+		return err
+	}
+	if maj != cbg.MajArray {
+		return fmt.Errorf("cbor input should be of type array")
+	}
+
+	if extra != 0 {
+		return fmt.Errorf("cbor input had wrong number of fields")
+	}
+
+	return nil
+}
 
 // state stores actors state by their ID.
 type State struct {
