fcb60ee452c7240b4c54069aea76bfb5cb004af5
一页素书 <2931107265@qq.com>
2021-01-19 10:16:34 +0800

    fix GasLimit estimate

--- pkg/consensus/expected.go
+++ pkg/consensus/expected.go
@@ -73,7 +73,7 @@ const AllowableClockDriftSecs = uint64(1)
 type Processor interface {
 	// ProcessTipSet processes all messages in a tip set.
 	ProcessTipSet(context.Context, *block.TipSet, *block.TipSet, []block.BlockMessagesInfo, vm.VmOption) (cid.Cid, []types.MessageReceipt, error)
-	ProcessUnsignedMessage(context.Context, types.ChainMsg, vm.VmOption) (*vm.Ret, error)
+	ProcessMessage(context.Context, types.ChainMsg, vm.VmOption) (*vm.Ret, error)
 	ProcessImplicitMessage(context.Context, *types.UnsignedMessage, vm.VmOption) (*vm.Ret, error)
 }
 
