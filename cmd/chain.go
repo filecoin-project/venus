b33641d894c880295b982c40def67cd8731849f2
diwufeiwen <40375298+diwufeiwen@users.noreply.github.com>
2021-02-24 10:14:00 +0800

    simplify chainls output (#4344)
    Co-authored-by: 一页素书 <2931107265@qq.com>

--- cmd/chain.go
+++ cmd/chain.go
@@ -4,6 +4,7 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/filecoin-project/go-address"
@@ -101,31 +102,30 @@ var chainLsCmd = &cmds.Command{
 			return err
 		}
 
-		res := make([]ChainLsResult, 0)
+		buf := new(bytes.Buffer)
+		writer := NewSilentWriter(buf)
+		tpInfoStr := ""
 		for _, key := range tipSetKeys {
 			tp, err := env.(*node.Env).ChainAPI.ChainGetTipSet(key)
 			if err != nil {
 				return err
 			}
 
-			h := tp.Height()
 			strTt := time.Unix(int64(tp.MinTimestamp()), 0).Format("2006-01-02 15:04:05")
 
-			blks := make([]BlockResult, len(tp.Blocks()))
-			for idx, blk := range tp.Blocks() {
-				blks[idx] = BlockResult{Cid: blk.Cid(), Miner: blk.Miner}
+			oneTpInfoStr := fmt.Sprintf("%v: (%s) [ ", tp.Height(), strTt)
+			for _, blk := range tp.Blocks() {
+				oneTpInfoStr += fmt.Sprintf("%s: %s,", blk.Cid().String(), blk.Miner)
 			}
+			oneTpInfoStr += " ]"
 
-			lsRes := ChainLsResult{Height: h, Timestamp: strTt, Blocks: blks}
-			res = append(res, lsRes)
+			tpInfoStr += oneTpInfoStr + "\n"
 		}
 
-		if err := re.Emit(res); err != nil {
-			return err
-		}
-		return nil
+		writer.WriteString(tpInfoStr)
+
+		return re.Emit(buf)
 	},
-	Type: []ChainLsResult{},
 }
 
 var chainSetHeadCmd = &cmds.Command{
