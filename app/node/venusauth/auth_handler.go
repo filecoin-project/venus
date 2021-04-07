6977d3fb84df03402000d32ee44c3373e5dcd454
Rennbon <343688972@qq.com>
2021-04-07 14:13:51 +0800

    no message

--- app/node/venusauth/auth_handler.go
+++ app/node/venusauth/auth_handler.go
@@ -48,34 +48,6 @@ func (h *Handler) Auth(w http.ResponseWriter, r *http.Request) {
 	r = r.WithContext(ctx)
 }
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	/*ctx := r.Context()
-	token := r.Header.Get("Authorization")
-	// if other nodes on the same PC, the permission check will passes directly
-	if r.RemoteAddr[:len("127.0.0.1")] == "127.0.0.1" {
-		ctx = core.WithPerm(ctx, core.PermAdmin)
-	} else {
-		if token == "" {
-			token = r.FormValue("token")
-			if token != "" {
-				token = "Bearer " + token
-			}
-		}
-		if token != "" {
-			if !strings.HasPrefix(token, "Bearer ") {
-				log.Warn("missing Bearer prefix in venusauth header")
-				w.WriteHeader(401)
-				return
-			}
-			token = strings.TrimPrefix(token, "Bearer ")
-			res, err := h.Verify(util.MacAddr(), "venus", r.RemoteAddr, r.Host, token)
-			if err != nil {
-				log.Warnf("JWT Verification failed (originating from %s): %s", r.RemoteAddr, err)
-				w.WriteHeader(401)
-				return
-			}
-			ctx = core.WithPerm(ctx, res.Perm)
-		}
-	}*/
 	h.Auth(w, r)
 	h.Next(w, r)
 }
