68b933e8c78eb7aa1df2990d3b77326110802bcf
hunjixin <1084400399@qq.com>
2021-06-08 08:55:53 +0800

    change permission

--- app/submodule/apiface/wallet.go
+++ app/submodule/apiface/wallet.go
@@ -11,25 +11,25 @@ import (
 )
 
 type IWallet interface {
-	// Rule[perm:admin]
+	// Rule[perm:sign]
 	WalletSign(ctx context.Context, k address.Address, msg []byte, meta wallet.MsgMeta) (*crypto.Signature, error)
 	// Rule[perm:admin]
 	WalletExport(addr address.Address, password string) (*crypto.KeyInfo, error)
 	// Rule[perm:admin]
 	WalletImport(key *crypto.KeyInfo) (address.Address, error)
-	// Rule[perm:admin]
+	// Rule[perm:write]
 	WalletHas(ctx context.Context, addr address.Address) (bool, error)
-	// Rule[perm:admin]
+	// Rule[perm:write]
 	WalletNewAddress(protocol address.Protocol) (address.Address, error)
-	// Rule[perm:admin]
+	// Rule[perm:read]
 	WalletBalance(ctx context.Context, addr address.Address) (abi.TokenAmount, error) //not exists in remote
-	// Rule[perm:admin]
+	// Rule[perm:write]
 	WalletDefaultAddress(ctx context.Context) (address.Address, error) //not exists in remote
 	// Rule[perm:admin]
 	WalletAddresses(ctx context.Context) []address.Address
 	// Rule[perm:admin]
 	WalletSetDefault(ctx context.Context, addr address.Address) error //not exists in remote
-	// Rule[perm:admin]
+	// Rule[perm:sign]
 	WalletSignMessage(ctx context.Context, k address.Address, msg *types.UnsignedMessage) (*types.SignedMessage, error)
 	// Rule[perm:admin]
 	LockWallet(ctx context.Context) error
