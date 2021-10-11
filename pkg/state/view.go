e7ddaedbe102c04a41b0a3c8b3acd2ecb331cb4d
zl <zl03jsj@163.com>
2021-10-11 17:14:51 +0800

    fix unconsensus apis (#4563)

--- pkg/state/view.go
+++ pkg/state/view.go
@@ -629,7 +629,7 @@ func (v *View) StateMinerPower(ctx context.Context, maddr addr.Address, tsk type
 		mpow, found, err = pas.MinerPower(maddr)
 		if err != nil || !found {
 			// TODO: return an error when not found?
-			return power.Claim{}, power.Claim{}, false, err
+			return power.Claim{}, tpow, false, err
 		}
 
 		minpow, err = pas.MinerNominalPowerMeetsConsensusMinimum(maddr)
