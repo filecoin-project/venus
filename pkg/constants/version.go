946e85ed1d6a20b2214f9f30d63777e07d7e6cd1
hunjixin <1084400399@qq.com>
2021-11-04 14:39:05 +0800

    update version to v1.1.2-rc2

--- pkg/constants/version.go
+++ pkg/constants/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.1.0"
+const BuildVersion = "1.1.2-rc2"
 
 // software version
 func UserVersion() string {
