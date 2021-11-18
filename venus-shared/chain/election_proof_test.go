d70e822eba46564dfec58a94483e927e204699bf
dtynn <dtynn@163.com>
2021-11-18 16:50:45 +0800

    feat: add more tests for TipSet / ElectionProof / Ticket

--- venus-shared/chain/election_proof_test.go
+++ venus-shared/chain/election_proof_test.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/venus/venus-shared/chain/params"
 	"github.com/filecoin-project/venus/venus-shared/testutil"
 )
 
@@ -16,6 +19,11 @@ func TestElectionProofBasic(t *testing.T) {
 	var buf bytes.Buffer
 	for i := 0; i < 32; i++ {
 		var src, dst ElectionProof
+
+		var power abi.StoragePower
+		testutil.Provide(t, &power, testutil.PositiveBigProvider())
+		require.True(t, power.GreaterThan(big.Zero()), "positive storage power")
+		totalPower := BigMul(power, NewInt(7))
 
 		opt := testutil.CborErBasicTestOptions{
 			Buf: &buf,
@@ -37,6 +45,10 @@ func TestElectionProofBasic(t *testing.T) {
 
 			Finished: func() {
 				require.Equal(t, src, dst, "from src to dst through cbor")
+				winCount := src.ComputeWinCount(power, totalPower)
+				require.GreaterOrEqual(t, winCount, int64(0), "win count >=0")
+				require.LessOrEqual(t, winCount, params.MaxWinCount, "win count <= MaxWinCount")
+				require.Equal(t, winCount, dst.ComputeWinCount(power, totalPower))
 
 				t1, t2 := Ticket{
 					VRFProof: src.VRFProof,
@@ -90,6 +102,7 @@ func TestTicketBasic(t *testing.T) {
 
 				require.True(t, t1.Compare(&t2) == 0, "ticket equal")
 				require.Equal(t, t1.Quality(), t2.Quality())
+				require.Equal(t, t1.String(), t2.String(), "ticket string")
 
 				testutil.Provide(t, &another, testutil.BytesFixedProvider(vrfLen))
 				require.Len(t, another.VRFProof, vrfLen, "vrf length")
