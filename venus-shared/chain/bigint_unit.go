3669d2982a85e5b910287511ebb84405434e8aac
dtynn <dtynn@163.com>
2021-11-18 11:18:03 +0800

    feat: add more tests for unit string

--- venus-shared/chain/bigint_unit.go
+++ venus-shared/chain/bigint_unit.go
@@ -8,31 +8,27 @@ import (
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)
-	den := big.NewRat(1, 1024)
-
-	var i int
-	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++
-		r = r.Mul(r, den)
-	}
-
-	f, _ := r.Float64()
+	f, i := unitNumber(bi, byteSizeUnits)
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)
+	f, i := unitNumber(bi, deciUnits)
+	return fmt.Sprintf("%.3g %s", f, deciUnits[i])
+}
+
+func unitNumber(n BigInt, units []string) (float64, int) {
+	r := new(big.Rat).SetInt(n.Int)
 	den := big.NewRat(1, 1024)
 
 	var i int
-	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
+	for f, _ := r.Float64(); f >= 1024 && i+1 < len(units); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
 	}
 
 	f, _ := r.Float64()
-	return fmt.Sprintf("%.3g %s", f, deciUnits[i])
+	return f, i
 }
