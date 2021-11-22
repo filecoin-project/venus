01212237cb6b6452f67161427acd11cc62c6e8e9
dtynn <dtynn@163.com>
2021-11-22 17:37:04 +0800

    fix: stuffs required by venus-component/libp2p/exchange

--- venus-shared/libp2p/peer_mgr.go
+++ venus-shared/libp2p/peer_mgr.go
@@ -7,6 +7,10 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
+type NewFilPeer struct {
+	Id peer.ID // nolint: stylecheck
+}
+
 type PeerManager interface {
 	AddFilecoinPeer(ctx context.Context, p peer.ID)
 	GetPeerLatency(ctx context.Context, p peer.ID) (time.Duration, bool)
