c3736aaafa815af94b55cfe3b3edb65f5d71a02d
dtynn <dtynn@163.com>
2021-11-23 11:44:35 +0800

    feat: logger with context

---
+++ venus-shared/logging/logger.go
@@ -0,0 +1,35 @@
+package logging
+
+import (
+	"context"
+
+	logging "github.com/ipfs/go-log"
+	"go.uber.org/zap"
+)
+
+type contextKey string
+
+var ctxKey contextKey = "logger"
+
+type (
+	EventLogger  = logging.ZapEventLogger
+	TaggedLogger = zap.SugaredLogger
+)
+
+var New = logging.Logger
+
+func ContextWithLogger(parent context.Context, l TaggedLogger) context.Context {
+	return context.WithValue(parent, ctxKey, l)
+}
+
+func LoggerFromContext(ctx context.Context, fallback *EventLogger) TaggedLogger {
+	val := ctx.Value(ctxKey)
+	if val != nil {
+		l, ok := val.(TaggedLogger)
+		if ok {
+			return l
+		}
+	}
+
+	return fallback.SugaredLogger
+}
