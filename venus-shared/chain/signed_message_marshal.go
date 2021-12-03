1e1f3f9dec82a9d6737ff33324c5a572c970186a
dtynn <dtynn@163.com>
2021-12-03 13:31:59 +0800

    fix: api diffs caused by json.Marshaler impl of Message & SignedMessage

---
+++ venus-shared/chain/signed_message_marshal.go
@@ -0,0 +1,21 @@
+package chain
+
+import (
+	"encoding/json"
+
+	"github.com/ipfs/go-cid"
+)
+
+type smCid struct {
+	*RawSignedMessage
+	CID cid.Cid
+}
+
+type RawSignedMessage SignedMessage
+
+func (sm *SignedMessage) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&smCid{
+		RawSignedMessage: (*RawSignedMessage)(sm),
+		CID:              sm.Cid(),
+	})
+}
