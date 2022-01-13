cf678daeafe630b3757f7b3a73cf11f665259b8f
simlecode <hj1557986@gmail.com>
2022-01-13 16:18:43 +0800

    fix api client

--- venus-shared/api/client/client.go
+++ venus-shared/api/client/client.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"context"
+
 	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/venus/venus-shared/api"
 	v0 "github.com/filecoin-project/venus/venus-shared/api/chain/v0"
 	v1 "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
 
@@ -11,20 +13,20 @@ import (
 
 func NewFullRPCV0(ctx context.Context, addr string, header http.Header) (v0.FullNode, jsonrpc.ClientCloser, error) {
 	var full v0.FullNodeStruct
-	closer, err := jsonrpc.NewClient(ctx, addr, "Filecoin", &full,
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin", api.GetInternalStructs(&full),
 		header)
 	return &full, closer, err
 }
 
 func NewFullRPCV1(ctx context.Context, addr string, header http.Header) (v1.FullNode, jsonrpc.ClientCloser, error) {
 	var full v1.FullNodeStruct
-	closer, err := jsonrpc.NewClient(ctx, addr, "Filecoin", &full,
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin", api.GetInternalStructs(&full),
 		header)
 	return &full, closer, err
 }
 
 func NewWalletRPC(ctx context.Context, addr string, header http.Header) (v1.IWallet, jsonrpc.ClientCloser, error) {
 	var wallet v1.IWalletStruct
-	closer, err := jsonrpc.NewClient(ctx, addr, "Filecoin", &wallet, header)
+	closer, err := jsonrpc.NewClient(ctx, addr, "Filecoin", &wallet.Internal, header)
 	return &wallet, closer, err
 }
