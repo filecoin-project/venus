565f351d963d70c991b0564a864ef641d5d033f1
dtynn <dtynn@163.com>
2022-01-18 10:18:54 +0800

    feat: shared types from venus-messager

---
+++ venus-shared/types/messager/address.go
@@ -0,0 +1,58 @@
+package messager
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/big"
+
+	"github.com/filecoin-project/venus/venus-shared/types"
+)
+
+type AddressState int
+
+const (
+	_ AddressState = iota
+	AddressStateAlive
+	AddressStateRemoving
+	AddressStateRemoved
+	AddressStateForbbiden // forbbiden received message
+)
+
+func (as AddressState) String() string {
+	switch as {
+	case AddressStateAlive:
+		return "Alive"
+	case AddressStateRemoving:
+		return "Removing"
+	case AddressStateRemoved:
+		return "Removed"
+	case AddressStateForbbiden:
+		return "Forbbiden"
+	default:
+		return fmt.Sprintf("unknow state %d", as)
+	}
+}
+
+func AddressStateToString(state AddressState) string {
+	return state.String()
+}
+
+type Address struct {
+	ID   types.UUID      `json:"id"`
+	Addr address.Address `json:"addr"`
+	//max for current, use nonce and +1
+	Nonce  uint64 `json:"nonce"`
+	Weight int64  `json:"weight"`
+	// number of address selection messages
+	SelMsgNum         uint64       `json:"selMsgNum"`
+	State             AddressState `json:"state"`
+	GasOverEstimation float64      `json:"gasOverEstimation"`
+	MaxFee            big.Int      `json:"maxFee,omitempty"`
+	MaxFeeCap         big.Int      `json:"maxFeeCap"`
+
+	IsDeleted int       `json:"isDeleted"` // 是否删除 1:是  -1:否
+	CreatedAt time.Time `json:"createAt"`  // 创建时间
+	UpdatedAt time.Time `json:"updateAt"`  // 更新时间
+}
---
+++ venus-shared/types/uuid.go
@@ -0,0 +1,79 @@
+package types
+
+import (
+	"bytes"
+	"database/sql/driver"
+
+	"golang.org/x/xerrors"
+
+	"github.com/google/uuid"
+)
+
+type UUID uuid.UUID
+
+func NewUUID() UUID {
+	return UUID(uuid.New())
+}
+
+func ParseUUID(uid string) (UUID, error) {
+	id, err := uuid.Parse(uid)
+	if err != nil {
+		return UUID{}, nil
+	}
+
+	return UUID(id), nil
+}
+
+// Value implement sql.Scanner
+func (uid UUID) IsEmpty() bool {
+	return uid == UUID{}
+}
+
+// Value implement sql.Scanner
+func (uid UUID) String() string {
+	return uuid.UUID(uid).String()
+}
+
+// Value implement sql.Scanner
+func (uid UUID) Value() (driver.Value, error) {
+	return uuid.UUID(uid).String(), nil
+}
+
+// Scan assigns a value from a database driver.
+// An error should be returned if the value cannot be stored
+// without loss of information.
+//
+// Reference types such as []byte are only valid until the next call to Scan
+// and should not be retained. Their underlying memory is owned by the driver.
+// If retention is necessary, copy their values before the next call to Scan.
+func (uid *UUID) Scan(value interface{}) error {
+	var id uuid.UUID
+	var err error
+	switch value := value.(type) {
+	case string:
+		id, err = uuid.Parse(value)
+	case []byte:
+		id, err = uuid.ParseBytes(value)
+	default:
+		return xerrors.Errorf("unsupport %t type for uuid", value)
+	}
+	if err != nil {
+		return err
+	}
+	*uid = (UUID)(id)
+	return nil
+}
+
+func (uid UUID) MarshalJSON() ([]byte, error) {
+	return []byte("\"" + uid.String() + "\""), nil
+}
+
+func (uid *UUID) UnmarshalJSON(b []byte) error {
+	b = bytes.Trim(b, "\"")
+	id, err := uuid.ParseBytes(b)
+	if err != nil {
+		return err
+	}
+	*uid = (UUID)(id)
+	return nil
+}
