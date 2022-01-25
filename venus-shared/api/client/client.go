71d9e19fcfa17d88d421ce2dceb99d65b2d9879c
simlecode <hj1557986@gmail.com>
2022-01-25 13:42:17 +0800

    remove blank line

--- venus-shared/api/client/client.go
+++ venus-shared/api/client/client.go
@@ -2,14 +2,12 @@ package client
 
 import (
 	"context"
-
-	"github.com/filecoin-project/venus/venus-shared/api"
+	"net/http"
 
 	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/venus/venus-shared/api"
 	v0 "github.com/filecoin-project/venus/venus-shared/api/chain/v0"
 	v1 "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
-
-	"net/http"
 )
 
 func NewFullRPCV0(ctx context.Context, addr string, header http.Header) (v0.FullNode, jsonrpc.ClientCloser, error) {
