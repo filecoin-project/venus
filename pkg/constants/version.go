ed2a206924886d6128eaaf3907f2d08573bc1373
tom <69969590+simlecode@users.noreply.github.com>
2022-03-01 11:17:45 +0800

    update version to v1.2.2 (#4807)

--- pkg/constants/version.go
+++ pkg/constants/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.2.1"
+const BuildVersion = "1.2.2"
 
 // software version
 func UserVersion() string {
