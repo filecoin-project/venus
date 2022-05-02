2172b131de075677c548a7f3f9fef46bd55fd3a7
diwufeiwen <1033935631@qq.com>
2022-05-02 22:09:56 +0800

    add: docker dir and dockerfile

---
+++ docker/script/compose.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo $@
+
+
+if [ $nettype=="calibnet" ]
+then
+    nettype="cali"
+fi
+
+echo $nettype
+./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 --import-snapshot /snapshot.car
