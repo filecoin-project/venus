b4d6200e64a3e379e187ba831000942cec4043f3
tanlang <lin.zexiao@qq.com>
2022-05-09 10:33:09 +0800

    fix:log more readable

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -6,5 +6,6 @@ if [ $nettype = "calibnet" ];then
     nettype="cali"
 fi
 
+echo "nettype:"
 echo $nettype
 ./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 --import-snapshot /snapshot.car
