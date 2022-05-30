63b9ab9aa8c7bd3cef431fa7a9e1423b6260ff3b
simlecode <hj1557986@gmail.com>
2022-05-30 23:25:02 +0800

    chore: dep: update go-fil-market

--- venus-shared/types/market/miner_deal.go
+++ venus-shared/types/market/miner_deal.go
@@ -9,7 +9,7 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/specs-actors/v8/actors/builtin/market"
+	"github.com/filecoin-project/go-state-types/builtin/v8/market"
 )
 
 type MinerDeal struct {
