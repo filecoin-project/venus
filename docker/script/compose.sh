f546229b3450d16e0628627ab44ee4d4300613d6
tanlang <lin.zexiao@qq.com>
2022-07-01 14:55:19 +0800

    feat: make snapshot optional

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -8,4 +8,11 @@ fi
 
 echo "nettype:"
 echo $nettype
-./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 --import-snapshot /snapshot.car
+
+if [ -f "/snapshot.car"  ]
+then
+    echo "snapshot.car exists"
+    ./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 --import-snapshot /snapshot.car
+else
+    echo "snapshot.car not found"
+    ./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 
