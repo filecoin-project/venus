f499798e59682d31cd4369b7becf9fc0731589f3
zl <zl03jsj@163.com>
2022-11-02 10:36:17 +0800

    chore: golangci-lint: varcheck,deadcode,structcheck of golangcilint is deprecated and was replaced by unused.

--- pkg/chainsync/types/bad_tipset_cache_test.go
+++ pkg/chainsync/types/bad_tipset_cache_test.go
@@ -10,21 +10,21 @@ import (
 )
 
 func TestBadTipsetCache(t *testing.T) {
-	badTsCache := NewBadTipSetCache()
+	badTSCache := NewBadTipSetCache()
 
 	var ts types.TipSet
 	testutil.Provide(t, &ts)
 
 	// stm: @CHAINSYNC_TYPES_ADD_CHAIN_001
-	badTsCache.AddChain([]*types.TipSet{&ts})
+	badTSCache.AddChain([]*types.TipSet{&ts})
 
 	var tsKey types.TipSetKey
 	testutil.Provide(t, &tsKey, testutil.WithSliceLen(3))
 
 	// stm: @CHAINSYNC_TYPES_ADD_001
-	badTsCache.Add(tsKey.String())
+	badTSCache.Add(tsKey.String())
 
 	// stm: @CHAINSYNC_TYPES_HAS_001
-	assert.True(t, badTsCache.Has(ts.Key().String()))
-	assert.True(t, badTsCache.Has(tsKey.String()))
+	assert.True(t, badTSCache.Has(ts.Key().String()))
+	assert.True(t, badTSCache.Has(tsKey.String()))
 }
