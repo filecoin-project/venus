660e06d3c45e4a4d33917429f3e290c791d36924
simlecode <69969590+simlecode@users.noreply.github.com>
2023-01-31 15:45:13 +0800

    fix: events: put the _signed_ message through the pipeline

--- pkg/events/filter/event.go
+++ pkg/events/filter/event.go
@@ -278,13 +278,13 @@ func (te *TipSetEvents) messages(ctx context.Context) ([]executedMessage, error)
 }
 
 type executedMessage struct {
-	msg *types.Message
+	msg types.ChainMsg
 	rct *types.MessageReceipt
 	// events extracted from receipt
 	evs []*types.Event
 }
 
-func (e *executedMessage) Message() *types.Message {
+func (e *executedMessage) Message() types.ChainMsg {
 	return e.msg
 }
 
@@ -441,7 +441,7 @@ func (m *EventFilterManager) loadExecutedMessages(ctx context.Context, msgTS, rc
 	ems := make([]executedMessage, len(msgs))
 
 	for i := 0; i < len(msgs); i++ {
-		ems[i].msg = msgs[i].VMMessage()
+		ems[i].msg = msgs[i]
 
 		var rct types.MessageReceipt
 		found, err := arr.Get(uint64(i), &rct)
