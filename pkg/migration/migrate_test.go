8789646907cc236696efbfb042226d8e3dd4dd5d
simlecode <69969590+simlecode@users.noreply.github.com>
2023-02-01 13:47:34 +0800

    chore: disable wallabynet and hypersapcenet

--- pkg/migration/migrate_test.go
+++ pkg/migration/migrate_test.go
@@ -18,15 +18,13 @@ func TestMigration(t *testing.T) {
 	tf.UnitTest(t)
 
 	cfgs := map[types.NetworkType]*config.NetworkParamsConfig{
-		types.Network2k:         &networks.Net2k().Network,
-		types.NetworkForce:      &networks.ForceNet().Network,
-		types.NetworkInterop:    &networks.InteropNet().Network,
-		types.NetworkButterfly:  &networks.ButterflySnapNet().Network,
-		types.NetworkCalibnet:   &networks.Calibration().Network,
-		types.NetworkMainnet:    &networks.Mainnet().Network,
-		types.Integrationnet:    &networks.IntegrationNet().Network,
-		types.NetworkWallaby:    &networks.WallabyNet().Network,
-		types.NetworkHyperspace: &networks.HyperspaceNet().Network,
+		types.Network2k:        &networks.Net2k().Network,
+		types.NetworkForce:     &networks.ForceNet().Network,
+		types.NetworkInterop:   &networks.InteropNet().Network,
+		types.NetworkButterfly: &networks.ButterflySnapNet().Network,
+		types.NetworkCalibnet:  &networks.Calibration().Network,
+		types.NetworkMainnet:   &networks.Mainnet().Network,
+		types.Integrationnet:   &networks.IntegrationNet().Network,
 	}
 
 	for nt, paramsCfg := range cfgs {
