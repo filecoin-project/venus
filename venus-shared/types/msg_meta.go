7f1480cc457a5c65bfd02425c5b42a252c808496
tanlang <lin.zexiao@qq.com>
2023-02-15 07:46:07 +0000

    feat: add MsgType.MTUndefined

--- venus-shared/types/msg_meta.go
+++ venus-shared/types/msg_meta.go
@@ -3,7 +3,8 @@ package types
 type MsgType string
 
 const (
-	MTUnknown = MsgType("unknown")
+	MTUndefined = MsgType("")
+	MTUnknown   = MsgType("unknown")
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = MsgType("message")
