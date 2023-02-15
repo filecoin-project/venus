46d93f4093ca1deb2ba9caee748b65d0905a6f18
simlecode <69969590+simlecode@users.noreply.github.com>
2023-02-15 09:34:04 +0800

    fix: copy the message before modifying it

--- pkg/statemanger/call.go
+++ pkg/statemanger/call.go
@@ -31,6 +31,10 @@ import (
 // tipset's parent. In the presence of null blocks, the height at which the message is invoked may
 // be less than the specified tipset.
 func (s *Stmgr) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*types.InvocResult, error) {
+	// Copy the message as we modify it below.
+	msgCopy := *msg
+	msg = &msgCopy
+
 	if msg.GasLimit == 0 {
 		msg.GasLimit = constants.BlockGasLimit
 	}
