d8ddc190f704cfaa273d568bfae268ec7b09f85e
bhaskarvilles <bhaskarvilles.eth@ethereum.email>
2023-03-05 15:11:31 +0530

    Add support for command line log level flags

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-	_ "net/http/pprof"
+	"flag"
+	"io/ioutil"
+	"log"
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
@@ -10,40 +12,48 @@ import (
 	"github.com/filecoin-project/venus/cmd"
 )
 
+var (
+	logLevel string
+)
+
+func init() {
+	flag.StringVar(&logLevel, "log-level", "", "Set log level (default: info)")
+}
+
 func main() {
+	flag.Parse()
+
 	// set default log level if no flags given
-	lvl := os.Getenv("GO_FILECOIN_LOG_LEVEL")
-	if lvl == "" {
+	if logLevel == "" {
 		logging.SetAllLoggers(logging.LevelInfo)
-		_ = logging.SetLogLevel("beacon", "error")
-		_ = logging.SetLogLevel("peer-tracker", "error")
-		_ = logging.SetLogLevel("dht", "error")
-		_ = logging.SetLogLevel("bitswap", "error")
-		_ = logging.SetLogLevel("graphsync", "info")
-		_ = logging.SetLogLevel("heartbeat", "error")
-		_ = logging.SetLogLevel("dagservice", "error")
-		_ = logging.SetLogLevel("peerqueue", "error")
-		_ = logging.SetLogLevel("swarm", "error")
-		_ = logging.SetLogLevel("swarm2", "error")
-		_ = logging.SetLogLevel("basichost", "error")
-		_ = logging.SetLogLevel("dht_net", "error")
-		_ = logging.SetLogLevel("pubsub", "error")
-		_ = logging.SetLogLevel("relay", "error")
-		_ = logging.SetLogLevel("dht/RtRefreshManager", "error")
 	} else {
-		level, err := logging.LevelFromString(lvl)
+		level, err := logging.LevelFromString(logLevel)
 		if err != nil {
-			level = logging.LevelInfo
+			log.Fatalf("Error setting log level: %v", err)
 		}
 		logging.SetAllLoggers(level)
 	}
 
-	if len(os.Args) > 1 {
-		if os.Args[1] == "-v" {
-			os.Args[1] = "version"
+	// read logger configuration file if present
+	if _, err := os.Stat("logger.json"); !os.IsNotExist(err) {
+		file, err := ioutil.ReadFile("logger.json")
+		if err != nil {
+			log.Fatalf("Error reading logger configuration file: %v", err)
+		}
+		if err := logging.SetLogLevelConfigString(string(file)); err != nil {
+			log.Fatalf("Error setting logger configuration: %v", err)
 		}
 	}
 
-	code, _ := cmd.Run(context.Background(), os.Args, os.Stdin, os.Stdout, os.Stderr)
+	if len(os.Args) > 1 && os.Args[1] == "-v" {
+		os.Args[1] = "version"
+	}
+
+	code, err := cmd.Run(context.Background(), os.Args, os.Stdin, os.Stdout, os.Stderr)
+	if err != nil {
+		log.Printf("Error running command: %v", err)
+		os.Exit(1)
+	}
+
 	os.Exit(code)
 }
