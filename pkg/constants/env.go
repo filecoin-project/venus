cfc6e7d80e011fd2816ac1e42e3cde28c0d471ca
hunjixin <1084400399@qq.com>
2023-03-29 10:29:49 +0800

    feat: use env to control insecurepost

--- pkg/constants/env.go
+++ pkg/constants/env.go
@@ -4,3 +4,6 @@ import "os"
 
 // FevmEnableEthRPC enables eth rpc, and enables storing a mapping of eth transaction hashes to filecoin message Cids.
 var FevmEnableEthRPC = os.Getenv("VENUS_FEVM_ENABLEETHRPC") == "1"
+
+// InsecurePoStValidation use to attach debug
+var InsecurePoStValidation = os.Getenv("INSECURE_POST_VALIDATION") == "1"
