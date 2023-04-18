c50f8c866b01df274f26f1c1b85e3e92159f6a25
simlecode <69969590+simlecode@users.noreply.github.com>
2023-04-18 17:11:29 +0800

    fix: eth: handle a potential divide by zero in receipt handling

--- app/submodule/eth/eth_api.go
+++ app/submodule/eth/eth_api.go
@@ -1375,7 +1375,10 @@ func newEthTxReceipt(ctx context.Context, tx types.EthTx, lookup *types.MsgLooku
 	gasOutputs := gas.ComputeGasOutputs(lookup.Receipt.GasUsed, int64(tx.Gas), baseFee, big.Int(tx.MaxFeePerGas), big.Int(tx.MaxPriorityFeePerGas), true)
 	totalSpent := big.Sum(gasOutputs.BaseFeeBurn, gasOutputs.MinerTip, gasOutputs.OverEstimationBurn)
 
-	effectiveGasPrice := big.Div(totalSpent, big.NewInt(lookup.Receipt.GasUsed))
+	effectiveGasPrice := big.Zero()
+	if lookup.Receipt.GasUsed > 0 {
+		effectiveGasPrice = big.Div(totalSpent, big.NewInt(lookup.Receipt.GasUsed))
+	}
 	receipt.EffectiveGasPrice = types.EthBigInt(effectiveGasPrice)
 
 	if receipt.To == nil && lookup.Receipt.ExitCode.IsSuccess() {
