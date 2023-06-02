322884f3bf24bebc7c38973ae28bfa69f8b4fdea
tanlang <lin.zexiao@qq.com>
2023-06-02 11:24:01 +0800

    feat: add ExecutionTrace cache

--- pkg/chainsync/syncer/syncer_integration_test.go
+++ pkg/chainsync/syncer/syncer_integration_test.go
@@ -40,7 +40,8 @@ func TestLoadFork(t *testing.T) {
 	sel := &chain.FakeChainSelector{}
 
 	blockValidator := builder.FakeStateEvaluator()
-	stmgr := statemanger.NewStateManger(builder.Store(), builder.MessageStore(), blockValidator, nil, nil, nil, nil, false)
+	stmgr, err := statemanger.NewStateManger(builder.Store(), builder.MessageStore(), blockValidator, nil, nil, nil, nil, false)
+	require.NoError(t, err)
 
 	s, err := syncer.NewSyncer(stmgr, blockValidator, sel, builder.Store(),
 		builder.Mstore(), builder.BlockStore(), builder, clock.NewFake(time.Unix(1234567890, 0)), nil)
