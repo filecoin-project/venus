5252965c9319f0da232a9bf06d44b8c5721ea8d9
Elvin <dumanxiang@qq.com>
2023-08-21 17:40:48 +0800

    set gcConfidence = 2 * constants.ForkLengthThreshold

--- app/submodule/eth/eth_event_api.go
+++ app/submodule/eth/eth_event_api.go
@@ -26,8 +26,6 @@ import (
 	"github.com/zyedidia/generic/queue"
 )
 
-const ChainHeadConfidence = 1
-
 var _ v1.IETHEvent = (*ethEventAPI)(nil)
 
 func newEthEventAPI(ctx context.Context, em *EthSubModule) (*ethEventAPI, error) {
@@ -128,7 +126,7 @@ func (e *ethEventAPI) Start(ctx context.Context) error {
 	// Start garbage collection for filters
 	go e.GC(ctx, time.Duration(e.em.cfg.FevmConfig.Event.FilterTTL))
 
-	ev, err := events.NewEventsWithConfidence(ctx, e.ChainAPI, ChainHeadConfidence)
+	ev, err := events.NewEvents(ctx, e.ChainAPI)
 	if err != nil {
 		return err
 	}
