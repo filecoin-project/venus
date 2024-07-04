d9d480324f30e9c05f82def0c19883cb10f7b9e7
beeleelee <beeleelee@outlook.com>
2024-07-04 18:05:05 +0800

    forcenet network params adjust  #6371

--- fixtures/networks/forcenet.go
+++ fixtures/networks/forcenet.go
@@ -71,8 +71,8 @@ func ForceNet() *NetworkConf {
 			AllowableClockDriftSecs: 1,
 			Eip155ChainID:           31415926,
 			ActorDebugging:          true,
-			F3Enabled:               false,
-			F3BootstrapEpoch:        -1,
+			F3Enabled:               true,
+			F3BootstrapEpoch:        100,
 		},
 	}
 	nc.Network.DrandSchedule[nc.Network.ForkUpgradeParam.UpgradePhoenixHeight] = config.DrandQuicknet
