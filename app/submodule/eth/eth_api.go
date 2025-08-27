676cd4f0bb072e6ec8d2c394383daab7a2be80b7
simlecode <69969590+simlecode@users.noreply.github.com>
2025-08-27 16:00:08 +0800

    fix(eth): apply limit in EthGetBlockReceiptsLimited

--- app/submodule/eth/eth_api.go
+++ app/submodule/eth/eth_api.go
@@ -464,8 +464,10 @@ func (a *ethAPI) EthGetTransactionReceiptLimited(ctx context.Context, txHash typ
 		return nil, fmt.Errorf("failed to lookup Eth Txn %s as %s: %w", txHash, c, err)
 	}
 	if msgLookup == nil {
-		// This is the best we can do. In theory, we could have just not indexed this
-		// transaction, but there's no way to check that here.
+		// This is the best we can do. We may just not have indexed this transaction, or we may have a
+		// limit applied and not searched far back enough, but we don't have a way to go. Because
+		// Ethereum tooling expects an empty response for transaction-not-found, we don't have a way of
+		// differentiating between "can't find" and "doesn't exist".
 		return nil, nil
 	}
 
@@ -507,6 +509,14 @@ func (a *ethAPI) EthGetBlockReceiptsLimited(ctx context.Context, blockParam type
 	ts, err := getTipsetByEthBlockNumberOrHash(ctx, a.em.chainModule.ChainReader, blockParam)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get tipset: %w", err)
+	}
+
+	head, err := a.chain.ChainHead(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get head: %v", err)
+	}
+	if limit > constants.LookbackNoLimit && ts.Height() < head.Height() {
+		return nil, fmt.Errorf("tipset %s is older than the allowed lookback limit", ts.Key())
 	}
 
 	tsCid, err := ts.Key().Cid()
